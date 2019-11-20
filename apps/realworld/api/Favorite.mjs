50be5a634f2245c2c4a9676928abea2e033b1537
tobiu <tobiasuhlig78@gmail.com>
2019-11-20 21:05:42 +0100

    RealWorld.api.Favorite

---
+++ apps/realworld/api/Favorite.mjs
@@ -0,0 +1,32 @@
+import Base from './Base.mjs';
+
+/**
+ * @class RealWorld.api.Favorite
+ * @extends RealWorld.api.Base
+ */
+class Favorite extends Base {
+    static getConfig() {return {
+        /**
+         * @member {String} className='RealWorld.api.Favorite'
+         * @private
+         */
+        className: 'RealWorld.api.Favorite',
+        /**
+         * @member {String} ntype='realworld-api-favorite'
+         * @private
+         */
+        ntype: 'realworld-api-favorite',
+        /**
+         * @member {String} resource='/articles'
+         */
+        resource: '/articles' // todo: `articles/${slug}/favorite`
+    }}
+}
+
+Neo.applyClassConfig(Favorite);
+
+let instance = Neo.create(Favorite);
+
+Neo.applyToGlobalNs(instance);
+
+export default instance;
