ac80ef05da4bdc29a11826cf690c8b6fa1953efe
tobiu <tobiasuhlig78@gmail.com>
2019-11-20 18:10:42 +0100

    RealWorld app: HomeComponent => paging buttons #73 => afterSetCurrentPage() => using this.pageSize instead of a static value

--- apps/realworld/views/HomeComponent.mjs
+++ apps/realworld/views/HomeComponent.mjs
@@ -261,7 +261,7 @@ class HomeComponent extends Component {
 
             me.vdom = vdom;
 
-            me.getController().articlesOffset = (value - 1) * 10;
+            me.getController().articlesOffset = (value - 1) * me.pageSize;
         }
     }
 
