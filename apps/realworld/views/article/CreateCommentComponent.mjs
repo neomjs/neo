aacb7f624ee720f9f7450592ab5cb0cdf3abfd21
tobiu <tobiasuhlig78@gmail.com>
2019-11-22 15:19:30 +0100

    #74 RealWorld.views.article.CreateCommentComponent: added a submitButton click listener, fetching the textarea value

--- apps/realworld/views/article/CreateCommentComponent.mjs
+++ apps/realworld/views/article/CreateCommentComponent.mjs
@@ -5,50 +5,94 @@ import {default as Component} from '../../../../src/component/Base.mjs';
  * @extends Neo.component.Base
  */
 class CreateCommentComponent extends Component {
-    static getConfig() {
-        return {
-            /**
-             * @member {String} className='RealWorld.views.article.CreateCommentComponent'
-             * @private
-             */
-            className: 'RealWorld.views.article.CreateCommentComponent',
-            /**
-             * @member {String} ntype='realworld-article-createcommentcomponent'
-             * @private
-             */
-            ntype: 'realworld-article-createcommentcomponent',
-            /**
-             * @member {String[]} cls=['card', 'comment-form']
-             */
-            cls: ['card', 'comment-form'],
-            /**
-             * @member {Object} _vdom
-             */
-            _vdom: {
-                tag: 'form',
+    static getConfig() {return {
+        /**
+         * @member {String} className='RealWorld.views.article.CreateCommentComponent'
+         * @private
+         */
+        className: 'RealWorld.views.article.CreateCommentComponent',
+        /**
+         * @member {String} ntype='realworld-article-createcommentcomponent'
+         * @private
+         */
+        ntype: 'realworld-article-createcommentcomponent',
+        /**
+         * @member {String[]} cls=['card', 'comment-form']
+         */
+        cls: ['card', 'comment-form'],
+        /**
+         * @member {Object} _vdom
+         */
+        _vdom: {
+            tag: 'form',
+            cn : [{
+                cls: ['card-block'],
                 cn : [{
-                    cls: ['card-block'],
-                    cn : [{
-                        tag        : 'textarea',
-                        cls        : ['form-control'],
-                        placeholder: 'Write a comment...',
-                        rows       : 3
-                    }]
-                }, {
-                    cls: ['card-footer'],
-                    cn : [{
-                        tag: 'img',
-                        cls: ['comment-author-img'],
-                        src: 'http://i.imgur.com/Qr71crq.jpg'
-                    }, {
-                        tag : 'button',
-                        cls : ['btn', 'btn-sm', 'btn-primary'],
-                        html: 'Post Comment',
-                        type: 'button' // override the default submit type
-                    }]
+                    tag        : 'textarea',
+                    cls        : ['form-control'],
+                    placeholder: 'Write a comment...',
+                    rows       : 3
                 }]
-            }
+            }, {
+                cls: ['card-footer'],
+                cn : [{
+                    tag: 'img',
+                    cls: ['comment-author-img'],
+                    src: 'http://i.imgur.com/Qr71crq.jpg'
+                }, {
+                    tag : 'button',
+                    cls : ['btn', 'btn-sm', 'btn-primary'],
+                    html: 'Post Comment',
+                    type: 'button' // override the default submit type
+                }]
+            }]
         }
+    }}
+
+    /**
+     *
+     * @param {Object} config
+     */
+    constructor(config) {
+        super(config);
+
+        let me           = this,
+            domListeners = me.domListeners,
+            vdom         = me.vdom;
+
+        domListeners.push({
+            click: {
+                fn      : me.onSubmitButtonClick,
+                delegate: '.btn-primary',
+                scope   : me
+            }
+        });
+
+        me.domListeners = domListeners;
+
+        vdom.cn[0].cn[0].id = me.getInputElId();
+        me.vdom = vdom;
+    }
+
+    getInputElId() {
+        return this.id + '__input';
+    }
+
+    /**
+     *
+     * @param {Object} data
+     */
+    onSubmitButtonClick(data) {
+        let me = this;
+
+        // read the input values from the main thread
+        // we could register an oninput event to this view as well and store the changes
+        Neo.main.DomAccess.getAttributes({
+            id        : me.getInputElId(),
+            attributes: 'value'
+        }).then(data => {
+            console.log(data);
+        });
     }
 }
 
