c86776e59a0f90ded61fda237cfc891721380856
tobiu <tobiasuhlig78@gmail.com>
2019-12-02 11:55:55 +0100

    Docs app: disable the examples tab for dist versions until #140 is resolved.

--- docs/app/view/MainContainer.mjs
+++ docs/app/view/MainContainer.mjs
@@ -110,6 +110,9 @@ class MainContainer extends Viewport {
                 keyProperty: 'id'
             });
         }
+
+        // Disable the examples Tab for build versions until the webpack builds can handle this (see: #140)
+        me.items[1].items[0].items[2].tabButtonConfig.disabled = !Neo.config.isExperimental;
     }
 
     /**
