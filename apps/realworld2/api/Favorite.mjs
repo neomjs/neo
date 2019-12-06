139adad54ebd6bb04c241da1697af870c40cd270
tobiu <tobiasuhlig78@gmail.com>
2019-12-06 11:32:34 +0100

    RW2: use the api classes from the first version #152

---
+++ apps/realworld2/api/Favorite.mjs
@@ -0,0 +1,43 @@
+import Base from './Base.mjs';
+
+/**
+ * @class RealWorld2.api.Favorite
+ * @extends RealWorld2.api.Base
+ */
+class Favorite extends Base {
+    static getConfig() {return {
+        /**
+         * @member {String} className='RealWorld2.api.Favorite'
+         * @private
+         */
+        className: 'RealWorld2.api.Favorite'
+    }}
+
+    /**
+     *
+     * @param {String} slug
+     */
+    add(slug) {
+        return this.post({
+            url: `/articles/${slug}/favorite`
+        });
+    }
+
+    /**
+     *
+     * @param {String} slug
+     */
+    remove(slug) {
+        return this.delete({
+            url: `/articles/${slug}/favorite`
+        });
+    }
+}
+
+Neo.applyClassConfig(Favorite);
+
+let instance = Neo.create(Favorite);
+
+Neo.applyToGlobalNs(instance);
+
+export default instance;
