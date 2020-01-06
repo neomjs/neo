606af8adfe3534b9e0020a55f3702dc857cba9a2
tobiu <tobiasuhlig78@gmail.com>
2020-01-06 13:07:50 +0100

    data.field.Integer: doc comments, beforeSetValue logic enhancement

--- src/data/field/Integer.mjs
+++ src/data/field/Integer.mjs
@@ -6,14 +6,34 @@ import Field from './Field.mjs';
  */
 class IntegerField extends Field {
     static getConfig() {return {
-        className : 'Neo.data.field.Integer',
-        ntype     : 'data-field-integer',
-        emptyValue: 0
+        /**
+         * @member {String} className='Neo.data.field.Integer'
+         * @private
+         */
+        className: 'Neo.data.field.Integer',
+        /**
+         * @member {String} ntype='data-field-integer'
+         * @private
+         */
+        ntype: 'data-field-integer',
+        /**
+         * @member {Number|null} defaultValue=0
+         */
+        defaultValue: 0
     }}
 
+    /**
+     * Triggered before the value config gets changed.
+     * @param {Array|String|null} value
+     * @param {Array|String|null} oldValue
+     * @returns {Array}
+     * @private
+     */
     beforeSetValue(value, oldValue) {
-        let emptyValue = this.nullableValue ? null : this.emptyValue;
-        return (value === null || value === undefined) ? emptyValue : parseInt(value, 10);
+        let me           = this,
+            defaultValue = me.defaultValue ? me.defaultValue : me.nullableValue ? null : undefined;
+
+        return (value === null || value === undefined) ? defaultValue : parseInt(value, 10);
     }
 }
 
