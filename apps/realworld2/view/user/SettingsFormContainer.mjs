8464c1428f865f75e13262ddbb7e8140e9425b46
tobiu <tobiasuhlig78@gmail.com>
2020-01-09 11:59:26 +0100

    RealWorld2.view.user.SettingsFormContainer: base class

---
+++ apps/realworld2/view/user/SettingsFormContainer.mjs
@@ -0,0 +1,56 @@
+import Container              from '../../../../src/form/Container.mjs';
+import Toolbar                from '../../../../src/container/Toolbar.mjs';
+import {default as TextField} from '../../../../src/form/field/Text.mjs';
+
+/**
+ * @class RealWorld2.view.user.SettingsFormContainer
+ * @extends Neo.form.Container
+ */
+class SettingsFormContainer extends Container {
+    static getConfig() {return {
+        /**
+         * @member {String} className='RealWorld2.view.user.SettingsFormContainer'
+         * @private
+         */
+        className: 'RealWorld2.view.user.SettingsFormContainer',
+        /**
+         * @member {Object} itemDefaults
+         */
+        itemDefaults: {
+            style: {
+                maxWidth: '500px'
+            }
+        },
+        /**
+         * @member {Array} items
+         */
+        items: [{
+            module       : TextField,
+            labelPosition: 'inline',
+            labelText    : 'URL of profile picture',
+            name         : 'image'
+        }, {
+            module       : TextField,
+            labelPosition: 'inline',
+            labelText    : 'Your Name',
+            name         : 'name'
+        }, {
+            module: Toolbar,
+            items : ['->', {
+                text: 'Submit'
+            }],
+            style: {
+                maxWidth: '500px',
+                padding : 0
+            }
+        }],
+        /**
+         * @member {Object} layout
+         */
+        layout: {ntype: 'base'}
+    }}
+}
+
+Neo.applyClassConfig(SettingsFormContainer);
+
+export {SettingsFormContainer as default};
