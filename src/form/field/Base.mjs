cbd42582254ec4a0a08e244f47d88e1061795529
tobiu <tobiasuhlig78@gmail.com>
2020-02-04 17:36:59 +0100

    form.field.Base: isValid() placeholder #232

--- src/form/field/Base.mjs
+++ src/form/field/Base.mjs
@@ -44,6 +44,14 @@ class Base extends Component {
     getSubmitValue() {
         return this.value;
     }
+
+    /**
+     *
+     * @return {Boolean}
+     */
+    isValid() {
+        return true;
+    }
 }
 
 /**
