ee4eefb2b137bcb263b9d3c67ed4d1570eceaa68
tobiu <tobiasuhlig78@gmail.com>
2020-02-20 17:39:14 +0100

    siesta/tests/ClassSystem.mjs: test enhancements

--- test/siesta/tests/ClassSystem.mjs
+++ test/siesta/tests/ClassSystem.mjs
@@ -1,46 +1,53 @@
 import Neo       from '../../../src/Neo.mjs';
 import * as core from '../../../src/core/_export.mjs';
 
-let valueA = 1,
-    valueB = 2,
-    instance;
-
 StartTest(t => {
     t.it('Module imports', t => {
         t.ok(Neo,       'Neo is imported as a JS module');
         t.ok(core.Base, 'core.Base is imported as a JS module');
     });
 
-    class TestClass extends core.Base {
-        static getConfig() {return {
-            /**
-             * @member {String} className='Neo.TestClass'
-             * @private
-             */
-            className: 'Neo.TestClass',
-            a_: valueA,
-            b_: valueB
-        }}
+    t.it('Class configs', t => {
+        let valueA = 1,
+            valueB = 2,
+            instance;
 
-        afterSetA(value, oldValue) {
-            t.isStrict(this.a, valueA, 'afterSetA: a equals ' + valueA);
-            t.isStrict(this.b, valueB, 'afterSetA: b equals ' + valueB);
+        t.diag('a=1, b=2');
+
+        class TestClass extends core.Base {
+            static getConfig() {return {
+                /**
+                 * @member {String} className='Neo.TestClass'
+                 * @private
+                 */
+                className: 'Neo.TestClass',
+
+                a_: valueA,
+                b_: valueB
+            }}
+
+            afterSetA(value, oldValue) {
+                t.isStrict(this.a, valueA, 'afterSetA: a equals ' + valueA);
+                t.isStrict(this.b, valueB, 'afterSetA: b equals ' + valueB);
+            }
+
+            afterSetB(value, oldValue) {
+                t.isStrict(this.a, valueA, 'afterSetB: a equals ' + valueA);
+                t.isStrict(this.b, valueB, 'afterSetB: b equals ' + valueB);
+            }
         }
 
-        afterSetB(value, oldValue) {
-            t.isStrict(this.a, valueA, 'afterSetB: a equals ' + valueA);
-            t.isStrict(this.b, valueB, 'afterSetB: b equals ' + valueB);
-        }
-    }
+        Neo.applyClassConfig(TestClass);
 
-    Neo.applyClassConfig(TestClass);
-
-    t.it('Create instance', t => {
         instance = Neo.create(TestClass, {
 
         });
 
+        t.diag('a=2');
         valueA = 2;
+        instance.a = valueA;
+
+        valueA = 3;
         valueB = 3;
 
         /*Object.assign(instance, { // this test will break => needs instance.set()
