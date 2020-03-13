9655ad046d4353d00c45c4494ba4900931c50d47
tobiu <tobiasuhlig78@gmail.com>
2020-03-13 19:22:55 +0100

    #267 GalleryMainContainer: removed the maxItems NumberField

--- examples/component/coronaGallery/GalleryMainContainer.mjs
+++ examples/component/coronaGallery/GalleryMainContainer.mjs
@@ -1,8 +1,7 @@
-import CountryGallery           from './CountryGallery.mjs';
-import {default as NumberField} from '../../../src/form/field/Number.mjs';
-import {default as Panel}       from '../../../src/container/Panel.mjs';
-import {default as RangeField}  from '../../../src/form/field/Range.mjs';
-import {default as Viewport}    from '../../../src/container/Viewport.mjs';
+import CountryGallery          from './CountryGallery.mjs';
+import {default as Panel}      from '../../../src/container/Panel.mjs';
+import {default as RangeField} from '../../../src/form/field/Range.mjs';
+import {default as Viewport}   from '../../../src/container/Viewport.mjs';
 
 /**
  * @class TestApp.GalleryMainContainer
@@ -113,15 +112,6 @@ class GalleryMainContainer extends Viewport {
                 minValue : 0,
                 maxValue : 100,
                 value    : 80
-            }, {
-                module   : NumberField,
-                clearable: false,
-                labelText: 'Max Items',
-                maxValue : 600,
-                minValue : 100,
-                name     : 'maxItems',
-                stepSize : 100,
-                value    : 300
             }, {
                 ntype       : 'button',
                 text        : 'Order by Row',
