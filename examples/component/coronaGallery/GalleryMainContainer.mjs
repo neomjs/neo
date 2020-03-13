4a8a10f73b6e87db91dc852aa619cdc032221ce6
tobiu <tobiasuhlig78@gmail.com>
2020-03-13 17:53:29 +0100

    #267 copying the gallery example into coronaGallery

---
+++ examples/component/coronaGallery/GalleryMainContainer.mjs
@@ -0,0 +1,222 @@
+import Gallery                 from '../../../src/component/Gallery.mjs';
+import {default as NumberField} from '../../../src/form/field/Number.mjs';
+import {default as Panel}      from '../../../src/container/Panel.mjs';
+import {default as RangeField} from '../../../src/form/field/Range.mjs';
+import {default as Viewport}   from '../../../src/container/Viewport.mjs';
+
+/**
+ * @class TestApp.GalleryMainContainer
+ * @extends Neo.tab.Container
+ */
+class GalleryMainContainer extends Viewport {
+    static getConfig() {return {
+        className: 'TestApp.MainContainer',
+        ntype: 'main-container',
+
+        autoMount: true,
+        /**
+         * @member {Neo.component.Gallery|null} gallery=null
+         */
+        gallery: null,
+        /**
+         * @member {Object|null} galleryConfig=null
+         */
+        galleryConfig: null,
+        layout: {ntype: 'hbox', align: 'stretch'},
+
+        items: [{
+            ntype : 'container',
+            flex  : 1,
+            layout: 'fit',
+            items : []
+        }, {
+            ntype : 'panel',
+            layout: {ntype: 'vbox', align: 'stretch'},
+            style : {backgroundColor: '#2b2b2b'},
+            width : 260,
+
+            itemDefaults: {
+                ntype        : 'rangefield',
+                flex         : '0 1 auto',
+                labelWidth   : '110px',
+                style        : {padding: '10px'},
+                useInputEvent: true,
+
+                listeners: {
+                    change: function(data) {
+                        if (this.name === 'opacity') {
+                            data.value /= 100;
+                        }
+                        Neo.get('neo-gallery-1')[this.name] = data.value;
+                    }
+                }
+            },
+
+            headers: [{
+                dock: 'top',
+                text: 'Gallery Controls'
+            }],
+
+            items: [{
+                labelText: 'Translate X',
+                maxValue : 5000,
+                minValue : 0,
+                name     : 'translateX',
+                value    : 0,
+                listeners: {
+                    change: function(data) {
+                        Neo.get('neo-gallery-1')[this.name] = data.value;
+                    },
+                    mounted: function(fieldId) {
+                        let field = Neo.get(fieldId);
+
+                        Neo.get('neo-gallery-1').on('changeTranslateX', function(value) {
+                            value = Math.min(Math.max(value, this.minValue), this.maxValue);
+                            this.value = value;
+                        }, field);
+                    }
+                }
+            }, {
+                labelText: 'Translate Y',
+                maxValue : 1500,
+                minValue : -1500,
+                name     : 'translateY',
+                value    : 0
+            }, {
+                labelText: 'Translate Z',
+                maxValue : 550,
+                minValue : -4500,
+                name     : 'translateZ',
+                value    : 0,
+                listeners: {
+                    change: function(data) {
+                        Neo.get('neo-gallery-1')[this.name] = data.value;
+                    },
+                    mounted: function(fieldId) {
+                        let field = Neo.get(fieldId);
+
+                        Neo.get('neo-gallery-1').on('changeTranslateZ', function(value) {
+                            value = Math.min(Math.max(value, this.minValue), this.maxValue);
+                            this.value = value;
+                        }, field);
+                    }
+                }
+            }, {
+                labelText: 'Amount Rows',
+                maxValue : 15,
+                minValue : 1,
+                name     : 'amountRows',
+                value    : 3
+            }, {
+                labelText: 'Opacity',
+                name     : 'maxOpacity',
+                minValue : 0,
+                maxValue : 100,
+                value    : 80
+            }, {
+                module   : NumberField,
+                clearable: false,
+                labelText: 'Max Items',
+                maxValue : 600,
+                minValue : 100,
+                name     : 'maxItems',
+                stepSize : 100,
+                value    : 300
+            }, {
+                ntype       : 'button',
+                text        : 'Order by Row',
+                listeners   : {},
+                style       : {margin: '20px'},
+                domListeners: {
+                    click: function() {
+                        const gallery    = Neo.get('neo-gallery-1'),
+                              orderByRow = !gallery.orderByRow;
+
+                        this.text = orderByRow === true ? 'Order By Column' : 'Order by Row';
+
+                        gallery.orderByRow = orderByRow;
+                    }
+                }
+            }, {
+                ntype    : 'button',
+                text     : 'Sort by Lastname',
+                listeners: {},
+                style    : {margin: '20px', marginBottom: '10px'},
+
+                domListeners: {
+                    click: function() {
+                        Neo.get('neo-gallery-1').store.sorters = [{
+                            property : 'lastname',
+                            direction: 'ASC'
+                        }, {
+                            property : 'firstname',
+                            direction: 'ASC'
+                        }];
+                    }
+                }
+            }, {
+                ntype    : 'button',
+                text     : 'Sort by Firstname',
+                listeners: {},
+                style    : {margin: '20px', marginTop: 0},
+
+                domListeners: {
+                    click: function() {
+                        Neo.get('neo-gallery-1').store.sorters = [{
+                            property : 'firstname',
+                            direction: 'ASC'
+                        }, {
+                            property : 'lastname',
+                            direction: 'ASC'
+                        }];
+                    }
+                }
+            }, {
+                ntype: 'label',
+                text : [
+                    '<b>Navigation Concept</b>',
+                    '<p>Click on an item to select it. Afterwards you can use the Arrow Keys to walk through the items.</p>'
+                ].join(''),
+
+                style: {
+                    backgroundColor: '#323232',
+                    color          : '#ddd',
+                    fontSize       : '13px',
+                    margin         : '10px',
+                    padding        : '10px',
+                    whiteSpace     : 'normal'
+                }
+            }]
+        }]
+    }}
+
+    /**
+     *
+     * @param {Object} config
+     */
+    constructor(config) {
+        super(config);
+
+        let me = this;
+
+        me.gallery = Neo.create({
+            module: Gallery,
+            id    : 'neo-gallery-1',
+            ...me.galleryConfig || {}
+        });
+
+        this.items[0].items.push(me.gallery);
+    }
+
+    /**
+     *
+     * @returns {Neo.data.Store}
+     */
+    getStore() {
+        return this.items[0].items[0].store;
+    }
+}
+
+Neo.applyClassConfig(GalleryMainContainer);
+
+export {GalleryMainContainer as default};
