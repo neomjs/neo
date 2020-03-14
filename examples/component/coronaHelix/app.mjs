e45d9cbbac12c52709fd874888b7d24dbedee269
tobiu <tobiasuhlig78@gmail.com>
2020-03-14 06:43:41 +0100

    #274 app paths, title

--- examples/component/coronaHelix/app.mjs
+++ examples/component/coronaHelix/app.mjs
@@ -2,7 +2,7 @@ import HelixMainContainer  from './HelixMainContainer.mjs';
 
 Neo.onStart = function() {
     Neo.app({
-        appPath : 'examples/component/helix/',
+        appPath : 'examples/component/coronaHelix/',
         mainView: HelixMainContainer,
         name    : 'TestApp'
     });
