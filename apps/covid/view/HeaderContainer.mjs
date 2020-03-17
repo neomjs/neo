4baf5ee824a1cbdafc1eae00d66de029282536e1
tobiu <tobiasuhlig78@gmail.com>
2020-03-18 00:16:23 +0100

    #282 Covid.view.HeaderContainer base class

---
+++ apps/covid/view/HeaderContainer.mjs
@@ -0,0 +1,29 @@
+import {default as Container} from '../../../src/container/Base.mjs';
+
+/**
+ * @class Covid.view.HeaderContainer
+ * @extends Neo.container.Base
+ */
+class HeaderContainer extends Container {
+    static getConfig() {return {
+        /**
+         * @member {String} className='Covid.view.HeaderContainer'
+         * @private
+         */
+        className: 'Covid.view.HeaderContainer',
+        /**
+         * @member {Object} layout={ntype: 'vbox', align: 'stretch'}
+         */
+        layout: {ntype: 'hbox', align: 'stretch'},
+        /**
+         * @member {Array} items
+         */
+        items: [{
+
+        }]
+    }}
+}
+
+Neo.applyClassConfig(HeaderContainer);
+
+export {HeaderContainer as default};
