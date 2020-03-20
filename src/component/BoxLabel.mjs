98892bb0ecc26553f414bf4077a3fbb0b5aa5e2e
tobiu <tobiasuhlig78@gmail.com>
2020-03-20 09:25:49 +0100

    Neo.component.BoxLabel

---
+++ src/component/BoxLabel.mjs
@@ -0,0 +1,29 @@
+import Label from './Label.mjs';
+
+/**
+ * Convenience class to render a bordered label with a text
+ * @class Neo.component.BoxLabel
+ * @extends Neo.component.Label
+ */
+class BoxLabel extends Label {
+    static getConfig() {return {
+        /**
+         * @member {String} className='Neo.component.BoxLabel'
+         * @private
+         */
+        className: 'Neo.component.BoxLabel',
+        /**
+         * @member {String} ntype='box-label'
+         * @private
+         */
+        ntype: 'box-label',
+        /**
+         * @member {String[]} cls=['neo-label']
+         */
+        cls: ['neo-box-label', 'neo-label']
+    }}
+}
+
+Neo.applyClassConfig(BoxLabel);
+
+export {BoxLabel as default};
