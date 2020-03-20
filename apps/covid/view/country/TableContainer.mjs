fe541918929f8adb13a4d816164fcee0ee9e9d22
tobiu <tobiasuhlig78@gmail.com>
2020-03-20 08:44:28 +0100

    Covid.view.country.TableContainer: width => 100% #328

--- apps/covid/view/country/TableContainer.mjs
+++ apps/covid/view/country/TableContainer.mjs
@@ -29,11 +29,14 @@ class TableContainer extends Container {
             dataField: 'country',
             text     : 'Country',
             renderer : function(value) {
-                return [
-                    '<div style="display: flex; align-items: center">',
-                        '<img style="height:20px; margin-right:10px; width:20px;" src="' + this.getCountryFlagUrl(value) + '">' + value,
-                    '</div>'
-                ].join('');
+                return {
+                    cls : ['neo-country-column', 'neo-table-cell'],
+                    html: [
+                        '<div style="display: flex; align-items: center">',
+                            '<img style="height:20px; margin-right:10px; width:20px;" src="' + this.getCountryFlagUrl(value) + '">' + value,
+                        '</div>'
+                    ].join('')
+                };
             }
         }, {
             align    : 'right',
