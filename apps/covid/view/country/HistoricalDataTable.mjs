2d33f2ca241445574f787f71024ff5bb85d91dff
tobiu <tobiasuhlig78@gmail.com>
2020-03-23 22:25:06 +0100

    Covid.view.country.HistoricalDataTable

---
+++ apps/covid/view/country/HistoricalDataTable.mjs
@@ -0,0 +1,65 @@
+import CountryStore from '../../store/Countries.mjs';
+import Container    from '../../../../src/table/Container.mjs';
+
+/**
+ * @class Covid.view.country.HistoricalDataTable
+ * @extends Neo.table.Container
+ */
+class HistoricalDataTable extends Container {
+    static getConfig() {return {
+        /**
+         * @member {String} className='Covid.view.country.HistoricalDataTable'
+         * @private
+         */
+        className: 'Covid.view.country.HistoricalDataTable',
+        /**
+         * @member {Array} cls=['covid-country-table', 'neo-table-container']
+         */
+        cls: ['covid-country-table', 'neo-table-container'],
+        /**
+         * @member {Boolean} createRandomData=false
+         */
+        createRandomData: false, // testing config
+        /**
+         * @member {Neo.data.Store} store=CountryStore
+         */
+        store: CountryStore,
+
+        columns: [{
+            dataField: 'date',
+            text     : 'Date',
+            renderer : function(value) {
+                return 'todo'; // parse date
+            }
+        }, {
+            align    : 'right',
+            dataField: 'cases',
+            text     : 'Cases'
+        }, {
+            align    : 'right',
+            dataField: 'deaths',
+            text     : 'Deaths',
+            renderer : function(value) {
+                return `<span style="color:red;">${value}</span>`;
+            }
+        }, {
+            align    : 'right',
+            dataField: 'critical',
+            text     : 'Critical',
+            renderer : function(value) {
+                return `<span style="color:orange;">${value}</span>`;
+            }
+        }, {
+            align    : 'right',
+            dataField: 'recovered',
+            text     : 'Recovered',
+            renderer : function(value) {
+                return `<span style="color:green;">${value}</span>`;
+            }
+        }]
+    }}
+}
+
+Neo.applyClassConfig(HistoricalDataTable);
+
+export {HistoricalDataTable as default};
