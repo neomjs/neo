6b9e0798435c9e34176bdea98fd65112de623124
tobiu <tobiasuhlig78@gmail.com>
2020-03-23 21:54:45 +0100

    Covid.store.HistoricalData

---
+++ apps/covid/store/HistoricalData.mjs
@@ -0,0 +1,24 @@
+import HistoricalData from '../model/HistoricalData.mjs';
+import Store          from '../../../src/data/Store.mjs';
+
+/**
+ * @class Covid.store.HistoricalData
+ * @extends Neo.data.Store
+ */
+class HistoricalData extends Store {
+    static getConfig() {return {
+        className: 'Covid.store.HistoricalData',
+
+        keyProperty: 'date',
+        model      : HistoricalData,
+
+        sorters: [{
+            property : 'date',
+            direction: 'DESC'
+        }]
+    }}
+}
+
+Neo.applyClassConfig(HistoricalData);
+
+export {HistoricalData as default};
