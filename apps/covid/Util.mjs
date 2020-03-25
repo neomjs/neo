90a845fe215555a017d44743f8c4cb4a4d4c2778
tobiu <tobiasuhlig78@gmail.com>
2020-03-25 13:11:37 +0100

    Covid Dashboard: Number Formatter #369 => util class

---
+++ apps/covid/Util.mjs
@@ -0,0 +1,40 @@
+import Base from '../../src/core/Base.mjs';
+
+/**
+ * @class Covid.Util
+ * @extends Neo.core.Base
+ */
+class Util extends Base {
+    static getStaticConfig() {return {
+        /**
+         * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString
+         * Change this config to enforce a county specific formatting (e.g. 'de-DE')
+         * @member {String} locales='default'
+         * @private
+         * @static
+         */
+        locales: 'default'
+    }}
+
+    static getConfig() {return {
+        /**
+         * @member {String} className='Covid.Util'
+         * @private
+         */
+        className: 'Covid.Util'
+    }}
+
+    /**
+     *
+     * @param {Number} value
+     * @return {String}
+     */
+    static formatNumber(value) {
+        console.log(Util.locales);
+        return value.toLocaleString(Util.locales);
+    }
+}
+
+Neo.applyClassConfig(Util);
+
+export default Util;
