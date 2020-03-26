be78cb719d62b3a1ea83e291f97d375ebb0968ce
tobiu <tobiasuhlig78@gmail.com>
2020-03-27 00:09:34 +0100

    Covid Dashboard: app.mjs => arrow function

--- apps/covid/app.mjs
+++ apps/covid/app.mjs
@@ -1,9 +1,7 @@
 import MainContainer from './view/MainContainer.mjs';
 
-Neo.onStart = function() {
-    Neo.app({
-        appPath : 'apps/covid/',
-        mainView: MainContainer,
-        name    : 'Covid'
-    });
-};
+Neo.onStart = () => Neo.app({
+    appPath : 'apps/covid/',
+    mainView: MainContainer,
+    name    : 'Covid'
+});
