a76fe142ac4e73d645e28edc6882ee6c1beba1f6
tobiu <tobiasuhlig78@gmail.com>
2020-05-01 11:06:38 +0200

    #516 webpack.config.main.js

---
+++ buildScripts/webpack/production/webpack.config.main.js
@@ -0,0 +1,22 @@
+const fs          = require('fs-extra'),
+      path        = require('path'),
+      processRoot = process.cwd(),
+      packageJson = JSON.parse(fs.readFileSync(path.resolve(processRoot, 'package.json'), 'utf8')),
+      neoPath     = packageJson.name === 'neo.mjs' ? './' : './node_modules/neo.mjs/',
+      config      = JSON.parse(fs.readFileSync(path.resolve(neoPath, 'buildScripts/webpack/production/build.json')), 'utf8'),
+      entry       = {main: path.resolve(neoPath, config.mainInput)};
+
+module.exports = {
+    mode  : 'production',
+    entry : entry,
+    target: 'web',
+
+    output: {
+        filename: (chunkData) => {
+            if (chunkData.chunk.name === 'main') {
+                return config.mainOutput;
+            }
+        },
+        path: path.resolve(processRoot, config.buildFolder)
+    }
+};
