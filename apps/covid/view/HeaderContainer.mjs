dc572c5de463fc28c3716885e7e3ece2c5521be6
tobiu <tobiasuhlig78@gmail.com>
2020-05-02 11:05:34 +0200

    Covid.view.HeaderContainer: increased the gh stars button width (might just vary on different content => 111 VS 888)

--- apps/covid/view/HeaderContainer.mjs
+++ apps/covid/view/HeaderContainer.mjs
@@ -88,7 +88,7 @@ class HeaderContainer extends Container {
                     flex: 1
                 }, {
                     style: {padding: '10px'},
-                    width: 110,
+                    width: 115,
                     vdom : {
                         cn: [{
                             tag              : 'a',
