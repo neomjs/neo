baa0cee253dc371804d58d90a7a1d179529e9825
tobiu <tobiasuhlig78@gmail.com>
2020-05-14 16:07:57 +0200

    RW1: app.mjs => typo fix for Neo.onStart()

--- apps/realworld/app.mjs
+++ apps/realworld/app.mjs
@@ -1,6 +1,6 @@
 import MainContainer from './view/MainContainer.mjs';
 
-Neo.onStart = Neo.app({
+Neo.onStart = () => Neo.app({
     appPath : 'apps/realworld/',
     mainView: MainContainer,
     name    : 'RealWorld'
