fca5ecff6856325ecb4a0051becf707640aa666f
tobiu <tobiasuhlig78@gmail.com>
2020-05-19 22:30:24 +0200

    Siesta: update the docs test #570

--- test/siesta/docs/tests/Main.mjs
+++ test/siesta/docs/tests/Main.mjs
@@ -1,7 +1,10 @@
 import Main from '../../../../src/Main.mjs';
-
+console.log(Main);
 StartTest(t => {
     t.it("Sanity", t => {
         t.ok(Main, 'Main is imported as a JS module');
+    },
+    next => {
+        t.done();
     });
 });
