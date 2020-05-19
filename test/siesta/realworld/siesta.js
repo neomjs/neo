12fdc537ebb6f0f17578090d20c733e26daa8ff6
tobiu <tobiasuhlig78@gmail.com>
2020-05-19 22:19:25 +0200

    Siesta: update the RW1 tests #569

--- test/siesta/realworld/siesta.js
+++ test/siesta/realworld/siesta.js
@@ -23,14 +23,15 @@ project.plan(
                     "Neo = self.Neo || {};",
                     "Neo.config = Neo.config || {};",
                     "Object.assign(Neo.config, {",
-                        "appPath       : 'apps/realworld/app.mjs',",
-                        "basePath      : '../../../',",
-                        "environment   : 'development',",
-                        "isExperimental: true,",
-                        "isInsideSiesta: true,",
-                        "themes        : [],",
-                        "useFontAwesome: false,",
-                        "workerBasePath: '../../../src/worker/'",
+                        "appPath         : 'apps/realworld/app.mjs',",
+                        "basePath        : '../../../',",
+                        "environment     : 'development',",
+                        "isExperimental  : true,",
+                        "isInsideSiesta  : true,",
+                        "mainThreadAddons: ['LocalStorage', 'Markdown'],",
+                        "themes          : [],",
+                        "useFontAwesome  : false,",
+                        "workerBasePath  : '../../../src/worker/'",
                     "});"
                 ].join("")
             }
