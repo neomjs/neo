30f9429ebf420f7558775a881014204ff5febb39
tobiu <tobiasuhlig78@gmail.com>
2020-06-05 13:20:23 +0200

    Create a new App shell for the covid app using the createApp program #675

---
+++ apps/sharedcovid/app.mjs
@@ -0,0 +1,9 @@
+import MainContainer from './MainContainer.mjs';
+
+Neo.onStart = function() {
+    Neo.app({
+        appPath : 'apps/sharedcovid/',
+        mainView: MainContainer,
+        name    : 'SharedCovid'
+    });
+};
