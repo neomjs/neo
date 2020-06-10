b3bfa8e8faf7a85f408bddafd80fb7df2b023b65
tobiu <tobiasuhlig78@gmail.com>
2020-06-10 22:00:54 +0200

    sharedcovid2: adjust the appPath #701

--- apps/sharedcovid2/app.mjs
+++ apps/sharedcovid2/app.mjs
@@ -2,7 +2,7 @@ import MainContainer from './MainContainer.mjs';
 
 Neo.onStart = function() {
     Neo.app({
-        appPath : 'apps/covid2/',
+        appPath : 'apps/sharedcovid2/',
         mainView: MainContainer,
         name    : 'Covid2'
     });
