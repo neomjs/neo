48382c5cd198ea536df1cf9f93be97468168c7fa
tobiu <tobiasuhlig78@gmail.com>
2020-06-10 21:48:47 +0200

    Modify the sharedcovid2 app to only contain an empty viewport #700

--- apps/sharedcovid2/MainContainer.mjs
+++ apps/sharedcovid2/MainContainer.mjs
@@ -1,6 +1,4 @@
-import {default as Component}    from '../../src/component/Base.mjs';
-import {default as TabContainer} from '../../src/tab/Container.mjs';
-import Viewport                  from '../../src/container/Viewport.mjs';
+import Viewport from '../../src/container/Viewport.mjs';
 
 /**
  * @class Covid2.MainContainer
@@ -12,34 +10,7 @@ class MainContainer extends Viewport {
         ntype    : 'main-container',
 
         autoMount: true,
-        layout   : {ntype: 'fit'},
-
-        items: [{
-            module: TabContainer,
-            height: 300,
-            width : 500,
-            style : {flex: 'none', margin: '20px'},
-
-            itemDefaults: {
-                module: Component,
-                cls   : ['neo-examples-tab-component'],
-                style : {padding: '20px'},
-            },
-
-            items: [{
-                tabButtonConfig: {
-                    iconCls: 'fa fa-home',
-                    text   : 'Tab 1'
-                },
-                vdom: {innerHTML: 'Welcome to your new Neo App.'}
-            }, {
-                tabButtonConfig: {
-                    iconCls: 'fa fa-play-circle',
-                    text   : 'Tab 2'
-                },
-                vdom: {innerHTML: 'Have fun creating something awesome!'}
-            }]
-        }]
+        layout   : {ntype: 'fit'}
     }}
 }
 
