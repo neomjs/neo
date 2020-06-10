44606b9e3b714fb6d960c3d0bc8c628c0b10c2b8
tobiu <tobiasuhlig78@gmail.com>
2020-06-10 22:21:08 +0200

    sharedcovid apps: app.mjs cleanup

--- apps/sharedcovid2/app.mjs
+++ apps/sharedcovid2/app.mjs
@@ -1,9 +1,7 @@
 import MainContainer from './MainContainer.mjs';
 
-Neo.onStart = function() {
-    Neo.app({
-        appPath : 'apps/sharedcovid2/',
-        mainView: MainContainer,
-        name    : 'Covid2'
-    });
-};
+Neo.onStart = Neo.app({
+    appPath : 'apps/sharedcovid2/',
+    mainView: MainContainer,
+    name    : 'Covid2'
+});
