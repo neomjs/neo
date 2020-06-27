4df275bd1f05884f46a63c78fd6ec5becde506a8
tobiu <tobiasuhlig78@gmail.com>
2020-06-27 17:18:03 +0200

    neo website: model.BlogPost base class #790

---
+++ apps/website/model/BlogPost.mjs
@@ -0,0 +1,48 @@
+import Model  from '../../../src/data/Model.mjs';
+
+/**
+ * @class Website.model.BlogPost
+ * @extends Neo.data.Model
+ */
+class BlogPost extends Model {
+    static getConfig() {return {
+        /**
+         * @member {String} className='Website.model.BlogPost'
+         * @protected
+         */
+        className: 'BlogPost.model.BlogPost',
+        /**
+         * @member {Object[]} fields
+         * @protected
+         */
+        fields: [{
+            name: 'author',
+            type: 'String'
+        }, {
+            name: 'id',
+            type: 'Integer'
+        }, {
+            name: 'image',
+            type: 'String'
+        }, {
+            name: 'name',
+            type: 'String'
+        }, {
+            name: 'provider',
+            type: 'String'
+        }, {
+            name: 'selectedInto',
+            type: 'Array'
+        }, {
+            name: 'type',
+            type: 'String'
+        }, {
+            name: 'url',
+            type: 'String'
+        }]
+    }}
+}
+
+Neo.applyClassConfig(BlogPost);
+
+export {BlogPost as default};
