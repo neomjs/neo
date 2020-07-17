2c017c06eb5eb8e814457b048d86002e8f225f4e
tobiu <tobiasuhlig78@gmail.com>
2020-07-17 19:27:49 +0200

    Neo.calendar.view.WeekComponent: base class

---
+++ src/calendar/view/WeekComponent.mjs
@@ -0,0 +1,28 @@
+import {default as Component}     from '../../component/Base.mjs';
+
+/**
+ * @class Neo.calendar.view.WeekComponent
+ * @extends Neo.container.Base
+ */
+class WeekComponent extends Component {
+    static getConfig() {return {
+        /**
+         * @member {String} className='Neo.calendar.view.WeekComponent'
+         * @protected
+         */
+        className: 'Neo.calendar.view.WeekComponent',
+        /**
+         * @member {String} ntype='calendar-view-weekComponent'
+         * @protected
+         */
+        ntype: 'calendar-view-weekComponent',
+        /**
+         * @member {String[]} cls=['neo-calendar-weekcomponent']
+         */
+        cls: ['neo-calendar-weekcomponent'],
+    }}
+}
+
+Neo.applyClassConfig(WeekComponent);
+
+export {WeekComponent as default};
