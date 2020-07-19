07097bd31fb720abe2af9f463f86574cfa2c007c
tobiu <tobiasuhlig78@gmail.com>
2020-07-19 17:11:59 +0200

    CalendarBasic.MainContainer: header

--- examples/calendar/basic/MainContainer.mjs
+++ examples/calendar/basic/MainContainer.mjs
@@ -1,4 +1,6 @@
+import Button                from '../../../src/component/Button.mjs';
 import {default as Calendar} from '../../../src/calendar/MainContainer.mjs';
+import Toolbar               from '../../../src/container/Toolbar.mjs';
 import Viewport              from '../../../src/container/Viewport.mjs';
 
 /**
@@ -14,9 +16,20 @@ class MainContainer extends Viewport {
         layout   : {ntype: 'vbox', align: 'stretch'},
 
         items: [{
-            ntype : 'container',
-            height: 100,
-            style : {backgroundColor: '#ddd'}
+            module : Toolbar,
+            flex   : 'none',
+            padding: 20,
+
+            style: {
+                backgroundColor: '#f2f2f2',
+                padding        : '10px 5px 10px 10px'
+            },
+
+            items: ['->', {
+                module : Button,
+                iconCls: 'fa fa-moon',
+                text   : 'Theme Dark'
+            }]
         }, {
             module: Calendar,
             flex  : 1
