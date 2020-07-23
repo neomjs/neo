ad999ce11e8e911878d03d24732f756ee1200964
tobiu <tobiasuhlig78@gmail.com>
2020-07-24 01:05:40 +0200

    Neo.calendar.view.SettingsContainer: base class

---
+++ src/calendar/view/SettingsContainer.mjs
@@ -0,0 +1,50 @@
+import {default as Container} from '../../container/Base.mjs';
+
+/**
+ * @class Neo.calendar.view.SettingsContainer
+ * @extends Neo.container.Base
+ */
+class SettingsContainer extends Container {
+    static getConfig() {return {
+        /**
+         * @member {String} className='Neo.calendar.view.SettingsContainer'
+         * @protected
+         */
+        className: 'Neo.calendar.view.SettingsContainer',
+        /**
+         * @member {String} ntype='calendar-settingscontainer'
+         * @protected
+         */
+        ntype: 'calendar-settingscontainer',
+        /**
+         * @member {String[]} cls=['neo-calendar-settingscontainer', 'neo-container']
+         */
+        cls: ['neo-calendar-settingscontainer', 'neo-container'],
+        /**
+         * @member {Object} layout={ntype:'vbox',align:'stretch'}
+         * @protected
+         */
+        layout: {ntype: 'vbox', align: 'stretch'}
+    }}
+
+    /**
+     *
+     * @param config
+     */
+    constructor(config) {
+        super(config);
+
+        let me = this;
+
+        me.items = [{
+            ntype: 'component',
+            vdom : {
+                html: 'Settings'
+            }
+        }]
+    }
+}
+
+Neo.applyClassConfig(SettingsContainer);
+
+export {SettingsContainer as default};
