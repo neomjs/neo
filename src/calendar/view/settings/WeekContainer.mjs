4446891741a1c5a08fab9129017864717f5b66c6
tobiu <tobiasuhlig78@gmail.com>
2020-07-24 16:51:18 +0200

    Neo.calendar.view.settings.WeekContainer: itemDefaults

--- src/calendar/view/settings/WeekContainer.mjs
+++ src/calendar/view/settings/WeekContainer.mjs
@@ -15,6 +15,14 @@ class WeekContainer extends Container {
          */
         className: 'Neo.calendar.view.settings.WeekContainer',
         /**
+         * @member {Object} itemDefaults
+         */
+        itemDefaults: {
+            flex      : 'none',
+            labelWidth: 130,
+            style     : {marginTop: '5px'}
+        },
+        /**
          * @member {Object} layout={ntype:'vbox',align:'stretch'}
          */
         layout: {ntype: 'vbox', align: 'stretch'}
@@ -31,75 +39,59 @@ class WeekContainer extends Container {
             weekComponent = me.getWeekComponent();
 
         me.items = [{
-            module    : TimeField,
-            clearable : false,
-            flex      : 'none',
-            labelText : 'Start Time',
-            labelWidth: 130,
-            listeners : {change: me.onStartTimeChange, scope: me},
-            maxValue  : '10:00',
-            minValue  : '00:00',
-            stepSize  : 60 * 60, // 1h
-            value     : '00:00'
+            module   : TimeField,
+            clearable: false,
+            labelText: 'Start Time',
+            listeners: {change: me.onStartTimeChange, scope: me},
+            maxValue : '10:00',
+            minValue : '00:00',
+            stepSize : 60 * 60, // 1h
+            style    : {},
+            value    : '00:00'
         }, {
-            module    : TimeField,
-            flex      : 'none',
-            labelText : 'End Time',
-            labelWidth: 130,
-            listeners : {change: me.onEndTimeChange, scope: me},
-            maxValue  : '23:00',
-            minValue  : '14:00',
-            stepSize  : 60 * 60, // 1h
-            style     : {marginTop: '5px'}
+            module   : TimeField,
+            labelText: 'End Time',
+            listeners: {change: me.onEndTimeChange, scope: me},
+            maxValue : '23:00',
+            minValue : '14:00',
+            stepSize : 60 * 60 // 1h
         }, {
             module        : NumberField,
             clearable     : false,
             excludedValues: [45],
-            flex          : 'none',
             inputEditable : false,
             labelText     : 'Interval',
-            labelWidth    : 130,
             listeners     : {change: me.onIntervalChange, scope: me},
             maxValue      : 60,
             minValue      : 15,
             stepSize      : 15,
-            style         : {marginTop: '5px'},
             value         : 30
         }, {
-            module    : NumberField,
-            clearable : false,
-            flex      : 'none',
-            labelText : 'Row Height',
-            labelWidth: 130,
-            listeners : {change: me.onRowHeightChange, scope: me},
-            maxValue  : 100,
-            minValue  : 8,
-            stepSize  : 2,
-            style     : {marginTop: '5px'},
-            value     : 20
+            module   : NumberField,
+            clearable: false,
+            labelText: 'Row Height',
+            listeners: {change: me.onRowHeightChange, scope: me},
+            maxValue : 100,
+            minValue : 8,
+            stepSize : 2,
+            value    : 20
         }, {
             module        : RadioField,
             checked       : weekComponent.timeAxisPosition === 'start',
             fieldValue    : 'start',
-            flex          : 'none',
             hideValueLabel: false,
             labelText     : 'timeAxisPosition',
-            labelWidth    : 130,
             listeners     : {change: me.onTimeAxisPositionChange, scope: me},
             name          : 'timeAxisPosition',
-            style         : {marginTop: '5px'},
             valueLabelText: 'start'
         }, {
             module        : RadioField,
             checked       : weekComponent.timeAxisPosition === 'end',
             fieldValue    : 'end',
-            flex          : 'none',
             hideValueLabel: false,
             labelText     : '',
-            labelWidth    : 130,
             listeners     : {change: me.onTimeAxisPositionChange, scope: me},
             name          : 'timeAxisPosition',
-            style         : {marginTop: '5px'},
             valueLabelText: 'end'
         }];
     }
