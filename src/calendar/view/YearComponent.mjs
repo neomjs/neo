9e1515dcdb704b90fa741bf17a3c59f16bf5aae4
tobiu <tobiasuhlig78@gmail.com>
2020-07-25 16:45:21 +0200

    Neo.calendar.view.YearComponent: dayNameFormat_

--- src/calendar/view/YearComponent.mjs
+++ src/calendar/view/YearComponent.mjs
@@ -21,6 +21,12 @@ class YearComponent extends Component {
          */
         cls: ['neo-calendar-yearcomponent'],
         /**
+         * The format of the column headers.
+         * Valid values are: narrow, short & long
+         * @member {String} dayNameFormat_='narrow'
+         */
+        dayNameFormat_: 'narrow',
+        /**
          * @member {Neo.calendar.store.Events|null} eventStore_=null
          */
         eventStore_: null,
@@ -35,7 +41,12 @@ class YearComponent extends Component {
                 cls: ['neo-months-container'],
                 cn : []
             }]
-        }
+        },
+        /**
+         * 0-6 => Sun-Sat
+         * @member {Number} weekStartDay_=0
+         */
+        weekStartDay_: 0
     }}
 
     /**
@@ -50,6 +61,33 @@ class YearComponent extends Component {
     /**
      *
      */
+    createDayNamesRow() {
+        let me   = this,
+            date = me.currentDate, // cloned
+            i    = 0,
+            row  = {cls: ['neo-calendar-week'], cn: [{cls: ['neo-top-left-spacer']}]};
+
+        date.setDate(me.currentDate.getDate() - me.currentDate.getDay() + me.weekStartDay);
+
+        const dt = new Intl.DateTimeFormat(Neo.config.locale, {
+            weekday: me.dayNameFormat
+        });
+
+        for (; i < 7; i++) {
+            row.cn.push({
+                cls : ['neo-weekday-cell'],
+                html: dt.format(date)
+            });
+
+            date.setDate(date.getDate() + 1);
+        }
+
+        return row;
+    }
+
+    /**
+     *
+     */
     createMonths() {
         let me             = this,
             dt             = new Intl.DateTimeFormat(Neo.config.locale, {month: 'long'}),
@@ -58,9 +96,7 @@ class YearComponent extends Component {
             vdom           = me.vdom,
             monthContainer = vdom.cn[1],
             i              = 0,
-            len            = 12,
-            weekLen        = 8,
-            j;
+            len            = 12;
 
         for (; i < len; i++) {
             currentDate.setMonth(i);
@@ -68,23 +104,14 @@ class YearComponent extends Component {
 
             monthContainer.cn.push({
                 cls: ['neo-month'],
-                cn : [{
-                    cls : ['neo-month-name'],
-                    html: currentMonth
-                }, {
-                    cls: ['neo-calendar-week'],
-                    cn : [{
-                        cls: ['neo-top-left-spacer']
-                    }]
-                }]
+                cn : [
+                    {
+                        cls : ['neo-month-name'],
+                        html: currentMonth
+                    },
+                    me.createDayNamesRow()
+                ]
             });
-
-            for (j=0; j < 7; j++) {
-                monthContainer.cn[i].cn[1].cn.push({
-                    cls : ['neo-weekday-cell'],
-                    html: j
-                });
-            }
         }
 
         me.vdom = vdom;
