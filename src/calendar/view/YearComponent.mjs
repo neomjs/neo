fc7055080f03df35f1be2eda9c2dc9c7d02502bb
tobiu <tobiasuhlig78@gmail.com>
2020-07-27 22:59:47 +0200

    Neo.calendar.view.YearComponent: cleanup

--- src/calendar/view/YearComponent.mjs
+++ src/calendar/view/YearComponent.mjs
@@ -317,9 +317,9 @@ class YearComponent extends Component {
                                 }]
                             }],
                             style: {
-                                height   : 2 * data.height + 'px',
+                                height   : `${2 * data.height}px`,
                                 transform: `translateY(${y}px)`,
-                                width    : data.width + 'px'
+                                width    : `${data.width}px`
                             }
                         }]
                     });
