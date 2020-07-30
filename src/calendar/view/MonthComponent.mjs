221a403b5734308934ae728773148f7c4f397875
tobiu <tobiasuhlig78@gmail.com>
2020-07-30 13:11:14 +0200

    Neo.calendar.view.MonthComponent: beforeSetDayNameFormat()

--- src/calendar/view/MonthComponent.mjs
+++ src/calendar/view/MonthComponent.mjs
@@ -223,6 +223,16 @@ class MonthComponent extends Component {
     }
 
     /**
+     * Triggered before the dayNameFormat config gets changed
+     * @param {String} value
+     * @param {String} oldValue
+     * @protected
+     */
+    beforeSetDayNameFormat(value, oldValue) {
+        return this.beforeSetEnumValue(value, oldValue, 'dayNameFormat', DateUtil.prototype.dayNameFormats);
+    }
+
+    /**
      * Triggered before the monthNameFormat config gets changed
      * @param {String} value
      * @param {String} oldValue
