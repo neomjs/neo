10a152ba815274c6eb63c6977525a3dc38fe0003
tobiu <tobiasuhlig78@gmail.com>
2020-08-07 00:23:59 +0200

    TableFiltering.MainContainer: editor checkbox styling

--- examples/tableFiltering/MainContainer.mjs
+++ examples/tableFiltering/MainContainer.mjs
@@ -66,7 +66,19 @@ class MainContainer extends Viewport {
             }, {
                 text     : 'Online',
                 dataField: 'isOnline',
-                renderer : data => `<i class="fa fa-${data.value ? 'check' : 'times'}"></i>`
+                renderer : data => `<i class="fa fa-${data.value ? 'check' : 'times'}"></i>`,
+
+                editorFieldConfig: {
+                    module: CheckBox,
+
+                    style: { // todo => scss
+                        alignItems    : 'center',
+                        display       : 'flex',
+                        height        : '37px',
+                        justifyContent: 'start',
+                        marginLeft    : '5px'
+                    }
+                }
             }]
         }]
     }}
