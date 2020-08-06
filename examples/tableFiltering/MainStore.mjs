da7f39dbe64ef084f4c29793e984008852a653b5
tobiu <tobiasuhlig78@gmail.com>
2020-08-06 20:18:02 +0200

    TableFiltering.MainStore: cleanup, more testing data

--- examples/tableFiltering/MainStore.mjs
+++ examples/tableFiltering/MainStore.mjs
@@ -13,37 +13,18 @@ class MainStore extends Store {
         keyProperty: 'githubId',
         model      : Model,
 
-        data: [{
-            country  : 'Germany',
-            firstname: 'Tobias',
-            githubId : 'tobiu',
-            lastname : 'Uhlig'
-        }, {
-            country  : 'USA',
-            firstname: 'Rich',
-            githubId : 'rwaters',
-            lastname : 'Waters'
-        }, {
-            country  : 'Germany',
-            firstname: 'Nils',
-            githubId : 'mrsunshine',
-            lastname : 'Dehl'
-        }, {
-            country  : 'USA',
-            firstname: 'Gerard',
-            githubId : 'camtnbikerrwc',
-            lastname : 'Horan'
-        }, {
-            country  : 'Slovakia',
-            firstname: 'Jozef',
-            githubId : 'jsakalos',
-            lastname : 'Sakalos'
-        }, {
-            country  : 'Germany',
-            firstname: 'Bastian',
-            githubId : 'bhaustein',
-            lastname : 'Haustein'
-        }]
+        data: [
+            {country: 'Germany',  firstname: 'Tobias',  githubId: 'tobiu',         lastname: 'Uhlig'},
+            {country: 'USA',      firstname: 'Rich',    githubId: 'rwaters',       lastname: 'Waters'},
+            {country: 'Germany',  firstname: 'Nils',    githubId: 'mrsunshine',    lastname: 'Dehl'},
+            {country: 'USA',      firstname: 'Gerard',  githubId: 'camtnbikerrwc', lastname: 'Horan'},
+            {country: 'Slovakia', firstname: 'Jozef',   githubId: 'jsakalos',      lastname: 'Sakalos'},
+            {country: 'Germany',  firstname: 'Bastian', githubId: 'bhaustein',     lastname: 'Haustein'},
+            {country: 'USA',      firstname: 'Durlabh', githubId: 'durlabhjain',   lastname: 'Jain'},
+            {country: 'Canada',   firstname: 'Kevin',   githubId: 'keckeroo',      lastname: 'Cassidy'},
+            {country: 'UK',       firstname: 'Nikola',  githubId: 'boemska-nik',   lastname: 'Markovic'},
+            {country: 'USA',      firstname: 'Hyle',    githubId: 'hylec',         lastname: 'Campbell'}
+        ]
     }}
 }
 
