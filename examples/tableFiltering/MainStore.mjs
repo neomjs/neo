9c010802a463b7fcd666f8508a5a530a5fac0fe0
tobiu <tobiasuhlig78@gmail.com>
2020-08-09 16:08:42 +0200

    TableFiltering.MainStore: USA => United States (to match countries.json)

--- examples/tableFiltering/MainStore.mjs
+++ examples/tableFiltering/MainStore.mjs
@@ -14,16 +14,16 @@ class MainStore extends Store {
         model      : Model,
 
         data: [
-            {country: 'Germany',  firstname: 'Tobias',  githubId: 'tobiu',         isOnline: true,  lastname: 'Uhlig'},
-            {country: 'USA',      firstname: 'Rich',    githubId: 'rwaters',       isOnline: false, lastname: 'Waters'},
-            {country: 'Germany',  firstname: 'Nils',    githubId: 'mrsunshine',    isOnline: true,  lastname: 'Dehl'},
-            {country: 'USA',      firstname: 'Gerard',  githubId: 'camtnbikerrwc', isOnline: true,  lastname: 'Horan'},
-            {country: 'Slovakia', firstname: 'Jozef',   githubId: 'jsakalos',      isOnline: false, lastname: 'Sakalos'},
-            {country: 'Germany',  firstname: 'Bastian', githubId: 'bhaustein',     isOnline: false, lastname: 'Haustein'},
-            {country: 'USA',      firstname: 'Durlabh', githubId: 'durlabhjain',   isOnline: true,  lastname: 'Jain'},
-            {country: 'Canada',   firstname: 'Kevin',   githubId: 'keckeroo',      isOnline: true,  lastname: 'Cassidy'},
-            {country: 'UK',       firstname: 'Nikola',  githubId: 'boemska-nik',   isOnline: true,  lastname: 'Markovic'},
-            {country: 'USA',      firstname: 'Hyle',    githubId: 'hylec',         isOnline: false, lastname: 'Campbell'}
+            {country: 'Germany',       firstname: 'Tobias',  githubId: 'tobiu',         isOnline: true,  lastname: 'Uhlig'},
+            {country: 'United States', firstname: 'Rich',    githubId: 'rwaters',       isOnline: false, lastname: 'Waters'},
+            {country: 'Germany',       firstname: 'Nils',    githubId: 'mrsunshine',    isOnline: true,  lastname: 'Dehl'},
+            {country: 'United States', firstname: 'Gerard',  githubId: 'camtnbikerrwc', isOnline: true,  lastname: 'Horan'},
+            {country: 'Slovakia',      firstname: 'Jozef',   githubId: 'jsakalos',      isOnline: false, lastname: 'Sakalos'},
+            {country: 'Germany',       firstname: 'Bastian', githubId: 'bhaustein',     isOnline: false, lastname: 'Haustein'},
+            {country: 'United States', firstname: 'Durlabh', githubId: 'durlabhjain',   isOnline: true,  lastname: 'Jain'},
+            {country: 'Canada',        firstname: 'Kevin',   githubId: 'keckeroo',      isOnline: true,  lastname: 'Cassidy'},
+            {country: 'UK',            firstname: 'Nikola',  githubId: 'boemska-nik',   isOnline: true,  lastname: 'Markovic'},
+            {country: 'United States', firstname: 'Hyle',    githubId: 'hylec',         isOnline: false, lastname: 'Campbell'}
         ]
     }}
 }
