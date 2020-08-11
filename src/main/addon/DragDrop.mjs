f696918bfc4ff555e50bbd05b9b88a857ab474fc
tobiu <tobiasuhlig78@gmail.com>
2020-08-11 11:54:43 +0200

    main.addon.DragDrop: base class

---
+++ src/main/addon/DragDrop.mjs
@@ -0,0 +1,38 @@
+import Base from '../../core/Base.mjs';
+
+/**
+ * @class Neo.main.addon.DragDrop
+ * @extends Neo.core.Base
+ * @singleton
+ */
+class DragDrop extends Base {
+    static getConfig() {
+        return {
+            /**
+             * @member {String} className='Neo.main.addon.DragDrop'
+             * @protected
+             */
+            className: 'Neo.main.addon.DragDrop',
+            /**
+             * @member {Boolean} singleton=true
+             * @protected
+             */
+            singleton: true
+        }
+    }
+
+    /**
+     * @param {Object} config
+     */
+    constructor(config) {
+        super(config);
+    }
+}
+
+Neo.applyClassConfig(DragDrop);
+
+let instance = Neo.create(DragDrop);
+
+Neo.applyToGlobalNs(instance);
+
+export default instance;
