ccf61615e010c81cd0a3dc487662a89d79d087aa
tobiu <tobiasuhlig78@gmail.com>
2020-08-15 10:53:27 +0200

    #994 unit test

--- test/siesta/tests/VdomCalendar.mjs
+++ test/siesta/tests/VdomCalendar.mjs
@@ -167,4 +167,61 @@ StartTest(t => {
             {action: 'moveNode', id: 'neo-event-1', index: 0, parentId: 'neo-column-1'} // todo: does not hurt, but not needed
         ], 'deltas got created successfully');
     });
+
+    t.it('Moving multiple events to the left', t => {
+        t.diag("Insert event into a column on the left");
+
+        vdom = {
+            id: 'neo-calendar-week',
+            cn: [
+                {id: 'neo-column-1', cls: ['neo-c-w-column'], cn : []},
+                {id: 'neo-column-2', cls: ['neo-c-w-column'], cn : [{id: 'neo-event-1', cls: ['neo-event']}]},
+                {id: 'neo-column-3', cls: ['neo-c-w-column'], cn : []},
+                {id: 'neo-column-4', cls: ['neo-c-w-column'], cn : [{id: 'neo-event-2', cls: ['neo-event']}]},
+                {id: 'neo-column-5', cls: ['neo-c-w-column'], cn : [{id: 'neo-event-3', cls: ['neo-event']}]}
+            ]
+        };
+
+        vnode = VdomHelper.create(vdom);
+
+        vdom = {
+            id: 'neo-calendar-week',
+            cn: [
+                {id: 'neo-column-1', cls: ['neo-c-w-column'], cn : [{id: 'neo-event-1', cls: ['neo-event']}]},
+                {id: 'neo-column-2', cls: ['neo-c-w-column'], cn : []},
+                {id: 'neo-column-3', cls: ['neo-c-w-column'], cn : [{id: 'neo-event-2', cls: ['neo-event']}]},
+                {id: 'neo-column-4', cls: ['neo-c-w-column'], cn : [{id: 'neo-event-3', cls: ['neo-event']}]},
+                {id: 'neo-column-5', cls: ['neo-c-w-column'], cn : []}
+            ]
+        };
+
+        output = VdomHelper.update({vdom: vdom, vnode: vnode}); deltas = output.deltas; vnode = output.vnode;
+
+        t.isDeeplyStrict(deltas, [
+            {action: 'moveNode', id: 'neo-event-1', index: 0, parentId: 'neo-column-1'},
+            {action: 'moveNode', id: 'neo-event-2', index: 0, parentId: 'neo-column-3'},
+            {action: 'moveNode', id: 'neo-event-3', index: 0, parentId: 'neo-column-4'}
+        ], 'deltas got created successfully');
+
+        t.diag("Revert operation");
+
+        vdom = {
+            id: 'neo-calendar-week',
+            cn: [
+                {id: 'neo-column-1', cls: ['neo-c-w-column'], cn : []},
+                {id: 'neo-column-2', cls: ['neo-c-w-column'], cn : [{id: 'neo-event-1', cls: ['neo-event']}]},
+                {id: 'neo-column-3', cls: ['neo-c-w-column'], cn : []},
+                {id: 'neo-column-4', cls: ['neo-c-w-column'], cn : [{id: 'neo-event-2', cls: ['neo-event']}]},
+                {id: 'neo-column-5', cls: ['neo-c-w-column'], cn : [{id: 'neo-event-3', cls: ['neo-event']}]}
+            ]
+        };
+
+        output = VdomHelper.update({vdom: vdom, vnode: vnode}); deltas = output.deltas; vnode = output.vnode;
+
+        t.isDeeplyStrict(deltas, [
+            {action: 'moveNode', id: 'neo-event-1', index: 0, parentId: 'neo-column-2'},
+            {action: 'moveNode', id: 'neo-event-2', index: 0, parentId: 'neo-column-4'},
+            {action: 'moveNode', id: 'neo-event-3', index: 0, parentId: 'neo-column-5'}
+        ], 'deltas got created successfully');
+    });
 });
