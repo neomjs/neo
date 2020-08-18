9ac0555ada2329be3c7edff56cc2a923d48ddee8
tobiu <tobiasuhlig78@gmail.com>
2020-08-18 18:24:00 +0200

    main.addon.DragDrop: dynamically importing either the touch or mouse sensor

--- src/main/addon/DragDrop.mjs
+++ src/main/addon/DragDrop.mjs
@@ -1,7 +1,5 @@
-import Base                     from '../../core/Base.mjs';
-import DomEvents                from '../DomEvents.mjs';
-import {default as MouseSensor} from '../draggable/sensor/Mouse.mjs';
-import {default as TouchSensor} from '../draggable/sensor/Touch.mjs';
+import Base      from '../../core/Base.mjs';
+import DomEvents from '../DomEvents.mjs';
 
 /**
  * @class Neo.main.addon.DragDrop
@@ -96,22 +94,23 @@ class DragDrop extends Base {
      */
     constructor(config) {
         super(config);
+
+        let imports = []
+
         this.addGlobalEventListeners();
 
-        console.log('Neo.config.hasTouchEvents', Neo.config.hasTouchEvents);
-
         if (Neo.config.hasTouchEvents) {
-
+            imports.push(import(/* webpackChunkName: 'src/main.draggable.sensor.Touch.js' */ '../draggable/sensor/Touch.mjs'));
+        } else {
+            imports.push(import(/* webpackChunkName: 'src/main.draggable.sensor.Mouse.js' */ '../draggable/sensor/Mouse.mjs'));
         }
 
-        // testing
-        Neo.create({
-            module: TouchSensor
+        Promise.all(imports).then(modules => {
+            // create the Touch or MouseSensor
+            Neo.create({
+                module: modules[0].default
+            });
         });
-
-        /*Neo.create({
-            module: MouseSensor
-        });*/
     }
 
     /**
