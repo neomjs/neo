4465d2b2fc8bd2bc6286d52e04d4f0c2f7c5d13e
tobiu <tobiasuhlig78@gmail.com>
2020-09-16 13:11:37 +0200

    Neo.AppWorker build entry point

---
+++ buildScripts/webpack/entrypoints/AppWorker.mjs
@@ -0,0 +1,44 @@
+import App from '../../../src/worker/App.mjs';
+
+/**
+ * We need a custom entry point for builds outside the framework (e.g. a workspace created via npx neo-app)
+ * @class Neo.AppWorker
+ * @extends Neo.worker.App
+ * @singleton
+ */
+class AppWorker extends App {
+    static getConfig() {return {
+        /**
+         * @member {String} className='Neo.AppWorker'
+         * @protected
+         */
+        className: 'Neo.AppWorker',
+        /**
+         * @member {Boolean} singleton=true
+         * @protected
+         */
+        singleton: true
+    }}
+    /**
+     *
+     * @param {String} path
+     * @returns {Promise}
+     */
+    importApp(path) {
+        return import(
+            /* webpackInclude: /\/app.mjs$/ */
+            /* webpackExclude: /\/node_modules$/ */
+            /* webpackChunkName: "chunks/[request]" */
+            /* webpackMode: "lazy" */
+            `../../${path}`
+        );
+    }
+}
+
+Neo.applyClassConfig(AppWorker);
+
+let instance = Neo.create(AppWorker);
+
+Neo.applyToGlobalNs(instance);
+
+export default instance;
