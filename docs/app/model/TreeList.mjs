210beda75f1084de69e7f6f6d84297882d602b86
tobiu <tobiasuhlig78@gmail.com>
2020-11-05 14:47:20 +0100

    Docs.model.TreeList: base class #1398

---
+++ docs/app/model/TreeList.mjs
@@ -0,0 +1,44 @@
+import Model  from '../../../src/data/Model.mjs';
+
+/**
+ * @class Docs.model.TreeList
+ * @extends Neo.data.Model
+ */
+class TreeList extends Model {
+    static getConfig() {return {
+        className: 'Docs.model.TreeList',
+
+        fields: [{
+            name: 'className',
+            type: 'String'
+        }, {
+            name: 'collapsed',
+            type: 'Boolean'
+        }, {
+            name: 'id',
+            type: 'Integer'
+        }, {
+            name: 'isLeaf',
+            type: 'Boolean'
+        }, {
+            name: 'name',
+            type: 'String'
+        }, {
+            name: 'parentId',
+            type: 'Integer'
+        }, {
+            name: 'path',
+            type: 'String'
+        }, {
+            name: 'singleton',
+            type: 'Boolean'
+        }, {
+            name: 'srcPath',
+            type: 'String'
+        }]
+    }}
+}
+
+Neo.applyClassConfig(TreeList);
+
+export {TreeList as default};
