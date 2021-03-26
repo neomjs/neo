5b964dac836eb19d032b24f569faeff221d0c721
tobiu <tobiasuhlig78@gmail.com>
2021-03-26 13:01:50 +0100

    Neo classes: method order #1591

--- src/core/IdGenerator.mjs
+++ src/core/IdGenerator.mjs
@@ -52,12 +52,6 @@ class IdGenerator {
         Neo.getId = me.getId.bind(me);
     }
 
-    onConstructed() {}
-
-    onAfterConstructed() {}
-
-    init() {}
-
     /**
      *
      * @param name
@@ -74,6 +68,12 @@ class IdGenerator {
 
         return me.base + (name === 'neo' ? '' : name + '-') + count;
     }
+
+    init() {}
+
+    onAfterConstructed() {}
+
+    onConstructed() {}
 }
 
 Neo.applyClassConfig(IdGenerator);
