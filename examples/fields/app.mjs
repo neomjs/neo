5833200e173977a5262edd9c22a1685f11ac7300
tobiu <tobiasuhlig78@gmail.com>
2021-03-29 00:49:37 +0200

    Neo.examples.fields => appName #1623

--- examples/fields/app.mjs
+++ examples/fields/app.mjs
@@ -3,7 +3,7 @@ import MainContainer from './MainContainer.mjs';
 const onStart = () => Neo.app({
     appPath : 'examples/fields/',
     mainView: MainContainer,
-    name    : 'TestApp',
+    name    : 'Neo.examples.fields',
     parentId: 'main-container'
 });
 
