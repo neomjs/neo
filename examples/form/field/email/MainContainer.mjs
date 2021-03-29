e4a7a2c1f2f4dc44a36c0538c96a291a0d3016ea
tobiu <tobiasuhlig78@gmail.com>
2021-03-29 09:29:00 +0200

    Neo.examples.form.field.email => appName #1626

--- examples/form/field/email/MainContainer.mjs
+++ examples/form/field/email/MainContainer.mjs
@@ -6,14 +6,12 @@ import Radio                 from '../../../../src/form/field/Radio.mjs';
 import TextField             from '../../../../src/form/field/Text.mjs';
 
 /**
- * @class TestApp.MainContainer
+ * @class Neo.examples.form.field.email.MainContainer
  * @extends Neo.examples.ConfigurationViewport
  */
 class MainContainer extends ConfigurationViewport {
     static getConfig() {return {
-        className: 'TestApp.MainContainer',
-        ntype    : 'main-container',
-
+        className           : 'TestApp.MainContainer',
         autoMount           : true,
         configItemLabelWidth: 160,
         layout              : {ntype: 'hbox', align: 'stretch'}
