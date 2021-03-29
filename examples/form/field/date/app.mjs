b3ed169fb65da34ac686e01a7e55373ecd3121ee
tobiu <tobiasuhlig78@gmail.com>
2021-03-29 09:26:49 +0200

    Neo.examples.form.field.date => appName #1625

--- examples/form/field/date/app.mjs
+++ examples/form/field/date/app.mjs
@@ -3,7 +3,7 @@ import MainContainer from './MainContainer.mjs';
 const onStart = () => Neo.app({
     appPath : 'examples/form/field/date/',
     mainView: MainContainer,
-    name    : 'TestApp'
+    name    : 'Neo.examples.form.field.date'
 });
 
 export {onStart as onStart};
