41c0fa49162bc57d63956dfb8db0f0987034f863
tobiu <tobiasuhlig78@gmail.com>
2021-03-29 13:24:53 +0200

    #1647 Neo.examples.tabs.MainContainer

--- examples/tabs/MainContainer.mjs
+++ examples/tabs/MainContainer.mjs
@@ -1,7 +1,6 @@
 import TabContainer  from '../../src/tab/Container.mjs';
 import NumberField   from '../../src/form/field/Number.mjs';
 import PasswordField from '../../src/form/field/Password.mjs';
-import TextField     from '../../src/form/field/Text.mjs';
 
 /**
  * @class Neo.examples.tabs.MainContainer
@@ -26,185 +25,146 @@ class MainContainer extends TabContainer {
             }
         },
 
-        items: [
-            {
-                ntype: 'container',
+        items: [{
+            ntype: 'container',
+
+            itemDefaults: {
+                ntype     : 'textfield',
+                flex      : '0 1 auto',
+                labelWidth: '120px'
+            },
+
+            layout: {
+                ntype: 'vbox'
+            },
+
+            items: [{
+                id       : 'firstNameField',
+                labelText: 'First Name'
+            }, {
+                labelText: 'Last Name'
+            }, {
+                module   : NumberField,
+                labelText: 'Number | 10-20',
+                maxValue : 20,
+                minValue : 10,
+                value    : 10
+            }, {
+                module   : PasswordField,
+                labelText: 'Password'
+            }, {
+                ntype: 'component',
+                flex : 1
+            }, {
+                ntype : 'toolbar',
+                layout: {ntype: 'hbox'},
 
                 itemDefaults: {
-                    ntype     : 'textfield',
-                    flex      : '0 1 auto',
-                    labelWidth: '120px'
-                },
-
-                layout: {
-                    ntype: 'vbox'
-                },
-
-                items: [
-                    {
-                        id       : 'firstNameField',
-                        labelText: 'First Name'
-                    },
-                    {
-                        labelText: 'Last Name'
-                    },
-                    {
-                        ntype    : 'numberfield',
-                        labelText: 'Number | 10-20',
-                        maxValue : 20,
-                        minValue : 10,
-                        value    : 10
-                    },
-                    {
-                        ntype    : 'passwordfield',
-                        labelText: 'Password'
-                    },
-                    {
-                        ntype: 'component',
-                        flex : 1
-                    },
-                    {
-                        ntype : 'toolbar',
-                        layout: {
-                            ntype: 'hbox'
-                        },
-                        itemDefaults: {
-                            ntype: 'button',
-                            style: {
-                                margin: '0 10px 0 0'
-                            }
-                        },
-                        items: [
-                            {
-                                text: 'Change Label Text',
-                                domListeners: {
-                                    click: {
-                                        fn: function () {
-                                            let field = Neo.getComponent('firstNameField');
-
-                                            field.labelText = 'New Label' + Math.round(Math.random() * 100);
-                                        }
-                                    }
-                                }
-                            },
-                            {
-                                text: 'Change Label Color',
-                                domListeners: {
-                                    click: {
-                                        fn: function () {
-                                            let field = Neo.getComponent('firstNameField'),
-                                                label = field.getLabelEl(),
-                                                style = label.style,
-                                                vdom  = field.vdom;
-
-                                            if (!style) {
-                                                style = {};
-                                            }
-
-                                            if (style.color && style.color === 'red') {
-                                                delete style.color;
-                                            } else {
-                                                style.color = 'red';
-                                            }
-
-                                            label.style = style;
-                                            field.vdom = vdom;
-                                        }
-                                    }
-                                }
-                            },
-                            {
-                                text: 'Move Fields',
-                                domListeners: {
-                                    click: {
-                                        fn: function () {
-                                            let field  = Neo.getComponent('firstNameField'),
-                                                parent = Neo.getComponent(field.parentId),
-                                                vdom   = parent.vdom,
-                                                cn     = vdom.cn,
-                                                tmp    = cn[1];
-
-                                            cn[1] = cn[3];
-                                            cn[3] = tmp;
-
-                                            cn[1].cn[0].style.color = 'steelblue';
-                                            cn[3].cn[0].style.color = 'green';
-
-                                            parent.vdom = vdom;
-                                        }
-                                    }
-                                }
-                            },
-                            {
-                                iconCls: 'fa fa-plus',
-                                text   : 'Insert Textfield',
-                                domListeners: {
-                                    click: {
-                                        fn: function () {
-                                            let button = Neo.getComponent('firstNameField'),
-                                                parent = Neo.getComponent(button.parentId);
-
-                                            // global variable for testing
-                                            if (!this.fieldCount) {
-                                                this.fieldCount = 0;
-                                            }
-
-                                            this.fieldCount++;
-
-                                            parent.insert(4, {
-                                                labelText: 'Dynamic Field ' + this.fieldCount
-                                            });
-                                        }
-                                    }
-                                }
-                            },
-                            {
-                                iconCls: 'fa fa-minus',
-                                text   : 'Remove Textfield',
-                                style: {
-                                    margin: 0
-                                },
-                                domListeners: {
-                                    click: {
-                                        fn: function (data) {
-                                            let button    = Neo.getComponent(data.target.id),
-                                                container = button.up('container');
-
-                                            if (container.items[4].ntype === 'textfield') {
-                                                container.removeAt(4);
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        ]
+                    ntype: 'button',
+                    style: {
+                        margin: '0 10px 0 0'
                     }
-                ],
+                },
 
-                tabButtonConfig: {
-                    iconCls: 'fa fa-home',
-                    text   : 'Tab 1'
-                }
-            },
-            {
-                tabButtonConfig: {
-                    iconCls: 'fa fa-play-circle',
-                    text   : 'Tab 2'
-                },
-                vdom: {
-                    innerHTML: 'Test 2'
-                }
-            },
-            {
-                tabButtonConfig: {
-                    iconCls: 'fa fa-user',
-                    text   : 'Tab 3'
-                },
-                vdom: {
-                    innerHTML: 'Test 3'
-                }
+                items: [{
+                    text: 'Change Label Text',
+                    handler: function () {
+                        let field = Neo.getComponent('firstNameField');
+
+                        field.labelText = 'New Label' + Math.round(Math.random() * 100);
+                    }
+                }, {
+                    text   : 'Change Label Color',
+                    handler: function () {
+                        let field = Neo.getComponent('firstNameField'),
+                            label = field.getLabelEl(),
+                            style = label.style,
+                            vdom  = field.vdom;
+
+                        if (!style) {
+                            style = {};
+                        }
+
+                        if (style.color && style.color === 'red') {
+                            delete style.color;
+                        } else {
+                            style.color = 'red';
+                        }
+
+                        label.style = style;
+                        field.vdom = vdom;
+                    }
+                }, {
+                    text   : 'Move Fields',
+                    handler: function () {
+                        let field  = Neo.getComponent('firstNameField'),
+                            parent = Neo.getComponent(field.parentId),
+                            vdom   = parent.vdom,
+                            cn     = vdom.cn,
+                            tmp    = cn[1];
+
+                        cn[1] = cn[3];
+                        cn[3] = tmp;
+
+                        cn[1].cn[0].style.color = 'steelblue';
+                        cn[3].cn[0].style.color = 'green';
+
+                        parent.vdom = vdom;
+                    }
+                }, {
+                    iconCls: 'fa fa-plus',
+                    text   : 'Insert Textfield',
+                    handler: function () {
+                        let button = Neo.getComponent('firstNameField'),
+                            parent = Neo.getComponent(button.parentId);
+
+                        // global variable for testing
+                        if (!this.fieldCount) {
+                            this.fieldCount = 0;
+                        }
+
+                        this.fieldCount++;
+
+                        parent.insert(4, {
+                            labelText: 'Dynamic Field ' + this.fieldCount
+                        });
+                    }
+                }, {
+                    iconCls: 'fa fa-minus',
+                    style  : {margin: 0},
+                    text   : 'Remove Textfield',
+                    handler: function (data) {
+                        let button    = Neo.getComponent(data.target.id),
+                            container = button.up('container');
+
+                        if (container.items[4].ntype === 'textfield') {
+                            container.removeAt(4);
+                        }
+                    }
+                }]
+            }],
+
+            tabButtonConfig: {
+                iconCls: 'fa fa-home',
+                text   : 'Tab 1'
             }
-        ]
+        }, {
+            tabButtonConfig: {
+                iconCls: 'fa fa-play-circle',
+                text   : 'Tab 2'
+            },
+            vdom: {
+                innerHTML: 'Test 2'
+            }
+        }, {
+            tabButtonConfig: {
+                iconCls: 'fa fa-user',
+                text   : 'Tab 3'
+            },
+            vdom: {
+                innerHTML: 'Test 3'
+            }
+        }]
     }}
 }
 
