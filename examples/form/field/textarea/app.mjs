9f398b16901bbda7412ecc92e8ddf32eb85078e4
tobiu <tobiasuhlig78@gmail.com>
2021-03-29 09:38:17 +0200

    Neo.examples.form.field.textarea => appName #1631

--- examples/form/field/textarea/app.mjs
+++ examples/form/field/textarea/app.mjs
@@ -3,7 +3,7 @@ import MainContainer from './MainContainer.mjs';
 const onStart = () => Neo.app({
     appPath : 'examples/form/field/textarea/',
     mainView: MainContainer,
-    name    : 'TestApp'
+    name    : 'Neo.examples.form.field.textarea'
 });
 
 export {onStart as onStart};
