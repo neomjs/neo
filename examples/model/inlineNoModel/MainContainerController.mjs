6f8dd8825fdf4608c08ed5201e320700d1e12325
tobiu <tobiasuhlig78@gmail.com>
2021-03-31 19:10:56 +0200

    examples/model/inlineNoModel #1695 copied the content from examples/inline & adjusted the paths. added to the build scripts.

---
+++ examples/model/inlineNoModel/MainContainerController.mjs
@@ -0,0 +1,81 @@
+import Component from '../../../src/controller/Component.mjs';
+
+/**
+ * @class Neo.examples.model.inlineNoModel.MainContainerController
+ * @extends Neo.controller.Component
+ */
+class MainContainerController extends Component {
+    static getConfig() {return {
+        /**
+         * @member {String} className='Neo.examples.model.inlineNoModel.MainContainerController'
+         * @protected
+         */
+        className: 'Neo.examples.model.inlineNoModel.MainContainerController'
+    }}
+
+    /**
+     *
+     * @param {Object} data
+     */
+    onButton1Click(data) {
+        this.updateButton1Text('Button 1');
+    }
+
+    /**
+     *
+     * @param {Object} data
+     */
+    onButton2Click(data) {
+        this.updateButton2Text('Button 2');
+    }
+
+    /**
+     *
+     * @param {Object} data
+     */
+    onLogModelIntoConsoleButtonClick(data) {
+        console.log(this.getModel());
+    }
+
+    /**
+     *
+     * @param {Object} data
+     */
+    onTextField1Change(data) {
+        if (data.oldValue !== null) {
+            this.updateButton1Text(data.value);
+        }
+    }
+
+    /**
+     *
+     * @param {Object} data
+     */
+    onTextField2Change(data) {
+        if (data.oldValue !== null) {
+            this.updateButton2Text(data.value);
+        }
+    }
+
+    /**
+     *
+     * @param {String} value
+     */
+    updateButton1Text(value) {
+        this.getModel().data['button1Text'] = value;
+    }
+
+    /**
+     *
+     * @param {String} value
+     */
+    updateButton2Text(value) {
+        this.getModel().setData({
+            button2Text: value
+        });
+    }
+}
+
+Neo.applyClassConfig(MainContainerController);
+
+export {MainContainerController as default};
