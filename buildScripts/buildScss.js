dad6a00f6fb89fabe8c557867d1c539d29311946
tobiu <tobiasuhlig78@gmail.com>
2021-05-02 20:25:07 +0200

    buildScripts/buildScss.js (in progress, added postcss into the mix)

--- buildScripts/buildScss.js
+++ buildScripts/buildScss.js
@@ -1,21 +1,24 @@
 'use strict';
 
-const chalk       = require('chalk'),
-      { program } = require('commander'),
-      cp          = require('child_process'),
-      cwd         = process.cwd(),
-      cpOpts      = {env: process.env, cwd: cwd, stdio: 'inherit', shell: true},
-      envinfo     = require('envinfo'),
-      fs          = require('fs-extra'),
-      inquirer    = require('inquirer'),
-      os          = require('os'),
-      path        = require('path'),
-      packageJson = require(path.resolve(cwd, 'package.json')),
-      neoPath     = packageJson.name === 'neo.mjs' ? './' : './node_modules/neo.mjs/',
-      sass        = require('sass'),
-      scssPath    = path.resolve(neoPath, 'resources/scss_new/src'),
-      programName = `${packageJson.name} buildThemes`,
-      questions   = [];
+const autoprefixer = require('autoprefixer'),
+      chalk        = require('chalk'),
+      { program }  = require('commander'),
+      cp           = require('child_process'),
+      cssnano      = require('cssnano'),
+      cwd          = process.cwd(),
+      cpOpts       = {env: process.env, cwd: cwd, stdio: 'inherit', shell: true},
+      envinfo      = require('envinfo'),
+      fs           = require('fs-extra'),
+      inquirer     = require('inquirer'),
+      os           = require('os'),
+      path         = require('path'),
+      packageJson  = require(path.resolve(cwd, 'package.json')),
+      neoPath      = packageJson.name === 'neo.mjs' ? './' : './node_modules/neo.mjs/',
+      postcss      = require('postcss'),
+      sass         = require('sass'),
+      scssPath     = path.resolve(neoPath, 'resources/scss_new/src'),
+      programName  = `${packageJson.name} buildThemes`,
+      questions    = [];
 
 const getAllScssFiles = function(dirPath, arrayOfFiles) {
     const files = fs.readdirSync(dirPath);
@@ -38,7 +41,15 @@ const getAllScssFiles = function(dirPath, arrayOfFiles) {
 const files      = getAllScssFiles(scssPath),
       useCssVars = true;
 
+let filePath;
+
+fs.mkdirpSync(path.resolve(neoPath, 'dist/production/css'));
+
 files.forEach(file => {
+    let fileName = path.parse(file).name;
+    console.log(fileName);
+    console.log(file);
+
     fs.readFile(file).then(content => {
         sass.render({
             data: `
@@ -49,7 +60,16 @@ files.forEach(file => {
             if (err) {
                 console.log(err);
             } else {
-                console.log(result.css.toString());
+                postcss([autoprefixer, cssnano]).process(result.css, {}).then(result => {
+                    filePath = path.resolve(neoPath, `dist/production/css/${fileName}.css`);
+                    console.log(result);
+                    console.log(filePath);
+                    console.log(fileName);
+                    fs.writeFile(filePath, result.css, () => true)
+                    if ( result.map ) {
+                        fs.writeFile(filePath, result.map.toString(), () => true)
+                    }
+                });
             }
         });
     });
