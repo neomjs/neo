47aaa4bc5d4c4f976bc0411ab8d2c966b4d0c300
tobiu <tobiasuhlig78@gmail.com>
2021-05-14 13:02:55 +0200

    buildScripts/docs: support for win32 #2062

--- buildScripts/docs/jsdocx.js
+++ buildScripts/docs/jsdocx.js
@@ -1,22 +1,22 @@
 const fs          = require('fs-extra'),
       jsdocx      = require('neo-jsdoc-x'),
       path        = require('path'),
-      processRoot = process.cwd(),
-      helper      = require(path.join(processRoot, 'node_modules/neo-jsdoc-x/src/lib/helper.js')),
+      cwd         = process.cwd(),
+      helper      = require(path.join(cwd, 'node_modules/neo-jsdoc-x/src/lib/helper.js')),
       packageJson = require(path.resolve(process.cwd(), 'package.json')),
       insideNeo   = packageJson.name === 'neo.mjs',
       neoPath     = insideNeo ? './' : './node_modules/neo.mjs/',
       appNames    = [],
       options = {
           access        : 'all',
-          files         : [path.join(neoPath, 'src/**/*.mjs'), './docs/app/**/*.mjs'],
+          files         : [path.resolve(neoPath, 'src/**/*.mjs'), path.resolve(neoPath, 'docs/app/**/*.mjs')],
           includePattern: ".+\\.(m)js(doc)?$",
           excludePattern: "(^|\\/|\\\\)_",
           recurse       : true,
           undocumented  : false
       };
 
-let appJsonPath = path.resolve(processRoot, 'buildScripts/myApps.json'),
+let appJsonPath = path.resolve(cwd, 'buildScripts/myApps.json'),
     appJson;
 
 if (fs.existsSync(appJsonPath)) {
@@ -35,7 +35,7 @@ if (appJson) {
     Object.entries(appJson.apps).forEach(([key, value]) => {
         if (key !== 'Docs') { // the docs app is automatically included
             appNames.push(key);
-            options.files.push('.' + value.output + '**/*.mjs');
+            options.files.push(path.join('./', value.output + '**/*.mjs'));
         }
     });
 }
