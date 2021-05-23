f310465093bd571a433cf0d673f07bb5f6befd07
tobiu <tobiasuhlig78@gmail.com>
2021-05-23 14:15:45 +0200

    Neo.util.Compare: isEqual() #2135 => comparing arrays

--- src/util/Compare.mjs
+++ src/util/Compare.mjs
@@ -17,9 +17,33 @@ class Compare extends Base {
      *
      * @param {*} item1
      * @param {*} item2
+     * @returns {Boolean}
      */
     static isEqual(item1, item2) {
+        if (item1 === item2) {
+            return true;
+        }
 
+        let type1 = typeof item1,
+            type2 = typeof item2;
+
+        if (type1 !== type2) {
+            return false;
+        }
+
+        if (Array.isArray(item1)) {
+            if (item1.length !== item2.length) {
+                return false;
+            }
+
+            for (const [i, v] of item1.entries()) {
+                if (!Compare.isEqual(v, item2[i])) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
 
