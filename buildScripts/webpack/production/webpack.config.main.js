b680f75abd8e2d68d4020c86edc0741b2004cad2
tobiu <tobiasuhlig78@gmail.com>
2021-05-26 11:18:35 +0200

    webpack.config.main: add the entry point script back in #2151

--- buildScripts/webpack/production/webpack.config.main.js
+++ buildScripts/webpack/production/webpack.config.main.js
@@ -2,11 +2,13 @@ const path        = require('path'),
       buildTarget = require('./buildTarget.json'),
       processRoot = process.cwd(),
       packageJson = require(path.resolve(processRoot, 'package.json')),
-      neoPath     = packageJson.name === 'neo.mjs' ? './' : './node_modules/neo.mjs/';
+      neoPath     = packageJson.name === 'neo.mjs' ? './' : './node_modules/neo.mjs/',
+      config      = require(path.resolve(neoPath, 'buildScripts/webpack/json/build.json')),
+      entry       = {main: path.resolve(neoPath, config.mainInput)};
 
 module.exports = {
     mode  : 'production',
-    entry : {main: path.resolve(neoPath, './src/Main.mjs')},
+    entry,
     target: 'web',
 
     output: {
