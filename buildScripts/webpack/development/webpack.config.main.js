fa56d94bfb5fdc22becc90e5d71b6739f7cda579
tobiu <tobiasuhlig78@gmail.com>
2021-05-26 11:41:24 +0200

    webpack.config.worker & main: config => filenameConfig #2153

--- buildScripts/webpack/development/webpack.config.main.js
+++ buildScripts/webpack/development/webpack.config.main.js
@@ -1,10 +1,10 @@
-const path        = require('path'),
-      buildTarget = require('./buildTarget.json'),
-      processRoot = process.cwd(),
-      packageJson = require(path.resolve(processRoot, 'package.json')),
-      neoPath     = packageJson.name === 'neo.mjs' ? './' : './node_modules/neo.mjs/',
-      config      = require(path.resolve(neoPath, 'buildScripts/webpack/json/build.json')),
-      entry       = {main: path.resolve(neoPath, config.mainInput)};
+const path           = require('path'),
+      buildTarget    = require('./buildTarget.json'),
+      processRoot    = process.cwd(),
+      packageJson    = require(path.resolve(processRoot, 'package.json')),
+      neoPath        = packageJson.name === 'neo.mjs' ? './' : './node_modules/neo.mjs/',
+      filenameConfig = require(path.resolve(neoPath, 'buildScripts/webpack/json/build.json')),
+      entry          = {main: path.resolve(neoPath, filenameConfig.mainInput)};
 
 module.exports = {
     mode   : 'development',
@@ -14,7 +14,7 @@ module.exports = {
 
     output: {
         chunkFilename: 'chunks/main/[id].js',
-        filename     : config.mainOutput,
+        filename     : filenameConfig.mainOutput,
         path         : path.resolve(processRoot, buildTarget.folder)
     }
 };
--- buildScripts/webpack/development/webpack.config.worker.js
+++ buildScripts/webpack/development/webpack.config.worker.js
@@ -3,12 +3,12 @@ const path        = require('path'),
       processRoot = process.cwd(),
       packageJson = require(path.resolve(processRoot, 'package.json')),
       neoPath     = packageJson.name === 'neo.mjs' ? './' : './node_modules/neo.mjs/',
-      config      = require(path.resolve(neoPath, 'buildScripts/webpack/json/build.json')),
+      filenameConfig      = require(path.resolve(neoPath, 'buildScripts/webpack/json/build.json')),
       entry       = {};
 
 module.exports = env => {
-    if (config.workers) {
-        Object.entries(config.workers).forEach(([key, value]) => {
+    if (filenameConfig.workers) {
+        Object.entries(filenameConfig.workers).forEach(([key, value]) => {
             if (key === env.worker) {
                 entry[key] = path.resolve(neoPath, value.input);
             }
@@ -27,8 +27,8 @@ module.exports = env => {
             filename: chunkData => {
                 let name = chunkData.chunk.name;
 
-                if (config.workers.hasOwnProperty(name)) {
-                    return config.workers[name].output;
+                if (filenameConfig.workers.hasOwnProperty(name)) {
+                    return filenameConfig.workers[name].output;
                 }
             },
 
