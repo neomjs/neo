58edab5af74d7c1489ca8f81b19706a24eaacfc9
tobiu <tobiasuhlig78@gmail.com>
2021-06-10 09:21:32 +0200

    #2294 calendar.view.MainContainer: activeView => week (pre release)

--- src/calendar/view/MainContainer.mjs
+++ src/calendar/view/MainContainer.mjs
@@ -44,7 +44,7 @@ class MainContainer extends Container {
          * valid values: 'day', 'week', 'month', 'year'
          * @member {String} activeView_='month'
          */
-        activeView_: 'month',
+        activeView_: 'week',
         /**
          * Scale the calendar with using s different base font-size
          * @member {Number|null} baseFontSize_=null
