47b880b156bfdd7ab3b2cc0cea1f6386e93e07d1
tobiu <tobiasuhlig78@gmail.com>
2021-06-21 12:14:33 +0200

    plugin.Resizable: support for very fast drag OPs #2431

--- src/plugin/Resizable.mjs
+++ src/plugin/Resizable.mjs
@@ -183,8 +183,10 @@ class Resizable extends Base {
             {'drag:end'  : me.onDragEnd,    scope: me, delegate: '.neo-resizable'},
             {'drag:move' : me.onDragMove,   scope: me, delegate: '.neo-resizable'},
             {'drag:start': me.onDragStart,  scope: me, delegate: '.neo-resizable'},
+            {mousedown   : me.onMouseDown,  scope: me, delegate: '.neo-resizable'},
             {mousemove   : me.onMouseMove,  scope: me, local   : true},
-            {mouseleave  : me.onMouseLeave, scope: me, delegate: `.${me.delegationCls}`}
+            {mouseleave  : me.onMouseLeave, scope: me, delegate: `.${me.delegationCls}`},
+            {mouseup     : me.onMouseUp,    scope: me, delegate: '.neo-resizable'}
         );
 
         me.owner.domListeners = domListeners;
@@ -448,6 +450,14 @@ class Resizable extends Base {
      *
      * @param {Object} data
      */
+    onMouseDown(data) {
+        this.isDragging = true;
+    }
+
+    /**
+     *
+     * @param {Object} data
+     */
     onMouseMove(data) {
         let me         = this,
             dir        = me.directions,
@@ -521,6 +531,14 @@ class Resizable extends Base {
                 me.removeAllNodes();
             }
         }
+    }
+
+    /**
+     *
+     * @param {Object} data
+     */
+    onMouseUp(data) {
+        this.isDragging = false;
     }
 
     /**
