a386ac1352d6e3d83f4cdae4e1b9690db13c4257
tobiu <tobiasuhlig78@gmail.com>
2021-07-09 00:14:02 +0200

    examples.calendar.weekview #2592

---
+++ examples/calendar/weekview/MainContainer.mjs
@@ -0,0 +1,49 @@
+import Button                  from '../../../src/button/Base.mjs';
+import WeekView                from '../../../src/calendar/view/week/Component.mjs';
+import MainContainerModel      from '../../../src/calendar/view/MainContainerModel.mjs';
+import Toolbar                 from '../../../src/container/Toolbar.mjs';
+import Viewport                from '../../../src/container/Viewport.mjs';
+
+/**
+ * @class Neo.examples.calendar.weekview.MainContainer
+ * @extends Neo.container.Viewport
+ */
+class MainContainer extends Viewport {
+    static getConfig() {return {
+        className : 'Neo.examples.calendar.weekview.MainContainer',
+        autoMount : true,
+        cls       : ['neo-examples-calendar-maincontainer', 'neo-viewport'],
+        layout    : {ntype: 'fit'},
+        model     : MainContainerModel,
+
+        calendarStoreConfig_: {
+            autoLoad: true,
+            url     : '../../examples/calendar/basic/data/calendars.json'
+        },
+
+        eventStoreConfig_: {
+            autoLoad: true,
+            url     : '../../examples/calendar/basic/data/events.json'
+        },
+
+        items: [{
+            module     : WeekView,
+            currentDate: new Date('2021-07-20'),
+            flex       : 1,
+            reference  : 'calendar'
+        }]
+    }}
+
+    /**
+     *
+     * @param config
+     */
+    constructor(config) {
+        super(config);
+        Neo.worker.App.insertThemeFiles(this.appName, null, 'Neo.calendar.view.MainContainer');
+    }
+}
+
+Neo.applyClassConfig(MainContainer);
+
+export {MainContainer as default};
