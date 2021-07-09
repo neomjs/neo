f5e5c425061dceddf1f37bd199ca49485b53d69e
tobiu <tobiasuhlig78@gmail.com>
2021-07-09 10:58:31 +0200

    examples.calendar.weekview.MainContainer: doc comments

--- examples/calendar/weekview/MainContainer.mjs
+++ examples/calendar/weekview/MainContainer.mjs
@@ -8,22 +8,53 @@ import WeekComponent      from '../../../src/calendar/view/week/Component.mjs';
  */
 class MainContainer extends Viewport {
     static getConfig() {return {
+        /**
+         * @member {String} className='Neo.examples.calendar.weekview.MainContainer'
+         * @protected
+         */
         className: 'Neo.examples.calendar.weekview.MainContainer',
+        /**
+         * Value for the component.Base autoMount_ config
+         * @member {Boolean} autoMount=true
+         */
         autoMount: true,
-        cls      : ['neo-examples-calendar-maincontainer', 'neo-viewport'],
-        layout   : {ntype: 'fit'},
-        model    : MainContainerModel,
-
+        /**
+         * Value for the container.Base layout_ config
+         * @member {Object} layout={ntype: 'fit'}
+         */
+        layout: {ntype: 'fit'},
+        /**
+         * Value for the component.Base model_ config
+         * The calendar view model relies on the connected view to have the calendarStoreConfig_ & eventStoreConfig_
+         * configs. We have two options here:
+         * 1. Extend Neo.calendar.view.week.Component and add the configs and view model there.
+         * 2. Just drop them into a parent view.
+         * @member {Neo.calendar.view.MainContainerModel} model=MainContainerModel
+         */
+        model: MainContainerModel,
+        /**
+         * Config options for Neo.calendar.store.Calendars.
+         * The calendar view model relies on it.
+         * @member {Object} calendarStoreConfig_
+         */
         calendarStoreConfig_: {
             autoLoad: true,
             url     : '../../examples/calendar/basic/data/calendars.json'
         },
-
+        /**
+         * Config options for Neo.calendar.store.Events.
+         * The calendar view model relies on it.
+         * @member {Object} eventStoreConfig_
+         */
         eventStoreConfig_: {
             autoLoad: true,
             url     : '../../examples/calendar/basic/data/events.json'
         },
-
+        /**
+         * Value for the container.Base items_ config
+         * The calendar view model relies on it.
+         * @member {Object[]} items
+         */
         items: [{
             module     : WeekComponent,
             currentDate: new Date('2021-07-20')
@@ -31,8 +62,9 @@ class MainContainer extends Viewport {
     }}
 
     /**
-     *
-     * @param config
+     * The styles for calendar events are located inside the MainContainer CSS output.
+     * We want to fetch the CSS without requiring to load the related JS module.
+     * @param {Object} config
      */
     constructor(config) {
         super(config);
