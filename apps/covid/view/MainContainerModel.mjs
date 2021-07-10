7fea1a2d94ad72fc753eee30cb6a12fc3e54c462
tobiu <tobiasuhlig78@gmail.com>
2021-07-10 22:24:04 +0200

    #2595 Covid.view..MainContainerModel: base class

---
+++ apps/covid/view/MainContainerModel.mjs
@@ -0,0 +1,28 @@
+import Component from '../../../src/model/Component.mjs';
+
+/**
+ * @class Covid.view..MainContainerModel
+ * @extends Neo.model.Component
+ */
+class MainContainerModel extends Component {
+    static getConfig() {return {
+        /**
+         * @member {String} className='Covid.view..MainContainerModel'
+         * @protected
+         */
+        className: 'Neo.calendar.view.MainContainerModel',
+        /**
+         * @member {Object} data
+         */
+        data: {
+            /**
+             * @member {String|null} data.country=null
+             */
+            country: null
+        }
+    }}
+}
+
+Neo.applyClassConfig(MainContainerModel);
+
+export {MainContainerModel as default};
