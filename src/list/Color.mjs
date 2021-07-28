2c66ffac08497a15cf7a37410d0af9259f6921c3
tobiu <tobiasuhlig78@gmail.com>
2021-07-28 17:43:57 +0200

    list.Color: dynamically updating the content in case selections change, adjusting the icon cls based on the selection

--- src/list/Color.mjs
+++ src/list/Color.mjs
@@ -29,11 +29,12 @@ class Color extends Base {
      * @returns {Object|Object[]|String} Either a config object to assign to the item, a vdom cn array or a html string
      */
     createItemContent(record, index) {
-        let me = this;
+        let me         = this,
+            isSelected = me.selectionModel.isSelected(me.getItemId(record[me.store.keyProperty]));
 
         return [{
             tag  : 'i',
-            cls  : ['neo-icon', 'fas', 'fa-check-square'],
+            cls  : ['neo-icon', 'fas', 'fa-check-square', `fa-${isSelected ? 'check-' : ''}square`],
             style: {
                 color: record[me.displayField]
             }
@@ -41,6 +42,14 @@ class Color extends Base {
             vtype: 'text',
             html : record[me.displayField]
         }];
+    }
+
+    /**
+     *
+     * @param {String[]} items
+     */
+    onSelect(items) {
+        this.createItems();
     }
 }
 
