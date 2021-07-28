893cc523ed06755901d9230f192a3d10da891ba1
tobiu <tobiasuhlig78@gmail.com>
2021-07-28 16:18:50 +0200

    examples.list.color: copied from examples.list.base, adjusted the class names

---
+++ examples/list/color/MainModel.mjs
@@ -0,0 +1,30 @@
+import Model  from '../../../src/data/Model.mjs';
+
+/**
+ * @class Neo.examples.list.color.MainModel
+ * @extends Neo.data.Model
+ */
+class MainModel extends Model {
+    static getConfig() {return {
+        className  : 'Neo.examples.list.color.MainModel',
+        keyProperty: 'githubId',
+
+        fields: [{
+            name: 'country',
+            type: 'String'
+        }, {
+            name: 'firstname',
+            type: 'String'
+        }, {
+            name: 'githubId',
+            type: 'String'
+        }, {
+            name: 'lastname',
+            type: 'String'
+        }]
+    }}
+}
+
+Neo.applyClassConfig(MainModel);
+
+export {MainModel as default};
