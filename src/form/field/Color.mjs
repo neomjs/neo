e32cdc89a4821d4a9c0b63b8f388dcb60bdde95e
tobiu <tobiasuhlig78@gmail.com>
2021-07-29 19:30:38 +0200

    form.field.Color: getColorIndicatorId() => cleanup

--- src/form/field/Color.mjs
+++ src/form/field/Color.mjs
@@ -77,7 +77,7 @@ class Color extends Select {
      * @returns {String}
      */
     getColorIndicatorId() {
-        return this.id + '__color-indicator'
+        return `${this.id}__color-indicator`;
     }
 }
 
