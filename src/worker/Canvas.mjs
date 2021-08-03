06448226964d11eda0b8ed7af058f883f613ac5a
tobiu <tobiasuhlig78@gmail.com>
2021-08-03 16:00:09 +0200

    worker.Canvas: base class

---
+++ src/worker/Canvas.mjs
@@ -0,0 +1,48 @@
+import Neo  from '../Neo.mjs';
+import Base from './Base.mjs';
+
+/**
+ * Experimental.
+ * The Canvas worker is responsible for dynamically manipulating offscreen canvas.
+ * See: https://developer.mozilla.org/en-US/docs/Web/API/OffscreenCanvas
+ * @class Neo.worker.Canvas
+ * @extends Neo.worker.Base
+ * @singleton
+ */
+class Canvas extends Base {
+    static getConfig() {return {
+        /**
+         * @member {String} className='Neo.worker.Canvas'
+         * @protected
+         */
+        className: 'Neo.worker.Canvas',
+        /**
+         * @member {Boolean} singleton=true
+         * @protected
+         */
+        singleton: true,
+        /**
+         * @member {String} workerId='canvas'
+         * @protected
+         */
+        workerId: 'canvas'
+    }}
+
+    /**
+     *
+     * @param {Object} config
+     */
+    constructor(config) {
+        super(config);
+
+        console.log('worker.Canvas ctor', this);
+    }
+}
+
+Neo.applyClassConfig(Canvas);
+
+let instance = Neo.create(Canvas);
+
+Neo.applyToGlobalNs(instance);
+
+export default instance;
