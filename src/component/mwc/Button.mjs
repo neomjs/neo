2e97bfcfef476c8ebbbe4cbc1ba7226f423ecfcb
tobiu <tobiasuhlig78@gmail.com>
2021-08-12 11:00:28 +0200

    Move component.mwc.Button into the repo #2696

---
+++ src/component/mwc/Button.mjs
@@ -0,0 +1,135 @@
+import Component from '../../../src/component/Base.mjs';
+
+/**
+ * @class Neo.component.mwc.Button
+ * @extends Neo.component.Base
+ */
+class Button extends Component {
+    static getConfig() {return {
+        /**
+         * @member {String} className='Neo.component.mwc.Button'
+         * @protected
+         */
+        className: 'Neo.component.mwc.Button',
+        /**
+         * @member {String} ntype='mwc-button'
+         * @protected
+         */
+        ntype: 'mwc-button',
+        /**
+         * @member {Boolean} dense_=false
+         */
+        dense_: false,
+        /**
+         * Shortcut for domListeners={click:handler}
+         * A string based value assumes that the handlerFn lives inside a ComponentController
+         * @member {Function|String|null} handler_=null
+         */
+        handler_: null,
+        /**
+         * @member {String} icon_=''
+         */
+        icon_: '',
+        /**
+         * @member {String} label_=''
+         */
+        label_: '',
+        /**
+         * @member {Boolean} outlined_=false
+         */
+        outlined_: false,
+        /**
+         * @member {Boolean} raised_=false
+         */
+        raised_: false,
+        /**
+         * @member {Boolean} unelevated_=false
+         */
+        unelevated_: false,
+        /**
+         * @member {Object} _vdom={tag:'mwc-button'}
+         */
+        _vdom:
+        {tag: 'mwc-button'}
+    }}
+
+    /**
+     * Triggered after the dense config got changed.
+     * @param {Boolean} value
+     * @param {Boolean} oldValue
+     * @protected
+     */
+    afterSetDense(value, oldValue) {
+        this.changeVdomRootKey('dense', value);
+    }
+
+    /**
+     * Triggered after the handler config got changed
+     * @param {String} value
+     * @param {String} oldValue
+     * @protected
+     */
+    afterSetHandler(value, oldValue) {
+        if (value) {
+            let me           = this,
+                domListeners = me.domListeners;
+
+            domListeners.push({click: value, scope: me});
+            me.domListeners = domListeners;
+        }
+    }
+
+    /**
+     * Triggered after the icon config got changed.
+     * @param {String} value
+     * @param {String} oldValue
+     * @protected
+     */
+    afterSetIcon(value, oldValue) {
+        this.changeVdomRootKey('icon', value);
+    }
+
+    /**
+     * Triggered after the label config got changed.
+     * @param {String} value
+     * @param {String} oldValue
+     * @protected
+     */
+    afterSetLabel(value, oldValue) {
+        this.changeVdomRootKey('label', value);
+    }
+
+    /**
+     * Triggered after the outlined config got changed.
+     * @param {Boolean} value
+     * @param {Boolean} oldValue
+     * @protected
+     */
+    afterSetOutlined(value, oldValue) {
+        this.changeVdomRootKey('outlined', value || null);
+    }
+
+    /**
+     * Triggered after the raised config got changed.
+     * @param {Boolean} value
+     * @param {Boolean} oldValue
+     * @protected
+     */
+    afterSetRaised(value, oldValue) {
+        this.changeVdomRootKey('raised', value || null);
+    }
+
+    /**
+     * Triggered after the unelevated config got changed.
+     * @param {Boolean} value
+     * @param {Boolean} oldValue
+     * @protected
+     */
+    afterSetUnelevated(value, oldValue) {
+        this.changeVdomRootKey('unelevated', value || null);
+    }
+}
+
+Neo.applyClassConfig(Button);
+
+export {Button as default};
