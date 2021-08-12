f55b9274d28530abb2b9f5b88a7d572bd5618ee2
tobiu <tobiasuhlig78@gmail.com>
2021-08-12 11:28:02 +0200

    #2699 examples.component.mwc.button: dense checkbox

--- examples/component/mwc/button/MainContainer.mjs
+++ examples/component/mwc/button/MainContainer.mjs
@@ -21,54 +21,60 @@ class MainContainer extends ConfigurationViewport {
         let me = this;
 
         return [{
-            module    :  NumberField,
-            clearable : true,
-            labelText : 'height',
-            listeners : {change: me.onConfigChange.bind(me, 'height')},
-            maxValue  : 100,
-            minValue  : 20,
-            stepSize  : 2,
-            value     : me.exampleComponent.height
+            module   : CheckBox,
+            checked  : me.exampleComponent.dense,
+            labelText: 'dense',
+            listeners: {change: me.onConfigChange.bind(me, 'dense')}
         }, {
-            module    :  TextField,
-            labelText : 'icon',
-            listeners : {change: me.onConfigChange.bind(me, 'icon')},
-            value     : me.exampleComponent.icon
+            module   : NumberField,
+            clearable: true,
+            labelText: 'height',
+            listeners: {change: me.onConfigChange.bind(me, 'height')},
+            maxValue : 100,
+            minValue : 20,
+            stepSize : 2,
+            value    : me.exampleComponent.height,
+            style    : {marginTop: '.5em'}
         }, {
-            module    :  TextField,
-            clearable : true,
-            labelText : 'label',
-            listeners : {change: me.onConfigChange.bind(me, 'label')},
-            style     : {marginTop: '.5em'},
-            value     : me.exampleComponent.label
+            module   : TextField,
+            labelText: 'icon',
+            listeners: {change: me.onConfigChange.bind(me, 'icon')},
+            value    : me.exampleComponent.icon
+        }, {
+            module   : TextField,
+            clearable: true,
+            labelText: 'label',
+            listeners: {change: me.onConfigChange.bind(me, 'label')},
+            style    : {marginTop: '.5em'},
+            value    : me.exampleComponent.label
         }, {
             module   : CheckBox,
             checked  : me.exampleComponent.outlined,
             labelText: 'outlined',
             listeners: {change: me.onConfigChange.bind(me, 'outlined')},
-            style     : {marginTop: '.5em'}
+            style    : {marginTop: '.5em'}
         }, {
             module   : CheckBox,
             checked  : me.exampleComponent.raised,
             labelText: 'raised',
             listeners: {change: me.onConfigChange.bind(me, 'raised')},
-            style     : {marginTop: '.5em'}
+            style    : {marginTop: '.5em'}
         }, {
             module   : CheckBox,
             checked  : me.exampleComponent.unelevated,
             labelText: 'unelevated',
             listeners: {change: me.onConfigChange.bind(me, 'unelevated')},
-            style     : {marginTop: '.5em'}
+            style    : {marginTop: '.5em'}
         }, {
-            module    :  NumberField,
-            clearable : true,
-            labelText : 'width',
-            listeners : {change: me.onConfigChange.bind(me, 'width')},
-            maxValue  : 300,
-            minValue  : 100,
-            stepSize  : 5,
-            value     : me.exampleComponent.width,
-            style     : {marginTop: '.5em'}
+            module   : NumberField,
+            clearable: true,
+            labelText: 'width',
+            listeners: {change: me.onConfigChange.bind(me, 'width')},
+            maxValue : 300,
+            minValue : 100,
+            stepSize : 5,
+            value    : me.exampleComponent.width,
+            style    : {marginTop: '.5em'}
         }];
     }
 
