287c99d0fbb59006ac3741cf1130f33dc4c9e7d6
tobiu <tobiasuhlig78@gmail.com>
2021-10-02 14:03:29 +0200

    #2748 Neo.examples.list.animate: copied the content of list.Base, adjusted the namespaces

---
+++ examples/list/animate/MainStore.mjs
@@ -0,0 +1,55 @@
+import MainModel from './MainModel.mjs';
+import Store     from '../../../src/data/Store.mjs';
+
+/**
+ * @class Neo.examples.list.animate.MainStore
+ * @extends Neo.data.Store
+ */
+class MainStore extends Store {
+    static getConfig() {return {
+        className  : 'Neo.examples.list.animate.MainStore',
+        keyProperty: 'githubId',
+        model      : MainModel,
+
+        data: [{
+            country  : 'Germany',
+            firstname: 'Tobias',
+            githubId : 'tobiu',
+            lastname : 'Uhlig'
+        }, {
+            country  : 'USA',
+            firstname: 'Rich',
+            githubId : 'rwaters',
+            lastname : 'Waters'
+        }, {
+            country  : 'Germany',
+            firstname: 'Nils',
+            githubId : 'mrsunshine',
+            lastname : 'Dehl'
+        }, {
+            country  : 'USA',
+            firstname: 'Gerard',
+            githubId : 'camtnbikerrwc',
+            lastname : 'Horan'
+        }, {
+            country  : 'Slovakia',
+            firstname: 'Jozef',
+            githubId : 'jsakalos',
+            lastname : 'Sakalos'
+        }, {
+            country  : 'Germany',
+            firstname: 'Bastian',
+            githubId : 'bhaustein',
+            lastname : 'Haustein'
+        }],
+
+        sorters: [{
+            property : 'firstname',
+            direction: 'ASC'
+        }]
+    }}
+}
+
+Neo.applyClassConfig(MainStore);
+
+export {MainStore as default};
