ddb138912cf38f2cc4d903e681fd485b57ff9895
tobiu <tobiasuhlig78@gmail.com>
2021-10-02 14:06:42 +0200

    #2748 examples.list.animate.MainContainer: simplified the content

--- examples/list/animate/MainContainer.mjs
+++ examples/list/animate/MainContainer.mjs
@@ -1,74 +1,24 @@
-import CheckBox              from '../../../src/form/field/CheckBox.mjs';
-import ConfigurationViewport from '../../ConfigurationViewport.mjs';
-import List                  from '../../../src/list/Base.mjs';
-import MainStore             from './MainStore.mjs';
-import NumberField           from '../../../src/form/field/Number.mjs';
+import List      from '../../../src/list/Base.mjs';
+import MainStore from './MainStore.mjs';
+import Viewport  from '../../../src/container/Viewport.mjs';
 
 /**
  * @class Neo.examples.list.animate.MainContainer
- * @extends Neo.examples.ConfigurationViewport
+ * @extends Neo.container.Viewport
  */
-class MainContainer extends ConfigurationViewport {
+class MainContainer extends Viewport {
     static getConfig() {return {
-        className           : 'Neo.examples.list.animate.MainContainer',
-        autoMount           : true,
-        configItemLabelWidth: 130,
-        configItemWidth     : 230,
-        layout              : {ntype: 'hbox', align: 'stretch'}
-    }}
+        className: 'Neo.examples.list.animate.MainContainer',
+        autoMount: true,
+        layout   : {ntype: 'vbox', align: 'stretch'},
 
-    createConfigurationComponents() {
-        let me = this;
-
-        return [{
-            module   : CheckBox,
-            checked  : me.exampleComponent.disableSelection,
-            labelText: 'disableSelection',
-            listeners: {change: me.onConfigChange.bind(me, 'disableSelection')}
-        }, {
-            module   : CheckBox,
-            checked  : me.exampleComponent.draggable,
-            labelText: 'draggable',
-            listeners: {change: me.onConfigChange.bind(me, 'draggable')},
-            style    : {marginTop: '10px'}
-        }, {
-            module   : NumberField,
-            clearable: true,
-            labelText: 'height',
-            listeners: {change: me.onConfigChange.bind(me, 'height')},
-            maxValue : 300,
-            minValue : 30,
-            stepSize : 5,
-            value    : me.exampleComponent.height,
-            style    : {marginTop: '10px'}
-        }, {
-            module   : CheckBox,
-            checked  : me.exampleComponent.useCheckBoxes,
-            labelText: 'useCheckBoxes',
-            listeners: {change: me.onConfigChange.bind(me, 'useCheckBoxes')},
-            style    : {marginTop: '10px'}
-        }, {
-            module   : NumberField,
-            clearable: true,
-            labelText: 'width',
-            listeners: {change: me.onConfigChange.bind(me, 'width')},
-            maxValue : 300,
-            minValue : 100,
-            stepSize : 5,
-            style    : {marginTop: '10px'},
-            value    : me.exampleComponent.width
-        }];
-    }
-
-    createExampleComponent() {
-        return Neo.create({
+        items: [{
             module      : List,
             animate     : true,
             displayField: 'firstname',
-            store       : MainStore,
-            width       : 100
-        });
-    }
+            store       : MainStore
+        }]
+    }}
 }
 
 Neo.applyClassConfig(MainContainer);
