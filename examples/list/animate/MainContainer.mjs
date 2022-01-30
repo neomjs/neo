2f87b6bbfcc7ccfee7ab1b59005a18b3e0aa19b0
tobiu <tobiasuhlig78@gmail.com>
2022-01-30 11:55:50 +0100

    examples.list.animate.MainContainerController #2812 => controller base class, changeSorting

--- examples/list/animate/MainContainer.mjs
+++ examples/list/animate/MainContainer.mjs
@@ -1,10 +1,11 @@
-import CheckBox    from '../../../src/form/field/CheckBox.mjs';
-import List        from './List.mjs';
-import MainStore   from './MainStore.mjs';
-import NumberField from '../../../src/form/field/Number.mjs';
-import TextField   from '../../../src/form/field/Text.mjs';
-import Toolbar     from '../../../src/container/Toolbar.mjs';
-import Viewport    from '../../../src/container/Viewport.mjs';
+import CheckBox                from '../../../src/form/field/CheckBox.mjs';
+import List                    from './List.mjs';
+import MainContainerController from './MainContainerController.mjs';
+import MainStore               from './MainStore.mjs';
+import NumberField             from '../../../src/form/field/Number.mjs';
+import TextField               from '../../../src/form/field/Text.mjs';
+import Toolbar                 from '../../../src/container/Toolbar.mjs';
+import Viewport                from '../../../src/container/Viewport.mjs';
 
 /**
  * @class Neo.examples.list.animate.MainContainer
@@ -12,10 +13,10 @@ import Viewport    from '../../../src/container/Viewport.mjs';
  */
 class MainContainer extends Viewport {
     static getConfig() {return {
-        className: 'Neo.examples.list.animate.MainContainer',
-        autoMount: true,
-        layout   : {ntype: 'vbox', align: 'stretch'},
-        sortBy   : 'firstname'
+        className : 'Neo.examples.list.animate.MainContainer',
+        autoMount : true,
+        controller: MainContainerController,
+        layout    : {ntype: 'vbox', align: 'stretch'}
     }}
 
     /**
@@ -40,14 +41,16 @@ class MainContainer extends Viewport {
                 text : 'Sort by'
             }, {
                 field       : 'firstname',
-                handler     : me.changeSorting.bind(me, 'firstname'),
+                handler     : 'changeSorting',
                 iconCls     : 'fas fa-arrow-circle-up',
                 iconPosition: 'right',
+                reference   : 'firstnameButton',
                 text        : 'Firstname'
             }, {
                 field       : 'lastname',
-                handler     : me.changeSorting.bind(me, 'lastname'),
+                handler     : 'changeSorting',
                 iconPosition: 'right',
+                reference   : 'lastnameButton',
                 text        : 'Lastname'
             }, {
                 module    : CheckBox,
@@ -78,8 +81,9 @@ class MainContainer extends Viewport {
             value               : 500,
             width               : 262
         }, {
-            module: List,
-            store : MainStore
+            module   : List,
+            reference: 'list',
+            store    : MainStore
         }];
     }
 
@@ -99,35 +103,6 @@ class MainContainer extends Viewport {
         let store = this.down({module: List}).store;
 
         store.getFilter('name').value = data.value;
-    }
-
-    /**
-     * @param {String} property
-     * @param {Object} data
-     */
-    changeSorting(property, data) {
-        let me              = this,
-            buttonFirstName = me.down({field: 'firstname'}),
-            buttonLastName  = me.down({field: 'lastname'}),
-            direction       = 'ASC',
-            store           = me.down({module: List}).store,
-            sorter          = store.sorters[0],
-            button;
-
-        button = property === 'firstname' ? buttonFirstName : buttonLastName;
-
-        if (property === me.sortBy) {
-            direction = sorter.direction === 'ASC' ? 'DESC' : 'ASC';
-        }
-
-        button.iconCls = `fas fa-arrow-circle-${direction === 'ASC' ? 'up' : 'down'}`;
-
-        button = button === buttonFirstName ? buttonLastName : buttonFirstName;
-        button.iconCls = null;
-
-        sorter.set({direction, property});
-
-        me.sortBy = property;
     }
 
     /**
---
+++ examples/list/animate/MainContainerController.mjs
@@ -0,0 +1,53 @@
+import Component from '../../../src/controller/Component.mjs';
+
+/**
+ * @class Neo.examples.list.animate.MainContainerController
+ * @extends Neo.controller.Component
+ */
+class MainContainerController extends Component {
+    static getConfig() {return {
+        /**
+         * @member {String} className='Neo.examples.list.animate.MainContainerController'
+         * @protected
+         */
+        className: 'Neo.examples.list.animate.MainContainerController',
+        /**
+         * @member {String} sortBy='firstname'
+         * @protected
+         */
+        sortBy: 'firstname'
+    }}
+
+    /**
+     * @param {Object} data
+     */
+    changeSorting(data) {
+        let me              = this,
+            buttonFirstName = me.getReference('firstnameButton'),
+            buttonLastName  = me.getReference('lastnameButton'),
+            direction       = 'ASC',
+            property        = data.component.field,
+            store           = me.getReference('list').store,
+            sorter          = store.sorters[0],
+            button;
+
+        button = property === 'firstname' ? buttonFirstName : buttonLastName;
+
+        if (property === me.sortBy) {
+            direction = sorter.direction === 'ASC' ? 'DESC' : 'ASC';
+        }
+
+        button.iconCls = `fas fa-arrow-circle-${direction === 'ASC' ? 'up' : 'down'}`;
+
+        button = button === buttonFirstName ? buttonLastName : buttonFirstName;
+        button.iconCls = null;
+
+        sorter.set({direction, property});
+
+        me.sortBy = property;
+    }
+}
+
+Neo.applyClassConfig(MainContainerController);
+
+export {MainContainerController as default};
