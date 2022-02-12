f3e9cac8cbd3f8d64d78cd0e19c578ac7626e9f1
tobiu <tobiasuhlig78@gmail.com>
2022-02-12 14:42:27 +0100

    #2874 Neo.examples.list.circle: copied the content from list.color & ns

---
+++ examples/list/circle/MainStore.mjs
@@ -0,0 +1,35 @@
+import MainModel from './MainModel.mjs';
+import Store     from '../../../src/data/Store.mjs';
+
+/**
+ * @class Neo.examples.list.circle.MainStore
+ * @extends Neo.data.Store
+ */
+class MainStore extends Store {
+    static getConfig() {return {
+        /**
+         * @member {String} className='Neo.examples.list.circle.MainStore'
+         * @protected
+         */
+        className: 'Neo.examples.list.circle.MainStore',
+        /**
+         * @member {Object[]} data
+         */
+        data: [
+            {id: 1, name: 'red'},
+            {id: 2, name: 'pink'},
+            {id: 3, name: 'orange'},
+            {id: 4, name: 'yellow'},
+            {id: 5, name: 'green'},
+            {id: 6, name: 'blue'}
+        ],
+        /**
+         * @member {Neo.data.Model} model=MainModel
+         */
+        model: MainModel
+    }}
+}
+
+Neo.applyClassConfig(MainStore);
+
+export default MainStore;
