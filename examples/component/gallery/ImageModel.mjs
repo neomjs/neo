f036fbd97608683b8317486c5be952fcf7819322
tobiu <tobiasuhlig78@gmail.com>
2022-02-21 23:03:04 +0100

    examples.component.gallery.ImageModel

---
+++ examples/component/gallery/ImageModel.mjs
@@ -0,0 +1,38 @@
+import Model from '../../../src/data/Model.mjs';
+
+/**
+ * @class Neo.examples.component.gallery.ImageModel
+ * @extends Neo.data.Model
+ */
+class ImageModel extends Model {
+    static getConfig() {return {
+        /**
+         * @member {String} className='Neo.examples.component.gallery.ImageModel'
+         * @protected
+         */
+        className: 'Neo.examples.component.gallery.ImageModel',
+        /**
+         * @member {Object[]} fields
+         */
+        fields: [{
+            name: 'firstname',
+            type: 'String'
+        }, {
+            name: 'id',
+            type: 'Integer'
+        }, {
+            name: 'image',
+            type: 'String'
+        }, {
+            name: 'isOnline',
+            type: 'Boolean'
+        }, {
+            name: 'lastname',
+            type: 'String'
+        }]
+    }}
+}
+
+Neo.applyClassConfig(ImageModel);
+
+export default ImageModel;
