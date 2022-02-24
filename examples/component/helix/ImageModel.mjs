7526020bd525bf881622976898c4dfb216e8bfc7
tobiu <tobiasuhlig78@gmail.com>
2022-02-24 21:21:15 +0100

    examples.component.helix.ImageModel & ImageStore

---
+++ examples/component/helix/ImageModel.mjs
@@ -0,0 +1,38 @@
+import Model from '../../../src/data/Model.mjs';
+
+/**
+ * @class Neo.examples.component.helix.ImageModel
+ * @extends Neo.data.Model
+ */
+class ImageModel extends Model {
+    static getConfig() {return {
+        /**
+         * @member {String} className='Neo.examples.component.helix.ImageModel'
+         * @protected
+         */
+        className: 'Neo.examples.component.helix.ImageModel',
+        /**
+         * @member {Object[]} fields
+         */
+        fields: [{
+            name: 'firstname',
+            type: 'String'
+        }, {
+            name: 'id',
+            type: 'Integer'
+        }, {
+            name: 'image',
+            type: 'String'
+        }, {
+            name: 'isOnline',
+            type: 'Boolean'
+        }, {
+            name: 'lastname',
+            type: 'String'
+        }]
+    }}
+}
+
+Neo.applyClassConfig(ImageModel);
+
+export default ImageModel;
---
+++ examples/component/helix/ImageStore.mjs
@@ -0,0 +1,32 @@
+import ImageModel from './ImageModel.mjs';
+import Store      from '../../../src/data/Store.mjs';
+
+/**
+ * @class Neo.examples.component.helix.ImageStore
+ * @extends Neo.data.Store
+ */
+class ImageStore extends Store {
+    static getConfig() {return {
+        /**
+         * @member {String} className='Neo.examples.component.helix.ImageModel'
+         * @protected
+         */
+        className: 'Neo.examples.component.helix.ImageStore',
+        /**
+         * @member {Boolean} autoLoad=true
+         */
+        autoLoad: true,
+        /**
+         * @member {Neo.data.Model} model=ImageModel
+         */
+        model: ImageModel,
+        /**
+         * @member {String} url='../../resources/examples/data/ai_contacts.json'
+         */
+        url: '../../resources/examples/data/ai_contacts.json'
+    }}
+}
+
+Neo.applyClassConfig(ImageStore);
+
+export default ImageStore;
