9b440052fbafbfde88ea08def88b2afa3f2d8d29
tobiu <tobiasuhlig78@gmail.com>
2022-04-11 12:18:40 +0200

    worker.Data: afterConnect() => cleanup

--- src/worker/Data.mjs
+++ src/worker/Data.mjs
@@ -42,10 +42,9 @@ class Data extends Base {
      *
      */
     afterConnect() {
-        let me      = this,
-            channel = new MessageChannel(),
-            port1   = channel.port1,
-            port2   = channel.port2;
+        let me             = this,
+            channel        = new MessageChannel(),
+            {port1, port2} = channel;
 
         port1.onmessage = me.onMessage.bind(me);
 
