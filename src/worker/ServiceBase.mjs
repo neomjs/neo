d14ef56ce04d477963673abcd2d6118a8fec922b
tobiu <tobiasuhlig78@gmail.com>
2022-04-11 12:20:05 +0200

    worker.ServiceBase: createMessageChannel() => cleanup

--- src/worker/ServiceBase.mjs
+++ src/worker/ServiceBase.mjs
@@ -116,10 +116,9 @@ class ServiceBase extends Base {
      * @param {Client} client
      */
     createMessageChannel(client) {
-        let me      = this,
-            channel = new MessageChannel(),
-            port1   = channel.port1,
-            port2   = channel.port2;
+        let me             = this,
+            channel        = new MessageChannel(),
+            {port1, port2} = channel;
 
         port1.onmessage = me.onMessage.bind(me);
 
