26ae2f6df662cdf9f35524e6b0e49646d530a185
tobiu <tobiasuhlig78@gmail.com>
2022-04-11 20:57:32 +0200

    create an example app shell using the CesiumJS main thread addon #2973

---
+++ examples/component/wrapper/cesiumJS/MainContainer.mjs
@@ -0,0 +1,30 @@
+import Button   from '../../../../src/button/Base.mjs';
+import Toolbar  from '../../../../src/container/Toolbar.mjs';
+import Viewport from '../../../../src/container/Viewport.mjs';
+
+/**
+ * @class Neo.examples.component.wrapper.cesiumJS.MainContainer
+ * @extends Neo.container.Viewport
+ */
+class MainContainer extends Viewport {
+    static getConfig() {return {
+        className : 'Neo.examples.component.wrapper.cesiumJS.MainContainer',
+        autoMount : true,
+        layout    : {ntype: 'vbox', align: 'stretch'},
+
+        items: [{
+            module: Toolbar,
+            flex  : 'none',
+            style : {margin: '20px'},
+            items : [{
+                module : Button,
+                iconCls: 'fa-solid fa-plane',
+                text   : 'Fly to'
+            }]
+        }]
+    }}
+}
+
+Neo.applyClassConfig(MainContainer);
+
+export default MainContainer;
