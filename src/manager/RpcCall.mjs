0757026bc770a7bec171b2ae45147a9eb7359bf2
tobiu <tobiasuhlig78@gmail.com>
2022-04-15 17:57:33 +0200

    manager.RpcCall: resolveBufferTimeout() => testing

--- src/manager/RpcCall.mjs
+++ src/manager/RpcCall.mjs
@@ -6,6 +6,17 @@ import Base from './Base.mjs';
  * @singleton
  */
 class RpcCall extends Base {
+    /**
+     * Time window in ms for buffering incoming call requests
+     * @member {Number} callBuffer=20
+     */
+    callBuffer = 20
+    /**
+     * Stores the urls of endpoints for which a setTimeout() call is in progress
+     * @member {String[]} endPointTimeouts=[]
+     */
+    endPointTimeouts = []
+
     static getConfig() {return {
         /**
          * @member {String} className='Neo.manager.RpcCall'
@@ -25,14 +36,31 @@ class RpcCall extends Base {
      * @returns {Promise<any>}
      */
     async onMessage(msg) {
-        let method = Neo.manager.RpcApi.get(`${msg.service}.${msg.method}`);
+        let me     = this,
+            method = Neo.manager.RpcApi.get(`${msg.service}.${msg.method}`),
+            url    = method.url;
 
         console.log(msg);
         console.log(method);
 
+        if (!me.endPointTimeouts.includes(url)) {
+            me.endPointTimeouts.push(url);
+
+            setTimeout(() => {
+                me.resolveBufferTimeout(url);
+            }, me.callBuffer)
+        }
+
         let response = await Neo.Fetch.get(msg);
 
         return response;
+    }
+
+    /**
+     * @param {String} url
+     */
+    resolveBufferTimeout(url) {
+        console.log('resolveBufferTimeout', url);
     }
 }
 
