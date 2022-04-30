2117537a67160dfd46bc6b724e2c78c55c38dc84
tobiu <tobiasuhlig78@gmail.com>
2022-04-30 17:35:10 +0200

    #3019 toolbar.Paging: createToolbarItems() & nav button handler fns

--- src/toolbar/Paging.mjs
+++ src/toolbar/Paging.mjs
@@ -21,6 +21,10 @@ class Paging extends Toolbar {
          */
         page_: 1,
         /**
+         * @member {Number} pageSize_=30
+         */
+        pageSize_: 30,
+        /**
          * @member {Neo.data.Store|null} store_=null
          */
         store_: null
@@ -31,14 +35,72 @@ class Paging extends Toolbar {
      */
     construct(config) {
         super.construct(config);
-        this.createToolbarItems()
+        this.createToolbarItems();
     }
 
     /**
      *
      */
     createToolbarItems() {
+        let me = this;
 
+        me.items = [{
+            handler: me.onFirstPageButtonClick.bind(me),
+            iconCls: 'fa fa-angles-left'
+        }, {
+            handler: me.onPrevPageButtonClick.bind(me),
+            iconCls: 'fa fa-angle-left',
+            style  : {marginLeft: '10px'}
+        }, {
+            handler: me.onNextPageButtonClick.bind(me),
+            iconCls: 'fa fa-angle-right',
+            style  : {marginLeft: '10px'}
+        }, {
+            handler: me.onLastPageButtonClick.bind(me),
+            iconCls: 'fa fa-angles-right',
+            style  : {marginLeft: '10px'}
+        }];
+    }
+
+    /**
+     * @returns {Number}
+     */
+    getMaxPages() {
+        return Math.floor(this.store.totalCount / this.pageSize);
+    }
+
+    /**
+     *
+     */
+    onFirstPageButtonClick() {
+        this.page = 1;
+    }
+
+    /**
+     *
+     */
+    onLastPageButtonClick() {
+        this.page = this.getMaxPages();
+    }
+
+    /**
+     *
+     */
+    onNextPageButtonClick() {
+        let me = this;
+
+        if (me.page <= me.getMaxPages()) {
+            me.page++;
+        }
+    }
+
+    /**
+     *
+     */
+    onPrevPageButtonClick() {
+        if (this.page > 1) {
+            this.page--;
+        }
     }
 }
 
