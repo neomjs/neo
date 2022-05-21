d2ffcba93741045309d3baba88b02ca1e0c6c0f4
tobiu <tobiasuhlig78@gmail.com>
2022-05-21 16:27:48 +0200

    #3082 dist/development

--- buildScripts/webpack/production/webpack.config.main.mjs
+++ buildScripts/webpack/production/webpack.config.main.mjs
@@ -1,5 +1,5 @@
-import fs   from 'fs-extra';
-import path from 'path';
+import fs                from 'fs-extra';
+import path              from 'path';
 import WebpackHookPlugin from 'webpack-hook-plugin';
 
 const cwd            = process.cwd(),
