69fba82942dbbbeca8e549ad35f40aa5b396a49a
tobiu <tobiasuhlig78@gmail.com>
2022-05-22 18:15:35 +0200

    buildScripts/main: copy resources to the dist envs for workspace based envs #3091

--- buildScripts/webpack/production/webpack.config.main.mjs
+++ buildScripts/webpack/production/webpack.config.main.mjs
@@ -5,13 +5,31 @@ import WebpackHookPlugin from 'webpack-hook-plugin';
 const cwd            = process.cwd(),
       requireJson    = path => JSON.parse(fs.readFileSync((path))),
       packageJson    = requireJson(path.resolve(cwd, 'package.json')),
-      neoPath        = packageJson.name === 'neo.mjs' ? './' : './node_modules/neo.mjs/',
+      insideNeo      = packageJson.name === 'neo.mjs',
+      neoPath        = insideNeo ? './' : './node_modules/neo.mjs/',
       buildTarget    = requireJson(path.resolve(neoPath, 'buildScripts/webpack/production/buildTarget.json')),
       filenameConfig = requireJson(path.resolve(neoPath, 'buildScripts/webpack/json/build.json')),
       entry          = {main: path.resolve(neoPath, filenameConfig.mainInput)},
       copyFolder     = path.resolve(neoPath, 'buildScripts/copyFolder.mjs'),
       faFrom         = path.resolve(cwd, 'node_modules/@fortawesome/fontawesome-free'),
-      faTo           = path.resolve(cwd, buildTarget.folder, 'resources/fontawesome-free');
+      faTo           = path.resolve(cwd, buildTarget.folder, 'resources/fontawesome-free'),
+      plugins        = [];
+
+if (!insideNeo) {
+    let resourcesPath = path.resolve(cwd, 'resources'),
+        itemPath, target;
+
+    fs.readdirSync(resourcesPath).forEach(itemName => {console.log(itemName);
+        itemPath = path.resolve(resourcesPath, itemName);
+
+        if (!isFile(itemPath) && itemName !== 'scss') {
+            target = path.resolve(cwd, buildTarget.folder, 'resources', itemName);
+
+            fs.mkdirpSync(target);
+            fs.copySync(path.join(resourcesPath, itemName), target);
+        }
+    });
+}
 
 export default {
     mode  : 'production',
@@ -21,7 +39,8 @@ export default {
     plugins: [
         new WebpackHookPlugin({
             onBuildEnd: [`node ${copyFolder} -s ${faFrom} -t ${faTo}`]
-        })
+        }),
+        ...plugins
     ],
 
     output: {
