61829093b441dacf2399b2f2c065d243dbebebe9
tobiu <tobiasuhlig78@gmail.com>
2022-07-03 18:33:06 +0200

    core.Util: cleanup

--- src/core/Util.mjs
+++ src/core/Util.mjs
@@ -40,11 +40,11 @@ class Util extends Base {
 
     /**
      * Makes the first character of a string uppercase
-     * @param {String} string
+     * @param {String} value
      * @returns {Boolean|String} Returns false for non string inputs
      */
-    static capitalize(string) {
-        return Util.isString(string) && string[0].toUpperCase() + string.slice(1);
+    static capitalize(value) {
+        return Util.isString(value) && value[0].toUpperCase() + value.slice(1);
     }
 
     /**
@@ -100,7 +100,7 @@ class Util extends Base {
     }
 
     /**
-     * Transforms all uppercase characters of a string into lowercase.
+     * Transforms all uppercase characters of a string into -lowercase.
      * Does not touch special characters.
      * @param {String} value The input containing uppercase characters
      * @returns {String} The lowercase output
