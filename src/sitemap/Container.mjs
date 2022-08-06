c5f6191b90a2b841987151fb05bcee50a17db9b5
tobiu <tobiasuhlig78@gmail.com>
2022-08-06 17:54:24 +0200

    sitemap.Container: groupStore_ #3358

--- src/sitemap/Container.mjs
+++ src/sitemap/Container.mjs
@@ -1,5 +1,6 @@
 import Base            from '../container/Base.mjs';
 import ClassSystemUtil from '../util/ClassSystem.mjs';
+import GroupStore      from './store/Groups.mjs';
 import ItemStore       from './store/Items.mjs';
 
 /**
@@ -18,10 +19,34 @@ class Container extends Base {
          */
         items: [],
         /*
+         * @member {Neo.sitemap.store.Groups|null} groupStore_=null
+         */
+        groupStore_: null,
+        /*
          * @member {Neo.sitemap.store.Items|null} itemStore_=null
          */
         itemStore_: null
     }}
+
+    /**
+     * Triggered after the itemStore config got changed
+     * @param {Neo.sitemap.store.Items|null} value
+     * @param {Neo.sitemap.store.Items|null} oldValue
+     * @protected
+     */
+    afterSetGroupStore(value, oldValue) {
+        let me = this;
+
+        value?.on({
+            filter      : 'onGroupStoreFilter',
+            load        : 'onGroupStoreLoad',
+            recordChange: 'onGroupStoreRecordChange',
+            sort        : 'onGroupStoreSort',
+            scope       : me
+        });
+
+        value?.getCount() > 0 && me.createColumns();
+    }
 
     /**
      * Triggered after the itemStore config got changed
@@ -44,6 +69,18 @@ class Container extends Base {
     }
 
     /**
+     * Triggered before the groupStore config gets changed.
+     * @param {Object|Neo.data.Store} value
+     * @param {Object|Neo.data.Store} oldValue
+     * @returns {Neo.data.Store}
+     * @protected
+     */
+    beforeSetGroupStore(value, oldValue) {
+        oldValue?.destroy();
+        return ClassSystemUtil.beforeSetInstance(value, GroupStore);
+    }
+
+    /**
      * Triggered before the itemStore config gets changed.
      * @param {Object|Neo.data.Store} value
      * @param {Object|Neo.data.Store} oldValue
@@ -60,6 +97,34 @@ class Container extends Base {
      */
     createColumns() {
         console.log('createColumns');
+    }
+
+    /**
+     *
+     */
+    onGroupStoreFilter() {
+        console.log('onItemStoreFilter');
+    }
+
+    /**
+     *
+     */
+    onGroupStoreLoad() {
+        console.log('onItemStoreLoad');
+    }
+
+    /**
+     *
+     */
+    onGroupStoreRecordChange() {
+        console.log('onItemStoreRecordChange');
+    }
+
+    /**
+     *
+     */
+    onGroupStoreSort() {
+        console.log('onItemStoreSort');
     }
 
     /**
