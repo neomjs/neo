f68bd192cb5fdffe49504b8427ef1d70670a4002
tobiu <tobiasuhlig78@gmail.com>
2022-08-06 17:38:53 +0200

    sitemap.Container: itemStore_ #3356

--- src/sitemap/Container.mjs
+++ src/sitemap/Container.mjs
@@ -1,4 +1,6 @@
-import Base from '../container/Base.mjs';
+import Base            from '../container/Base.mjs';
+import ClassSystemUtil from '../util/ClassSystem.mjs';
+import ItemStore       from './store/Items.mjs';
 
 /**
  * @class Neo.sitemap.Container
@@ -14,8 +16,79 @@ class Container extends Base {
         /*
          * @member {Object[]} items
          */
-        items: []
+        items: [],
+        /*
+         * @member {Neo.sitemap.store.Items|null} itemStore_=null
+         */
+        itemStore_: null
     }}
+
+    /**
+     * Triggered after the itemStore config got changed
+     * @param {Neo.sitemap.store.Items|null} value
+     * @param {Neo.sitemap.store.Items|null} oldValue
+     * @protected
+     */
+    afterSetItemStore(value, oldValue) {
+        let me = this;
+
+        value?.on({
+            filter      : 'onItemStoreFilter',
+            load        : 'onItemStoreLoad',
+            recordChange: 'onItemStoreRecordChange',
+            sort        : 'onItemStoreSort',
+            scope       : me
+        });
+
+        value?.getCount() > 0 && me.createColumns();
+    }
+
+    /**
+     * Triggered before the itemStore config gets changed.
+     * @param {Object|Neo.data.Store} value
+     * @param {Object|Neo.data.Store} oldValue
+     * @returns {Neo.data.Store}
+     * @protected
+     */
+    beforeSetItemStore(value, oldValue) {
+        oldValue?.destroy();
+        return ClassSystemUtil.beforeSetInstance(value, ItemStore);
+    }
+
+    /**
+     *
+     */
+    createColumns() {
+        console.log('createColumns');
+    }
+
+    /**
+     *
+     */
+    onItemStoreFilter() {
+        console.log('onItemStoreFilter');
+    }
+
+    /**
+     *
+     */
+    onItemStoreLoad() {
+        console.log('onItemStoreLoad');
+    }
+
+    /**
+     *
+     */
+    onItemStoreRecordChange() {
+        console.log('onItemStoreRecordChange');
+    }
+
+    /**
+     *
+     */
+    onItemStoreSort() {
+        console.log('onItemStoreSort');
+    }
 }
 
 Neo.applyClassConfig(Container);
