365d9b4c391365a7871ba0dcd382d90f1ec89fa7
tobiu <tobiasuhlig78@gmail.com>
2022-08-30 16:59:16 +0200

    grid.header.Button: isSorted_ #3411

--- src/grid/header/Button.mjs
+++ src/grid/header/Button.mjs
@@ -1,4 +1,5 @@
 import BaseButton from '../../button/Base.mjs';
+import NeoArray   from '../../util/Array.mjs';
 
 /**
  * @class Neo.grid.header.Button
@@ -19,8 +20,54 @@ class Button extends BaseButton {
         /**
          * @member {Array} cls=['neo-grid-header-button']
          */
-        cls: ['neo-grid-header-button']
+        cls: ['neo-grid-header-button'],
+        /**
+         * 'ASC', 'DESC' or null
+         * @member {String|null} isSorted_=null
+         * @protected
+         */
+        isSorted_: null
     }}
+
+    /**
+     * Triggered after the isSorted config got changed
+     * @param {Boolean} value
+     * @param {Boolean} oldValue
+     * @protected
+     */
+    afterSetIsSorted(value, oldValue) {
+        let me        = this,
+            cls       = me.cls,
+            container = me.up('grid-container');
+
+        switch(value) {
+            case null:
+                NeoArray.add(cls, 'neo-sort-hidden');
+                break;
+            case 'ASC':
+                NeoArray.remove(cls, 'neo-sort-desc');
+                NeoArray.remove(cls, 'neo-sort-hidden');
+                NeoArray.add(cls, 'neo-sort-asc');
+                break;
+            case 'DESC':
+                NeoArray.remove(cls, 'neo-sort-asc');
+                NeoArray.remove(cls, 'neo-sort-hidden');
+                NeoArray.add(cls, 'neo-sort-desc');
+                break;
+        }
+
+        me.cls = cls;
+
+        // testing check until all example tables have a store
+        if (!container || !container.store) {
+            return;
+        }
+
+        me.mounted && me.fire('sort', {
+            direction: value,
+            property : me.dataField
+        });
+    }
 }
 
 Neo.applyClassConfig(Button);
