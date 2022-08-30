49fcde0d166c60ee34d4346fad2c3a16e9b87ebc
tobiu <tobiasuhlig78@gmail.com>
2022-08-30 11:07:48 +0200

    #3401 examples.table.covid.TableContainer: columns, Util.mjs

--- examples/table/covid/TableContainer.mjs
+++ examples/table/covid/TableContainer.mjs
@@ -1,6 +1,7 @@
 import BaseTableContainer       from '../../../src/table/Container.mjs';
 import Store                    from './Store.mjs';
-import TableContainerController from './TableContainerController.mjs'
+import TableContainerController from './TableContainerController.mjs';
+import Util                     from './Util.mjs';
 
 /**
  * @class Neo.examples.table.covid.TableContainer
@@ -14,14 +15,83 @@ class TableContainer extends BaseTableContainer {
          */
         className: 'Neo.examples.table.covid.TableContainer',
         /**
+         * Default configs for each column
+         * @member {Object} columnDefaults
+         */
+        columnDefaults: {
+            align               : 'right',
+            defaultSortDirection: 'DESC',
+            renderer            : Util.formatNumber
+        },
+        /**
          * @member {Object[]} columns
          */
-        columns: [
-            {dataField: 'firstname', text: 'Firstname'},
-            {dataField: 'lastname',  text: 'Lastname'},
-            {dataField: 'githubId',  text: 'Github Id'},
-            {dataField: 'country',   text: 'Country'}
-        ],
+        columns: [{
+            cls      : ['neo-index-column', 'neo-table-header-button'],
+            dataField: 'index',
+            dock     : 'left',
+            minWidth : 40,
+            text     : '#',
+            renderer : Util.indexRenderer,
+            width    : 40
+        }, {
+            align               : 'left',
+            dataField           : 'country',
+            defaultSortDirection: 'ASC',
+            dock                : 'left',
+            text                : 'Country',
+            width               : 200,
+
+            renderer: data => {
+                return {
+                    cls : ['neo-country-column', 'neo-table-cell'],
+                    html: [
+                        '<div style="display: flex; align-items: center">',
+                        '<img style="height:20px; margin-right:10px; width:20px;" src="' + Util.getCountryFlagUrl(data.value) + '">' + data.value,
+                        '</div>'
+                    ].join('')
+                };
+            }
+        }, {
+            dataField: 'cases',
+            text     : 'Cases'
+        }, {
+            dataField: 'casesPerOneMillion',
+            text     : 'Cases / 1M'
+        }, {
+            dataField: 'infected',
+            text     : 'Infected',
+            renderer : data => Util.formatInfected(data)
+        }, {
+            dataField: 'active',
+            text     : 'Active',
+            renderer : data => Util.formatNumber(data, '#64B5F6')
+        },  {
+            dataField: 'recovered',
+            text     : 'Recovered',
+            renderer : data => Util.formatNumber(data, '#28ca68')
+        }, {
+            dataField: 'critical',
+            text     : 'Critical',
+            renderer : data => Util.formatNumber(data, 'orange')
+        }, {
+            dataField: 'deaths',
+            text     : 'Deaths',
+            renderer : data => Util.formatNumber(data, '#fb6767')
+        }, {
+            dataField: 'todayCases',
+            text     : 'Cases today'
+        }, {
+            dataField: 'todayDeaths',
+            text     : 'Deaths today',
+            renderer : data => Util.formatNumber(data, '#fb6767')
+        }, {
+            dataField: 'tests',
+            text     : 'Tests'
+        }, {
+            dataField: 'testsPerOneMillion',
+            text     : 'Tests / 1M'
+        }],
         /**
          * @member {Neo.controller.Component} controller=TableContainerController
          */
