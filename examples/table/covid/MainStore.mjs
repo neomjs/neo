c9f2bec3c6504e390b75ea7395a610fc59b1d91e
tobiu <tobiasuhlig78@gmail.com>
2022-08-30 10:13:41 +0200

    examples.table.covid #3401 copied the content from examples.table.container & adjusted the namespace(s)

---
+++ examples/table/covid/MainStore.mjs
@@ -0,0 +1,55 @@
+import Store from '../../../src/data/Store.mjs';
+import Model from './MainModel.mjs';
+
+/**
+ * @class Neo.examples.table.covid.MainStore
+ * @extends Neo.data.Store
+ */
+class MainStore extends Store {
+    static getConfig() {return {
+        className  : 'Neo.examples.table.covid.MainStore',
+        keyProperty: 'githubId',
+        model      : Model,
+
+        data: [{
+            country  : 'Germany',
+            firstname: 'Tobias',
+            githubId : 'tobiu',
+            lastname : 'Uhlig'
+        },
+        {
+            country  : 'USA',
+            firstname: 'Rich',
+            githubId : 'rwaters',
+            lastname : 'Waters'
+        },
+        {
+            country  : 'Germany',
+            firstname: 'Nils',
+            githubId : 'mrsunshine',
+            lastname : 'Dehl'
+        },
+        {
+            country  : 'USA',
+            firstname: 'Gerard',
+            githubId : 'camtnbikerrwc',
+            lastname : 'Horan'
+        },
+        {
+            country  : 'Slovakia',
+            firstname: 'Jozef',
+            githubId : 'jsakalos',
+            lastname : 'Sakalos'
+        },
+        {
+            country  : 'Germany',
+            firstname: 'Bastian',
+            githubId : 'bhaustein',
+            lastname : 'Haustein'
+        }]
+    }}
+}
+
+Neo.applyClassConfig(MainStore);
+
+export default MainStore;
