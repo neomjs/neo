cb0c544336934662f40b094182a5b8e584bab469
tobiu <tobiasuhlig78@gmail.com>
2022-08-30 10:55:55 +0200

    #3401 examples.table.covid.TableContainerController: addStoreItems(), loadData()

--- examples/table/covid/Store.mjs
+++ examples/table/covid/Store.mjs
@@ -11,43 +11,6 @@ class Store extends BaseStore {
         keyProperty: 'country',
         model      : Model,
 
-        data: [{
-            country  : 'Germany',
-            firstname: 'Tobias',
-            githubId : 'tobiu',
-            lastname : 'Uhlig'
-        },
-        {
-            country  : 'USA',
-            firstname: 'Rich',
-            githubId : 'rwaters',
-            lastname : 'Waters'
-        },
-        {
-            country  : 'Germany',
-            firstname: 'Nils',
-            githubId : 'mrsunshine',
-            lastname : 'Dehl'
-        },
-        {
-            country  : 'USA',
-            firstname: 'Gerard',
-            githubId : 'camtnbikerrwc',
-            lastname : 'Horan'
-        },
-        {
-            country  : 'Slovakia',
-            firstname: 'Jozef',
-            githubId : 'jsakalos',
-            lastname : 'Sakalos'
-        },
-        {
-            country  : 'Germany',
-            firstname: 'Bastian',
-            githubId : 'bhaustein',
-            lastname : 'Haustein'
-        }],
-
         sorters: [{
             property : 'active',
             direction: 'DESC'
--- examples/table/covid/TableContainerController.mjs
+++ examples/table/covid/TableContainerController.mjs
@@ -5,6 +5,11 @@ import Controller from '../../../src/controller/Component.mjs';
  * @extends Neo.controller.Component
  */
 class TableContainerController extends Controller {
+    /**
+     * @member {String} apiUrl='https://disease.sh/v3/covid-19/countries'
+     */
+    apiUrl = 'https://disease.sh/v3/covid-19/countries'
+
     static getConfig() {return {
         /**
          * @member {String} className='Neo.examples.table.covid.TableContainerController'
@@ -12,6 +17,44 @@ class TableContainerController extends Controller {
          */
         className: 'Neo.examples.table.covid.TableContainerController'
     }}
+
+    /**
+     * @param {Object} config
+     */
+    construct(config) {
+        super.construct(config);
+        this.loadData();
+    }
+
+    /**
+     * @param {Object[]} data
+     */
+    addStoreItems(data) {
+        let store = this.component.store;
+
+        data.forEach(item => {
+            if (item.country.includes('"')) {
+                item.country = item.country.replace('"', "\'");
+            }
+
+            item.casesPerOneMillion = item.casesPerOneMillion > item.cases ? 'N/A' : item.casesPerOneMillion || 0;
+            item.infected           = item.casesPerOneMillion;
+        });
+
+        store.data = data;
+    }
+
+    /**
+     *
+     */
+    loadData() {
+        let me = this;
+
+        fetch(me.apiUrl)
+            .then(response => response.json())
+            .catch(err => console.log('Can’t access ' + me.apiUrl, err))
+            .then(data => me.addStoreItems(data));
+    }
 }
 
 Neo.applyClassConfig(TableContainerController);
