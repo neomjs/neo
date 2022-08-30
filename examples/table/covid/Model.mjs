562d76bc2ec76bbed0d722ce543332b00fe038a3
tobiu <tobiasuhlig78@gmail.com>
2022-08-30 10:37:12 +0200

    #3401 examples.table.covid.Model: fields based on the covid app

--- examples/table/covid/Model.mjs
+++ examples/table/covid/Model.mjs
@@ -9,17 +9,47 @@ class Model extends BaseModel {
         className: 'Neo.examples.table.covid.Model',
 
         fields: [{
+            name: 'active',
+            type: 'Integer'
+        }, {
+            name: 'cases',
+            type: 'Integer'
+        }, {
+            name: 'casesPerOneMillion',
+            type: 'Integer'
+        }, {
             name: 'country',
-            type: 'string'
+            type: 'String'
         }, {
-            name: 'firstname',
-            type: 'string'
+            name: 'countryInfo',
+            type: 'Object' // _id, flag, iso2, iso3, lat, long
         }, {
-            name: 'githubId',
-            type: 'string'
+            name: 'critical',
+            type: 'Integer'
         }, {
-            name: 'lastname',
-            type: 'string'
+            name: 'deaths',
+            type: 'Integer'
+        }, {
+            name: 'index',
+            type: 'Integer'
+        }, {
+            name: 'infected', // renderer parses to % of population
+            type: 'Integer'
+        }, {
+            name: 'recovered',
+            type: 'Integer'
+        }, {
+            name: 'tests',
+            type: 'Integer'
+        }, {
+            name: 'testsPerOneMillion',
+            type: 'Integer'
+        }, {
+            name: 'todayCases',
+            type: 'Integer'
+        }, {
+            name: 'todayDeaths',
+            type: 'Integer'
         }]
     }}
 }
