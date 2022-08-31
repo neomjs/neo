02391b763dc74e8169a4a5d8db0728c02273c6a7
tobiu <tobiasuhlig78@gmail.com>
2022-08-31 10:33:35 +0200

    selection.grid.* #3417

---
+++ src/selection/grid/CellModel.mjs
@@ -0,0 +1,184 @@
+import Model from '../Model.mjs';
+
+/**
+ * @class Neo.selection.grid.CellModel
+ * @extends Neo.selection.Model
+ */
+class CellModel extends Model {
+    static getConfig() {return {
+        /**
+         * @member {String} className='Neo.selection.grid.CellModel'
+         * @protected
+         */
+        className: 'Neo.selection.grid.CellModel',
+        /**
+         * @member {String} ntype='selection-grid-cellmodel'
+         * @protected
+         */
+        ntype: 'selection-grid-cellmodel',
+        /**
+         * @member {String} cls='selection-cellmodel'
+         * @protected
+         */
+        cls: 'neo-selection-cellmodel'
+    }}
+
+    /**
+     *
+     */
+    addDomListener() {
+        let me           = this,
+            view         = me.view,
+            domListeners = view.domListeners;
+
+        domListeners.push({
+            click   : me.onCellClick,
+            delegate: '.neo-grid-cell',
+            scope   : me
+        });
+
+        view.domListeners = domListeners;
+    }
+
+    /**
+     * @param {Object} data
+     */
+    onCellClick(data) {
+        let me   = this,
+            id   = null,
+            path = data.path,
+            i    = 0,
+            len  = path.length;
+
+        for (; i < len; i++) {
+            if (path[i].tagName === 'td') {
+                id = path[i].id;
+                break;
+            }
+        }
+
+        if (id) {
+            me.toggleSelection(id);
+        }
+    }
+
+    /**
+     * @param {Object} data
+     */
+    onKeyDownDown(data) {
+        this.onNavKeyRow(data, 1);
+    }
+
+    /**
+     * @param {Object} data
+     */
+    onKeyDownLeft(data) {
+        this.onNavKeyColumn(data, -1);
+    }
+
+    /**
+     * @param {Object} data
+     */
+    onKeyDownRight(data) {
+        this.onNavKeyColumn(data, 1);
+    }
+
+    /**
+     * @param {Object} data
+     */
+    onKeyDownUp(data) {
+        this.onNavKeyRow(data, -1);
+    }
+
+    /**
+     * @param {Object} data
+     * @param {Number} step
+     */
+    onNavKeyColumn(data, step) {
+        let me            = this,
+            view          = me.view,
+            idArray       = data.path[0].id.split('__'),
+            currentColumn = idArray[2],
+            dataFields    = view.columns.map(c => c.dataField),
+            newIndex      = (dataFields.indexOf(currentColumn) + step) % dataFields.length,
+            id;
+
+        while (newIndex < 0) {
+            newIndex += dataFields.length;
+        }
+
+        idArray[2] = dataFields[newIndex];
+        id = idArray.join('__');
+
+        me.select(id);
+        view.focus(id);
+    }
+
+    /**
+     * @param {Object} data
+     * @param {Number} step
+     */
+    onNavKeyRow(data, step) {
+        let me       = this,
+            view     = me.view,
+            store    = view.store,
+            idArray  = data.path[0].id.split('__'),
+            recordId = idArray[1],
+            newIndex = (store.indexOf(recordId) + step) % store.getCount(),
+            id;
+
+        while (newIndex < 0) {
+            newIndex += store.getCount();
+        }
+
+        idArray[1] = store.getKeyAt(newIndex);
+        id = idArray.join('__');
+
+        me.select(id);
+        view.focus(id);
+    }
+
+    /**
+     * @param {Neo.component.Base} component
+     */
+    register(component) {
+        super.register(component);
+
+        let me   = this,
+            id   = me.id,
+            view = me.view;
+
+        if (view.keys) {
+            view.keys._keys.push(
+                {fn: 'onKeyDownDown'  ,key: 'Down'  ,scope: id},
+                {fn: 'onKeyDownLeft'  ,key: 'Left'  ,scope: id},
+                {fn: 'onKeyDownRight' ,key: 'Right' ,scope: id},
+                {fn: 'onKeyDownUp'    ,key: 'Up'    ,scope: id}
+            );
+        }
+    }
+
+    /**
+     *
+     */
+    unregister() {
+        let me   = this,
+            id   = me.id,
+            view = me.view;
+
+        if (view.keys) {
+            view.keys.removeKeys([
+                {fn: 'onKeyDownDown'  ,key: 'Down'  ,scope: id},
+                {fn: 'onKeyDownLeft'  ,key: 'Left'  ,scope: id},
+                {fn: 'onKeyDownRight' ,key: 'Right' ,scope: id},
+                {fn: 'onKeyDownUp'    ,key: 'Up'    ,scope: id}
+            ]);
+        }
+
+        super.unregister();
+    }
+}
+
+Neo.applyClassConfig(CellModel);
+
+export default CellModel;
---
+++ src/selection/grid/ColumnModel.mjs
@@ -0,0 +1,185 @@
+import Model    from '../Model.mjs';
+import VDomUtil from '../../util/VDom.mjs';
+
+/**
+ * @class Neo.selection.grid.ColumnModel
+ * @extends Neo.selection.Model
+ */
+class ColumnModel extends Model {
+    static getConfig() {return {
+        /**
+         * @member {String} className='Neo.selection.grid.ColumnModel'
+         * @protected
+         */
+        className: 'Neo.selection.grid.ColumnModel',
+        /**
+         * @member {String} ntype='selection-grid-columnmodel'
+         * @protected
+         */
+        ntype: 'selection-grid-columnmodel',
+        /**
+         * @member {String} cls='selection-columnmodel'
+         * @protected
+         */
+        cls: 'neo-selection-columnmodel'
+    }}
+
+    /**
+     *
+     */
+    addDomListener() {
+        let me           = this,
+            view         = me.view,
+            domListeners = view.domListeners;
+
+        domListeners.push({
+            click   : me.onCellClick,
+            delegate: '.neo-grid-cell',
+            scope   : me
+        });
+
+        view.domListeners = domListeners;
+    }
+
+    /**
+     * @param {Object} eventPath
+     * @returns {String|null} cellId
+     */
+    static getCellId(eventPath) {
+        let id   = null,
+            i    = 0,
+            len  = eventPath.length;
+
+        for (; i < len; i++) {
+            if (eventPath[i].tagName === 'td') {
+                id = eventPath[i].id;
+                break;
+            }
+        }
+
+        return id;
+    }
+
+    /**
+     * todo: move to table.Container or view
+     * @param {String} cellId
+     * @param {Array} columns
+     * @returns {Number} index
+     */
+    static getColumnIndex(cellId, columns) {
+        let idArray       = cellId.split('__'),
+            currentColumn = idArray[2],
+            dataFields    = columns.map(c => c.dataField);
+
+        return dataFields.indexOf(currentColumn);
+    }
+
+    /**
+     * @param {Object} data
+     */
+    onCellClick(data) {
+        let me   = this,
+            id   = ColumnModel.getCellId(data.path),
+            columnNodeIds, index, tbodyNode;
+
+        if (id) {
+            index         = ColumnModel.getColumnIndex(id, me.view.items[0].items);
+            tbodyNode     = VDomUtil.findVdomChild(me.view.vdom, {tag: 'tbody'}).vdom;
+            columnNodeIds = VDomUtil.getColumnNodesIds(tbodyNode, index);
+
+            me.select(columnNodeIds);
+        }
+    }
+
+    /**
+     * @param {Object} data
+     */
+    onKeyDownLeft(data) {
+        this.onNavKeyColumn(data, -1);
+    }
+
+    /**
+     * @param {Object} data
+     */
+    onKeyDownRight(data) {
+        this.onNavKeyColumn(data, 1);
+    }
+
+    /**
+     * @param {Object} data
+     * @param {Number} step
+     */
+    onNavKeyColumn(data, step) {
+        let me            = this,
+            idArray       = ColumnModel.getCellId(data.path).split('__'),
+            currentColumn = idArray[2],
+            view          = me.view,
+            dataFields    = view.columns.map(c => c.dataField),
+            newIndex      = (dataFields.indexOf(currentColumn) + step) % dataFields.length,
+            columnNodeIds, id, tbodyNode;
+
+        while (newIndex < 0) {
+            newIndex += dataFields.length;
+        }
+
+        idArray[2] = dataFields[newIndex];
+        id = idArray.join('__');
+
+        tbodyNode     = VDomUtil.findVdomChild(me.view.vdom, {tag: 'tbody'}).vdom;
+        columnNodeIds = VDomUtil.getColumnNodesIds(tbodyNode, newIndex);
+
+        me.select(columnNodeIds);
+        view.focus(id); // we have to focus one cell to ensure the keynav keeps working
+    }
+
+    /**
+     * @param {Neo.component.Base} component
+     */
+    register(component) {
+        super.register(component);
+
+        let me   = this,
+            id   = me.id,
+            view = me.view;
+
+        if (view.keys) {
+            view.keys._keys.push({
+                fn   : 'onKeyDownLeft',
+                key  : 'Left',
+                scope: id
+            }, {
+                fn   : 'onKeyDownRight',
+                key  : 'Right',
+                scope: id
+            });
+        }
+    }
+
+
+    /**
+     *
+     */
+    unregister() {
+        let me   = this,
+            id   = me.id,
+            view = me.view;
+
+        if (view.keys) {
+            view.keys.removeKeys([{
+                fn   : 'onKeyDownLeft',
+                key  : 'Left',
+                scope: id
+            }, {
+                fn   : 'onKeyDownRight',
+                key  : 'Right',
+                scope: id
+            }]);
+        }
+
+        super.unregister();
+    }
+}
+
+Neo.applyClassConfig(ColumnModel);
+
+export default ColumnModel;
