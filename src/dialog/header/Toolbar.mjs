4f16a238585f9ac45d744f6a82bb2a82dae522d3
tobiu <tobiasuhlig78@gmail.com>
2022-11-05 10:22:09 +0100

    #3545 header.Toolbar: first test to apply actions

--- src/dialog/header/Toolbar.mjs
+++ src/dialog/header/Toolbar.mjs
@@ -10,8 +10,39 @@ class Toolbar extends Base {
          * @member {String} className='Neo.dialog.header.Toolbar'
          * @protected
          */
-        className: 'Neo.dialog.header.Toolbar'
+        className: 'Neo.dialog.header.Toolbar',
+        /**
+         * @member {String[]|null} actions=['close','maximise']
+         */
+        actions: ['close', 'maximise']
     }}
+
+    createItems() {
+        let me    = this,
+            items = me.items || [];
+
+        if (me.actions) {
+            items.push('->');
+
+            me.actions.forEach(action => {
+                switch(action) {
+                    case 'close': {
+                        items.push({
+                            flag   : 'close-button',
+                            iconCls: 'far fa-window-close'
+                            //handler: me.closeOrHide.bind(me)
+                        });
+                        break;
+                    }
+                }
+            })
+        }
+
+        me.items = items;
+        console.log(me.actions, me.items);
+
+        super.createItems();
+    }
 }
 
 Neo.applyClassConfig(Toolbar);
