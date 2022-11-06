39496cbf413a6f9762af63903936d1b5e7ba1727
tobiu <tobiasuhlig78@gmail.com>
2022-11-06 12:49:43 +0100

    #3545 dialog.header.Toolbar: actionMap, refactoring

--- src/dialog/header/Toolbar.mjs
+++ src/dialog/header/Toolbar.mjs
@@ -5,16 +5,24 @@ import Base from '../../toolbar/Base.mjs';
  * @extends Neo.toolbar.Base
  */
 class Toolbar extends Base {
+    /**
+     * @member {Object} actionMap
+     */
+    actionMap = {
+        close   : () => {return {action: 'close',    iconCls: 'far fa-window-close'}},
+        maximize: () => {return {action: 'maximize', iconCls: 'far fa-window-maximize'}}
+    }
+    /**
+     * @member {String[]|null} actions=['close','maximize']
+     */
+    actions = ['close', 'maximize']
+
     static getConfig() {return {
         /**
          * @member {String} className='Neo.dialog.header.Toolbar'
          * @protected
          */
         className: 'Neo.dialog.header.Toolbar',
-        /**
-         * @member {String[]|null} actions=['close','maximize']
-         */
-        actions: ['close', 'maximize'],
         /**
          * @member {String} title='Dialog Title'
          */
@@ -24,24 +32,10 @@ class Toolbar extends Base {
     /**
      *
      */
-    createCustomAction() {
-        // todo
-        return {action: 'info', handler: this.fireAction, iconCls: 'fa-regular fa-circle-question'};
-    }
-
-    /**
-     *
-     */
     createItems() {
         let me      = this,
             handler = me.fireAction.bind(me),
-            items   = me.items || [],
-
-        map = {
-            close   : () => {return {action: 'close',    handler, iconCls: 'far fa-window-close'}},
-            maximize: () => {return {action: 'maximize', handler, iconCls: 'far fa-window-maximize'}},
-            default : () => me.createCustomAction()
-        };
+            items   = me.items || [];
 
         me.title && items.push({
             ntype: 'label',
@@ -54,8 +48,10 @@ class Toolbar extends Base {
             items.push('->');
 
             me.actions.forEach(action => {
-                action = map[action] || map['default'];
-                items.push(action());
+                items.push({
+                    handler,
+                    ...me.actionMap[action]()
+                })
             })
         }
 
