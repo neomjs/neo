c196d43dc5abd732b2d682edb8c812baa16ba26a
tobiu <tobiasuhlig78@gmail.com>
2022-11-06 19:22:10 +0100

    #3543 worker.ServiceBase: clearing the caches once a new app version connects

--- src/worker/ServiceBase.mjs
+++ src/worker/ServiceBase.mjs
@@ -250,13 +250,17 @@ class ServiceBase extends Base {
      * @param {Object} msg
      * @param {ExtendableMessageEvent} event
      */
-    onRegisterNeoConfig(msg, event) {
+    async onRegisterNeoConfig(msg, event) {
+        let me = this;
+
         Neo.config = Neo.config || {};
         Object.assign(Neo.config, msg.data);
 
-        this.onConnect(event.source);
+        if (me.version !== Neo.config.version) {
+            await me.clearCaches()
+        }
 
-        console.log(this.version, Neo.config.version, this);
+        me.onConnect(event.source);
     }
 
     /**
