ae4bc07f53219ca3794959edb6c38952188a6b83
tobiu <tobiasuhlig78@gmail.com>
2022-12-07 18:17:23 +0100

    buildScripts/watchThemes: add support for imported SCSS files #3580

--- buildScripts/watchThemes.mjs
+++ buildScripts/watchThemes.mjs
@@ -5,12 +5,13 @@ import path         from 'path';
 import postcss      from 'postcss';
 import sass         from 'sass';
 
-let cwd         = process.cwd(),
-    requireJson = path => JSON.parse(fs.readFileSync((path))),
-    packageJson = requireJson(path.resolve(cwd, 'package.json')),
-    neoPath     = packageJson.name === 'neo.mjs' ? './' : './node_modules/neo.mjs/',
-    mixinPath   = path.resolve(neoPath, 'resources/scss/mixins/_all.scss'),
-    scssPath    = path.resolve(cwd, 'resources/scss');
+let cwd             = process.cwd(),
+    requireJson     = path => JSON.parse(fs.readFileSync((path))),
+    packageJson     = requireJson(path.resolve(cwd, 'package.json')),
+    neoPath         = packageJson.name === 'neo.mjs' ? './' : './node_modules/neo.mjs/',
+    mixinPath       = path.resolve(neoPath, 'resources/scss/mixins/_all.scss'),
+    regexSassImport = /@import[^'"]+?['"](.+?)['"];?/g,
+    scssPath        = path.resolve(cwd, 'resources/scss');
 
 if (path.sep === '\\') {
     mixinPath = mixinPath.replace(/\\/g, '/');
@@ -47,7 +48,7 @@ function buildFile(filename) {
     fs.readFile(filePath).then(content => {
         try {
             let result = sass.renderSync({
-                data          : data + content.toString(),
+                data          : data + scssCombine(content.toString(), path.parse(filePath).dir),
                 outFile       : destPath,
                 sourceMap     : true,
                 sourceMapEmbed: false
@@ -94,6 +95,33 @@ function buildFile(filename) {
             });
         } catch(error) {
             console.log('SCSS build failed for', chalk.red(filename));
+            console.log(error);
         }
     });
+}
+
+/**
+ * @param {String} content
+ * @param {String} baseDir
+ * @returns {String}
+ */
+function scssCombine (content, baseDir) {
+    if (regexSassImport.test(content)) {
+        content = content.replace(regexSassImport, (m, capture) => {
+            let parse = path.parse(path.resolve(baseDir, capture)),
+                file  = path.resolve(`${parse.dir}/${parse.name}.scss`);
+
+            if (!fs.existsSync(file)) {
+                file = path.resolve(`${parse.dir}/_${parse.name}.scss`);
+
+                if (!fs.existsSync(file)) {
+                    return '';
+                }
+            }
+
+            return scssCombine(fs.readFileSync(file).toString(), path.dirname(file));
+        });
+    }
+
+    return content;
 }
