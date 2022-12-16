0bf34d2715952bc9a9d8057dc47a718e2d81bfdd
tobiu <tobiasuhlig78@gmail.com>
2022-12-16 13:59:06 +0100

    layout.CssGrid #1194

---
+++ src/layout/Grid.mjs
@@ -0,0 +1,42 @@
+import Base     from './Base.mjs';
+import NeoArray from '../util/Array.mjs';
+
+/**
+ * @class Neo.layout.Grid
+ * @extends Neo.layout.Base
+ */
+class Grid extends Base {
+    static getConfig() {return {
+        /**
+         * @member {String} className='Neo.layout.Grid'
+         * @protected
+         */
+        className: 'Neo.layout.Grid',
+        /**
+         * @member {String} ntype='layout-hbox'
+         * @protected
+         */
+        ntype: 'layout-grid'
+    }}
+
+    /**
+     * Applies CSS classes to the container this layout is bound to
+     */
+    applyRenderAttributes() {
+        let me         = this,
+            container  = Neo.getComponent(me.containerId),
+            wrapperCls = container?.wrapperCls || [];
+
+        if (!container) {
+            Neo.logError('layout.Grid: applyRenderAttributes -> container not yet created', me.containerId);
+        }
+
+        NeoArray.add(wrapperCls, 'neo-layout-grid');
+
+        container.wrapperCls = wrapperCls;
+    }
+}
+
+Neo.applyClassConfig(Grid);
+
+export default Grid;
