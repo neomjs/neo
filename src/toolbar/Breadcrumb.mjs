42234ad1ea089d0edbcf035d6b6f9e741994699a
tobiu <tobiasuhlig78@gmail.com>
2023-01-15 22:18:17 +0100

    toolbar.Breadcrumb #3871 store_

--- src/toolbar/Breadcrumb.mjs
+++ src/toolbar/Breadcrumb.mjs
@@ -1,4 +1,6 @@
-import Toolbar from '../toolbar/Base.mjs';
+import ClassSystemUtil from '../util/ClassSystem.mjs';
+import Store           from '../data/Store.mjs';
+import Toolbar         from '../toolbar/Base.mjs';
 
 /**
  * @class Neo.toolbar.Breadcrumb
@@ -14,8 +16,34 @@ class Breadcrumb extends Toolbar {
         /**
          * @member {Object[]} items
          */
-        items: []
+        items: [],
+        /**
+         * @member {Neo.data.Store|Object} store_=null
+         */
+        store_: null
     }}
+
+    /**
+     * Triggered after the store config got changed
+     * @param {Neo.data.Store|Object} value
+     * @param {Neo.data.Store|Object} oldValue
+     * @protected
+     */
+    afterSetStore(value, oldValue) {
+
+    }
+
+    /**
+     * Triggered before the store config gets changed
+     * @param {Neo.data.Store|Object} value
+     * @param {Neo.data.Store|Object} oldValue
+     * @returns {Neo.data.Store}
+     * @protected
+     */
+    beforeSetStore(value, oldValue) {
+        oldValue?.destroy();
+        return ClassSystemUtil.beforeSetInstance(value, Store);
+    }
 }
 
 Neo.applyClassConfig(Breadcrumb);
