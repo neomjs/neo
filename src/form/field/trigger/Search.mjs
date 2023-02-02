b6745d6e7d00ae1eb5f53a7d6c16b7def9040bc3
tobiu <tobiasuhlig78@gmail.com>
2023-02-02 11:45:18 +0100

    form.field.trigger.Search #3968

---
+++ src/form/field/trigger/Search.mjs
@@ -0,0 +1,28 @@
+import Picker from './Picker.mjs';
+
+/**
+ * @class Neo.form.field.trigger.Search
+ * @extends Neo.form.field.trigger.Picker
+ */
+class Search extends Picker {
+    static config = {
+        /**
+         * @member {String} className='Neo.form.field.trigger.Search'
+         * @protected
+         */
+        className: 'Neo.form.field.trigger.Search',
+        /**
+         * @member {String} ntype='trigger-search'
+         * @protected
+         */
+        ntype: 'trigger-search',
+        /**
+         * @member {String|null} iconCls='fas fa-magnifying-glass'
+         */
+        iconCls: 'fas fa-magnifying-glass'
+    }
+}
+
+Neo.applyClassConfig(Search);
+
+export default Search;
