f87308fce9944f700f80aceb5ec9775984e34fde
tobiu <tobiasuhlig78@gmail.com>
2023-02-19 22:46:14 +0100

    npm run inject-package-version

--- examples/ServiceWorker.mjs
+++ examples/ServiceWorker.mjs
@@ -20,9 +20,9 @@ class ServiceWorker extends ServiceBase {
          */
         singleton: true,
         /**
-         * @member {String} version='5.1.10'
+         * @member {String} version='5.1.11'
          */
-        version: '5.1.10'
+        version: '5.1.11'
     }
 
     /**
