bf6107d4a8762c9b48a1f69241bc475b2439166c
tobiu <tobiasuhlig78@gmail.com>
2023-02-20 16:45:03 +0100

    Form.Overwrites #4090

---
+++ apps/form/Overwrites.mjs
@@ -0,0 +1,13 @@
+Neo.overwrites = {
+    Neo: {
+        form: {
+            field: {
+                Text: {
+                    labelPosition_: 'inline'
+                }
+            }
+        }
+    }
+}
+
+export default Neo.overwrites;
--- apps/form/app.mjs
+++ apps/form/app.mjs
@@ -1,4 +1,5 @@
-import Viewport from './view/Viewport.mjs';
+import Overwrites from './Overwrites.mjs';
+import Viewport   from './view/Viewport.mjs';
 
 export const onStart = () => Neo.app({
     mainView: Viewport,
