264529894cd72af2090e3bcbf09b5aba34b109bd
tobiu <tobiasuhlig78@gmail.com>
2023-02-21 14:15:38 +0100

    v5.1.16

--- src/DefaultConfig.mjs
+++ src/DefaultConfig.mjs
@@ -237,12 +237,12 @@ const DefaultConfig = {
     useVdomWorker: true,
     /**
      * buildScripts/injectPackageVersion.mjs will update this value
-     * @default '5.1.15'
+     * @default '5.1.16'
      * @memberOf! module:Neo
      * @name config.version
      * @type String
      */
-    version: '5.1.15'
+    version: '5.1.16'
 };
 
 Object.assign(DefaultConfig, {
