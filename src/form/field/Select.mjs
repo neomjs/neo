509cfd2b051a1a26056f37c8cb278788d7068e3f
tobiu <tobiasuhlig78@gmail.com>
2023-04-04 16:51:22 +0200

    form.field.Select: fireChangeEvent() => cleanup

--- src/form/field/Select.mjs
+++ src/form/field/Select.mjs
@@ -297,29 +297,25 @@ class Select extends Picker {
         let me            = this,
             FormContainer = Neo.form?.Container,
             record        = me.record,
-            oldRecord;
+            oldRecord, params;
 
         if (!(me.forceSelection && !record)) {
             oldRecord = me.store.get(oldValue) || null;
 
-            me.fire('change', {
+            params = {
                 component: me,
                 oldRecord,
                 oldValue,
                 record,
                 value
-            });
+            };
+
+            me.fire('change', params);
 
             if (!me.suspendEvents) {
                 ComponentManager.getParents(me).forEach(parent => {
                     if (FormContainer && parent instanceof FormContainer) {
-                        parent.fire('fieldChange', {
-                            component: me,
-                            oldRecord,
-                            oldValue,
-                            record,
-                            value
-                        })
+                        parent.fire('fieldChange', params)
                     }
                 })
             }
