f04e7a4659f0568b24cd8ebe427bd713d581be21
tobiu <tobiasuhlig78@gmail.com>
2023-04-20 10:28:32 +0200

    form.field.ZipCode: basic first implementation #4301

---
+++ src/form/field/ZipCode.mjs
@@ -0,0 +1,26 @@
+import Text from './Text.mjs';
+
+/**
+ * An extended form.field.Text which uses an input pattern to limit the amount of character which users can enter.
+ * The first version will only support numbers. Feel free to open feature requests.
+ * @class Neo.form.field.ZipCode
+ * @extends Neo.form.field.Text
+ */
+class ZipCode extends Text {
+    static config = {
+        /**
+         * @member {String} className='Neo.form.field.ZipCode'
+         * @protected
+         */
+        className: 'Neo.form.field.ZipCode',
+        /**
+         * @member {String} ntype='zipcodefield'
+         * @protected
+         */
+        ntype: 'zipcodefield'
+    }
+}
+
+Neo.applyClassConfig(ZipCode);
+
+export default ZipCode;
