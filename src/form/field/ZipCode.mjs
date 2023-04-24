defa6ddc7827698619260ef288cb3c08bb31852c
tobiu <tobiasuhlig78@gmail.com>
2023-04-24 10:38:50 +0200

    form.field.ZipCode: removed the static input pattern (we need a country config first)

--- src/form/field/ZipCode.mjs
+++ src/form/field/ZipCode.mjs
@@ -17,11 +17,7 @@ class ZipCode extends Text {
          * @member {String} ntype='zipcodefield'
          * @protected
          */
-        ntype: 'zipcodefield',
-        /**
-         * @member {RegExp|null} inputPattern=null
-         */
-        inputPattern: /[0-9]*/
+        ntype: 'zipcodefield'
     }
 }
 
