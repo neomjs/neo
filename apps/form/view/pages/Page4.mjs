639b10d9ce116531cd8b7e09449b8efe34361973
tobiu <tobiasuhlig78@gmail.com>
2023-04-26 15:10:36 +0200

    #4323 Form.view.pages.Page4: using showErrorTexts

--- apps/form/view/pages/Page4.mjs
+++ apps/form/view/pages/Page4.mjs
@@ -16,11 +16,12 @@ class Page4 extends FormPageContainer {
          * @member {Object} itemDefaults
          */
         itemDefaults: {
-            module       : CheckBox,
-            groupRequired: true,
-            labelText    : null,
-            labelWidth   : 70,
-            name         : 'fruits'
+            module        : CheckBox,
+            groupRequired : true,
+            labelText     : null,
+            labelWidth    : 70,
+            name          : 'fruits',
+            showErrorTexts: false
         },
         /**
          * @member {Object[]} items
@@ -41,12 +42,14 @@ class Page4 extends FormPageContainer {
             value         : 'orange',
             valueLabelText: 'Orange'
         }, {
+            showErrorTexts: true, // overwriting the itemDefaults value
             value         : 'strawberry',
             valueLabelText: 'Strawberry'
         }, {
             labelText     : 'Boolean',
             groupRequired : false, // overwriting the itemDefaults value
             name          : 'boolean',
+            showErrorTexts: true,  // overwriting the itemDefaults value
             style         : {marginTop: '50px'},
             uncheckedValue: false,
             value         : true
