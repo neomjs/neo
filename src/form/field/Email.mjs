ce7fc21997f543f2a732223c271eec8c3d05fd69
tobiu <tobiasuhlig78@gmail.com>
2023-04-29 10:56:33 +0200

    form.field.Email: add an emailRegex & include it into the field validation #4347

--- src/form/field/Email.mjs
+++ src/form/field/Email.mjs
@@ -5,6 +5,13 @@ import Text from './Text.mjs';
  * @extends Neo.form.field.Text
  */
 class Email extends Text {
+    /**
+     * @member {RegExp} emailRegex
+     * @protected
+     * @static
+     */
+    static emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i
+
     static config = {
         /**
          * @member {String} className='Neo.form.field.Email'
@@ -17,9 +24,34 @@ class Email extends Text {
          */
         ntype: 'emailfield',
         /**
+         * @member {String} errorTextValidEmail='Not a valid email address'
+         */
+        errorTextValidEmail: 'Not a valid email address',
+        /**
          * @member {String} inputType='email'
          */
         inputType: 'email'
+    }
+
+    /**
+     * Checks for client-side field errors
+     * @param {Boolean} silent=true
+     * @returns {Boolean} Returns true in case there are no client-side errors
+     */
+    validate(silent=true) {
+        let me          = this,
+            returnValue = super.validate(silent);
+
+        if (returnValue) {
+            if (!Email.emailRegex.test(me.value)) {
+                me._error = me.errorTextValidEmail;
+                returnValue = false;
+            }
+        }
+
+        !returnValue && !me.clean && me.updateError(me._error, silent);
+
+        return returnValue
     }
 }
 
