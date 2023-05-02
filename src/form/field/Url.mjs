3643c5326fc4515260a750d82cb51d433cd2bc4e
tobiu <tobiasuhlig78@gmail.com>
2023-05-02 20:49:40 +0200

    form.field.Url: validate() #4364

--- src/form/field/Url.mjs
+++ src/form/field/Url.mjs
@@ -19,10 +19,57 @@ class Url extends Text {
          */
         ntype: 'urlfield',
         /**
+         * @member {String} errorTextValidUrl='Not a valid URL'
+         */
+        errorTextValidUrl: 'Not a valid URL',
+        /**
          * Value for the inputType_ textfield config
          * @member {String} inputType='url'
          */
-        inputType: 'url'
+        inputType: 'url',
+        /**
+         * Specify allowed protocols
+         * @member {String[]} protocols=['http:','https:']
+         */
+        protocols: ['http:', 'https:']
+    }
+
+    /**
+     * Checks for client-side field errors
+     * @param {Boolean} silent=true
+     * @returns {Boolean} Returns true in case there are no client-side errors
+     */
+    validate(silent=true) {
+        let me          = this,
+            returnValue = super.validate(silent),
+            value       = me.value;
+
+        if (returnValue) {
+            if (value && !me.verifyUrl(value)) {
+                me._error = me.errorTextValidUrl;
+                returnValue = false;
+            }
+        }
+
+        !returnValue && !me.clean && me.updateError(me._error, silent);
+
+        return returnValue
+    }
+
+    /**
+     * @param {String} value
+     * @returns {Boolean}
+     */
+    verifyUrl(value) {
+        let url;
+
+        try {
+            url = new URL(value);
+        } catch(e) {
+            return false
+        }
+
+        return this.protocols.includes(url.protocol)
     }
 }
 
