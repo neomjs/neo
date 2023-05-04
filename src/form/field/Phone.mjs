703a42f9e6ce2269ffbe823ff2db9fc2c7d30f3e
tobiu <tobiasuhlig78@gmail.com>
2023-05-04 13:10:23 +0200

    form.field.Phone: adjusted the inputPattern docs comment

--- src/form/field/Phone.mjs
+++ src/form/field/Phone.mjs
@@ -23,7 +23,7 @@ class Phone extends Text {
          */
         errorTextInputPattern: data => `Not a valid phone number`,
         /**
-         * @member {RegExp|null} inputPattern=/^[0-9\-+\(\) ]+$/
+         * @member {RegExp|null} inputPattern=/^\+?\(?[0-9]+\)?([\-\s\.]?[/0-9]+)*$/
          */
         inputPattern: /^\+?\(?[0-9]+\)?([\-\s\.]?[/0-9]+)*$/,
         /**
