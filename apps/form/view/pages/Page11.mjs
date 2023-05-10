d7fbad06219b593bda58eb3cd315161d2fde3683
tobiu <tobiasuhlig78@gmail.com>
2023-05-10 11:27:14 +0200

    #4406 Form.view.pages.Page11: country & zip code fields in different order

--- apps/form/view/pages/Page11.mjs
+++ apps/form/view/pages/Page11.mjs
@@ -14,35 +14,73 @@ class Page11 extends FormPageContainer {
          */
         className: 'Form.view.pages.Page11',
         /**
+         * @member {Object} itemDefaults
+         */
+        itemDefaults: {
+            itemDefaults: {
+                flex : 1,
+                style: {maxWidth: '300px'}
+            }
+        },
+        /**
          * @member {Object[]} items
          */
         items: [{
-            module        : SelectField,
-            editable      : false,
-            forceSelection: true,
-            labelText     : 'Country',
-            name          : 'page11.countryfield',
-            reference     : 'country',
-            value         : 'DE',
+            ntype : 'container',
+            layout: {ntype: 'hbox'},
 
-            store: {
-                data: [
-                    {id: 'DE',     name: 'Germany'},
-                    {id: 'Others', name: 'Others'}
-                ]
-            }
+            items : [{
+                module        : SelectField,
+                editable      : false,
+                forceSelection: true,
+                labelText     : 'Country',
+                name          : 'page11.countryfield1',
+                reference     : 'country',
+                value         : 'DE',
+
+                store: {
+                    data: [
+                        {id: 'DE',     name: 'Germany'},
+                        {id: 'Others', name: 'Others'}
+                    ]
+                }
+            }, {
+                module      : ZipCodeField,
+                countryField: 'country',
+                labelText   : 'Munich',
+                name        : 'page11.zipcodefield1',
+                required    : true,
+                style       : {marginLeft: '10px', maxWidth: '300px'},
+                value       : '80796'
+            }]
         }, {
-            module      : ZipCodeField,
-            countryField: 'country',
-            labelText   : 'Munich',
-            name        : 'page11.field1',
-            required    : true,
-            value       : '80796'
-        }, {
-            module      : ZipCodeField,
-            countryField: 'country',
-            labelText   : 'Page 11 Field 2',
-            name        : 'page11.field2'
+            ntype : 'container',
+            layout: {ntype: 'hbox', align: 'stretch'},
+
+            items: [{
+                module   : ZipCodeField,
+                labelText: 'Munich',
+                name     : 'page11.zipcodefield2',
+                required : true,
+                value    : '80796'
+            }, {
+                module        : SelectField,
+                editable      : false,
+                forceSelection: true,
+                labelText     : 'Country',
+                name          : 'page11.countryfield2',
+                reference     : 'country',
+                style         : {marginLeft: '10px', maxWidth: '300px'},
+                value         : 'DE',
+                zipCodeField  : 'page11.zipcodefield2',
+
+                store: {
+                    data: [
+                        {id: 'DE',     name: 'Germany'},
+                        {id: 'Others', name: 'Others'}
+                    ]
+                }
+            }]
         }]
     }
 }
