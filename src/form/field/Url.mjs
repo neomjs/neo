3d603184466e13a4e1b7abc98162ed06f561a670
tobiu <tobiasuhlig78@gmail.com>
2023-05-12 13:57:46 +0200

    form.field.Url: switch to a regex instead of using the URL constructor #4422

--- src/form/field/Url.mjs
+++ src/form/field/Url.mjs
@@ -19,107 +19,22 @@ class Url extends Text {
          */
         ntype: 'urlfield',
         /**
-         * @member {String} errorTextValidUrl='Not a valid URL'
+         * @member {String} errorTextInputPattern=data=>'Not a valid URL'
          */
-        errorTextValidUrl: 'Not a valid URL',
+        errorTextInputPattern: data => 'Not a valid URL',
+        /**
+         * @member {RegExp|null} inputPattern_=/^(https?:\/\/)?(([a-zA-Z0-9_-]+\.)*([a-zA-Z0-9_-]+\.[a-zA-Z]{2,}))(\/([a-zA-Z0-9._-]+)\/?)*$/
+         */
+        inputPattern_: /^(https?:\/\/)?(([a-zA-Z0-9_-]+\.)*([a-zA-Z0-9_-]+\.[a-zA-Z]{2,}))(\/([a-zA-Z0-9._-]+)\/?)*$/,
+        /**
+         * @member {Boolean} inputPatternDOM=false
+         */
+        inputPatternDOM: false,
         /**
          * Value for the inputType_ textfield config
          * @member {String} inputType='url'
          */
-        inputType: 'url',
-        /**
-         * Specify allowed protocols.
-         * 'none' means that user inputs like "www.google.com" will be considered as valid.
-         * @member {String[]} protocols=['http:','https:','none']
-         */
-        protocols: ['http:', 'https:', 'none']
-    }
-
-    /**
-     * Triggered when accessing the value config
-     * The default URL ctor has some sanity checks to convert an "almost" valid URL into a real one.
-     * E.g. new URL("http:www.google.com").href => "http://www.google.com"
-     * @param {String|null} value
-     * @returns {String|null}
-     * @protected
-     */
-    beforeGetValue(value) {
-        if (value) {
-            let me   = this,
-                href = me.getUrl(value)?.href;
-
-            if (!href && me.protocols.includes('none')) {
-                href = me.getUrl(`https://${value}`)?.href;
-
-                if (href) {
-                    href = href.replace('https://', '')
-                }
-            }
-
-            if (href) {
-                return href
-            }
-        }
-
-        return value
-    }
-
-    /**
-     * Returns false in case an URL could not get created
-     * @param {String} value
-     * @returns {Boolean|URL}
-     */
-    getUrl(value) {
-        let url;
-
-        try {
-            url = new URL(value);
-        } catch(e) {
-            return false
-        }
-
-        return url
-    }
-
-    /**
-     * Checks for client-side field errors
-     * @param {Boolean} silent=true
-     * @returns {Boolean} Returns true in case there are no client-side errors
-     */
-    validate(silent=true) {
-        let me          = this,
-            returnValue = super.validate(silent),
-            value       = me.value;
-
-        if (returnValue) {
-            if (value && !me.verifyUrl(value)) {
-                me._error = me.errorTextValidUrl;
-                returnValue = false;
-            }
-        }
-
-        !returnValue && !me.clean && me.updateError(me._error, silent);
-
-        return returnValue
-    }
-
-    /**
-     * @param {String} value
-     * @returns {Boolean}
-     */
-    verifyUrl(value) {
-        let me  = this,
-            url = me.getUrl(value);
-
-        if (!url && me.protocols.includes('none')) {
-            url = me.getUrl(`https://${value}`);console.log(url)
-        }
-
-        if (!url) {
-            return false
-        }
-
-        return me.protocols.includes(url.protocol)
+        inputType: 'url'
     }
 }
 
