b5e988c73d6734424f3090c9962a559c608c0587
tobiu <tobiasuhlig78@gmail.com>
2023-05-12 15:11:20 +0200

    form.field.Number: - testing log

--- src/form/field/Number.mjs
+++ src/form/field/Number.mjs
@@ -285,7 +285,7 @@ class Number extends Text {
         let me    = this,
             value = me.value;
 
-        if (value !== null) {console.log(value);
+        if (value !== null) {
             value = me.stepSizeDigits > 0 ? parseFloat(value) : parseInt(value);
             value = Math.max(me.minValue, value);
             value = Math.min(me.maxValue, value);
