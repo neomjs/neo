d4b598b843421cecb36e3bf8f8abca412b7072a5
Torsten <td@m-gd.com>
2023-05-18 23:17:31 +0200

    feature: Switch field

---
+++ src/form/field/Switch.mjs
@@ -0,0 +1,43 @@
+import CheckBox from './CheckBox.mjs';
+
+/**
+ * @class Neo.form.field.Switch
+ * @extends Neo.form.field.Base
+ */
+class Switch extends CheckBox {
+    static config = {
+        /**
+         * @member {String} className='Neo.form.field.Switch'
+         * @protected
+         */
+        className: 'Neo.form.field.Switch',
+        /**
+         * @member {String} ntype='switchfield'
+         * @protected
+         */
+        ntype: 'switchfield',
+        /**
+         * @member {String[]} baseCls=['neo-checkboxfield']
+         */
+        baseCls: ['neo-switchfield'],
+        /**
+         * @member {Object} _vdom
+         */
+        _vdom:
+            {cn: [
+                {tag: 'label', cls: ['neo-checkbox-label'], cn: [
+                    {tag: 'span', cls: []},
+                    {tag: 'input', cls: ['neo-checkbox-input']},
+                    {tag: 'i', cls: ['neo-checkbox-icon'], removeDom: true},
+                    {tag: 'span', cls: ['neo-checkbox-value-label']}
+                ]},
+                {cls: ['neo-error-wrapper'], removeDom: true, cn: [
+                    {cls: ['neo-error']}
+                ]}
+            ]}
+    }
+}
+
+Neo.applyClassConfig(Switch);
+
+export default Switch;
