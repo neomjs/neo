4da45c0446b31bb9c3c77a06dbe2cb1cb1a469c7
tobiu <tobiasuhlig78@gmail.com>
2023-05-19 15:06:19 +0200

    examples.container.Base (refactoring) #4459

---
+++ examples/container/base/MainContainer.mjs
@@ -0,0 +1,78 @@
+import Button    from '../../../src/button/Base.mjs';
+import Container from '../../../src/container/Base.mjs';
+
+/**
+ * @class Neo.examples.container.base.MainContainer
+ * @extends Neo.container.Base
+ */
+class MainContainer extends Container {
+    static config = {
+        className: 'Neo.examples.container.base.MainContainer',
+        autoMount: true,
+        layout   : 'vbox',
+
+        items: [{
+            ntype  : 'button',
+            iconCls: 'fa fa-home',
+            text   : 'Hello',
+            width  : 200
+        }, {
+            ntype  : 'button',
+            iconCls: 'fa fa-user',
+            text   : 'World'
+        }, {
+            ntype : 'container',
+            layout: {
+                ntype: 'hbox',
+                align: 'stretch'
+            },
+            items : [{
+                ntype  : 'button',
+                iconCls: 'fa fa-home',
+                style  : {color: 'red'},
+                text   : 'Hello2',
+                width  : 200
+            }, {
+                ntype    : 'button',
+                flex     : 1,
+                iconCls  : 'fa fa-user',
+                iconColor: 'red',
+                text     : 'World2'
+            }]
+        }, {
+            ntype: 'container',
+
+            layout: {
+                ntype: 'vbox',
+                align: 'start'
+            },
+
+            style: {
+                marginTop: '30px'
+            },
+
+            items: [{
+                ntype       : 'button',
+                iconCls     : 'fa fa-home',
+                iconPosition: 'right',
+                text        : 'Right'
+            }, {
+                ntype       : 'button',
+                flex        : 1,
+                iconCls     : 'fa fa-user',
+                iconPosition: 'top',
+                text        : 'Top'
+            }, {
+                ntype       : 'button',
+                flex        : 1,
+                iconCls     : 'fa fa-play-circle',
+                iconPosition: 'bottom',
+                text        : 'Bottom'
+            }]
+        }]
+    }
+}
+
+Neo.applyClassConfig(MainContainer);
+
+export default MainContainer;
--- examples/container/base/app.mjs
+++ examples/container/base/app.mjs
@@ -2,6 +2,6 @@ import MainContainer from './MainContainer.mjs';
 
 export const onStart = () => Neo.app({
     mainView: MainContainer,
-    name    : 'Neo.examples.container',
+    name    : 'Neo.examples.container.base',
     parentId: 'main-container'
 });
