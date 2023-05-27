87eff5b54c540f6aeb7368cf984ce3015e28b8bc
Torsten <td@m-gd.com>
2023-05-27 17:04:09 +0200

    update: match coding guidelines

--- src/container/Accordion.mjs
+++ src/container/Accordion.mjs
@@ -1,13 +1,12 @@
-import Panel         from './Panel.mjs';
-// used in code
 import AccordionItem from './AccordionItem.mjs';
 import NeoArray      from '../util/Array.mjs';
+import Panel         from './Panel.mjs';
 
 /**
  * @class Neo.container.Accordion
  * @extends Neo.container.Panel
  */
-class AccordionContainer extends Panel {
+class Accordion extends Panel {
     static config = {
         /**
          * @member {String} className='Neo.container.Accordion'
@@ -23,71 +22,73 @@ class AccordionContainer extends Panel {
          * @member {String[]} baseCls=['neo-accordion']
          */
         baseCls: ['neo-accordion'],
-
         /**
          * Add zero based numbers, which accordion items you want initially expanded
          * @member {Number[]} initialOpen=[]
          */
         initialOpen_: [],
         /**
+         * @member {Object} itemDefaults={ntype:'accordionitem'}
+         */
+        itemDefaults: {ntype: 'accordionitem'},
+        /**
+         * @member {Object[]} items=[]
+         */
+        items: [],
+        /**
          * Max number of accordion items, which can be expanded at the same time
-         * @member {Number} maxExpandedItems=[]
+         * @member {Number} maxExpandedItems=1
          */
         maxExpandedItems: 1,
         /**
          * Keep track of currently open items
-         * @member {String[]} openItems=[]
+         * @member {String[]} expandedItems=[]
          * @private
          */
-        openItems_: [],
+        expandedItems_: [],
         /**
          * Creates a top header
          * @memeber {String|null} title=null
          */
-        title_: null,
-
-        itemDefaults: {ntype: 'accordionitem'},
-        items       : []
+        title_: null
     }
 
     /**
-     * After changing initalOpen
-     *
-     * @param {Number[]} openArray
+     * @param {Number[]} value
+     * @param {Number[]} oldValue
      */
-    afterSetInitialOpen(openArray) {
-        const me        = this,
-              items     = me.items,
-              openItems = me.openItems;
+    afterSetInitialOpen(value, oldValue) {
+        const me            = this,
+              items         = me.items,
+              expandedItems = me.expandedItems;
 
-        openArray.forEach((itemNo) => {
+        value.forEach((itemNo) => {
             const id   = Neo.getId(me.itemDefaults.ntype),
                   item = items[itemNo];
 
             item.expanded = true;
             item.id = id;
-            NeoArray.add(openItems, id);
+            NeoArray.add(expandedItems, id);
         });
 
-        me.openItems = openItems;
+        me.expandedItems = expandedItems;
     }
 
     /**
      * After changes to title config, we add a header
-     * @param {String|null} newTitle
+     * @param {String|null} value
+     * @param {String|null} oldValue
      */
-    afterSetTitle(newTitle) {
+    afterSetTitle(value, oldValue) {
         const me      = this,
               titleEl = me.down({flag: 'titleEl'});
 
-        if (newTitle && !titleEl) {
-            const titleCls = me.titleCls;
-
+        if (value && !titleEl) {
             me.headers = [{
-                dock   : 'top',
-                text   : newTitle,
                 baseCls: 'neo-accordion-title',
-                cls    : ['neo-accordion-title']
+                cls    : ['neo-accordion-title'],
+                dock   : 'top',
+                text   : value
             }];
         }
     }
@@ -95,36 +96,35 @@ class AccordionContainer extends Panel {
     /**
      * Called by accordion items.
      * Checks for maxExpandedItems
-     *
      * @param {Object}                      data
      * @param {Boolean}                     data.expanded newState
      * @param {Neo.container.AccordionItem} data.target   accordion item
-     *
-     * @private
+     * @protected
      */
     childExpandChange(data) {
         const me               = this,
               maxExpandedItems = me.maxExpandedItems,
-              openItems        = me.openItems,
-              curNoOpenItems   = openItems.length,
+              expandedItems    = me.expandedItems,
+              curNoOpenItems   = expandedItems.length,
               target           = data.target,
               targetId         = target.id,
               expanded         = data.expanded;
 
         if (expanded
             && maxExpandedItems !== 0
-            && curNoOpenItems === maxExpandedItems) {
-            Neo.get(openItems[0]).expanded = false;
-            NeoArray.remove(openItems, openItems[0]);
+            && curNoOpenItems === maxExpandedItems
+        ) {
+            Neo.get(expandedItems[0]).expanded = false;
+            NeoArray.remove(expandedItems, expandedItems[0]);
         }
 
         target.expanded = expanded;
-        NeoArray.toggle(openItems, targetId);
+        NeoArray.toggle(expandedItems, targetId);
 
-        me.openItems = openItems;
+        me.expandedItems = expandedItems;
     }
 }
 
-Neo.applyClassConfig(AccordionContainer);
+Neo.applyClassConfig(Accordion);
 
-export default AccordionContainer;
+export default Accordion;
