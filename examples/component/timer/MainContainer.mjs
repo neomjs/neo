f345c03b73c32e267684e4909b30ec6a62e8514c
Torsten <td@m-gd.com>
2023-07-06 22:03:25 +0200

    added a (start/stop) timer to Neo

---
+++ examples/component/timer/MainContainer.mjs
@@ -0,0 +1,70 @@
+import ConfigurationViewport from '../../ConfigurationViewport.mjs';
+import Timer               from '../../../src/component/Timer.mjs';
+import Container             from '../../../src/container/Base.mjs';
+import TextField              from "../../../src/form/field/Text.mjs";
+import ColorField             from "../../../src/form/field/Color.mjs"
+
+/**
+ * @class Neo.examples.component.timer.MainContainer
+ * @extends Neo.examples.ConfigurationViewport
+ */
+class MainContainer extends ConfigurationViewport {
+    static config = {
+        className           : 'Neo.examples.component.timer.MainContainer',
+    }
+
+    createConfigurationComponents() {
+        let me = this;
+
+        return [{
+            module   : TextField,
+            value    : '8rem',
+            listeners: {change: me.onConfigChange.bind(me, 'dimensions')}
+        }, {
+            module   : ColorField,
+            clearable : false,
+            labelText: 'colorStart',
+            value: '#8a9b0f',
+            listeners: {change: me.onConfigChange.bind(me, 'colorStart')}
+        }, {
+            module   : ColorField,
+            clearable : false,
+            labelText: 'colorEnd',
+            value: '#940a3d',
+            listeners: {change: me.onConfigChange.bind(me, 'colorEnd')}
+        }];
+    }
+
+    createExampleComponent() {
+        return Neo.create({
+            module: Container,
+            style: {
+                overflow: 'auto',
+                maxHeight: '100%'
+            },
+            items : [{
+                html: '<h1>Configurable</h1>',
+                style: {textAlign: 'center'}
+            }, {
+                module    : Timer,
+                duration  : '20s',
+                flag      : 'timer-component',
+                dimensions: '8rem',
+            }]
+        });
+    }
+
+    /**
+     * @param {String} config
+     * @param {Object} opts
+     */
+    onConfigChange(config, opts) {
+        const timer = this.down({flag: 'timer-component'});
+
+        timer[config] = opts.value;
+    }
+}
+
+Neo.applyClassConfig(MainContainer);
+
+export default MainContainer;
---
+++ examples/component/timer/MainContainerController.mjs
@@ -0,0 +1,95 @@
+import ComponentController from '../../../src/controller/Component.mjs';
+import Toast               from '../../../src/component/Toast.mjs';
+
+/**
+ * @class Neo.examples.component.toast.MainContainerController
+ * @extends Neo.controller.Component
+ */
+class MainContainerController extends ComponentController {
+    static config = {
+        /**
+         * @member {String} className='Neo.examples.component.toast.MainContainerController'
+         * @protected
+         */
+        className: 'Neo.examples.component.toast.MainContainerController'
+    }
+
+    /**
+     * @param {Object} config
+     */
+    construct(config) {
+        super.construct(config);
+        Neo.main.addon.HighlightJS.switchTheme('dark');
+    }
+
+    /**
+     * Whenever any field changes we update the output
+     * @param {Object} data
+     * @returns {Promise<void>}
+     */
+    async onChange(data) {
+        let me      = this,
+            form    = me.getReference('form'),
+            output  = me.getReference('output'),
+            button  = me.getReference('creation-button'),
+            oVdom   = output.vdom.cn[0].cn[0],
+            isValid = await form.isValid(),
+            values;
+
+        if (Neo.isBoolean(data.value)) {
+            me.getReference('closable').value = data.value;
+        }
+
+        values = await form.getValues();
+
+        values.appName = me.component.appName;
+        button.disabled = !isValid;
+
+        if (isValid) {
+            oVdom.cn = output.itemTpl(values);
+
+            output.update();
+
+            await Neo.timeout(20)
+            me.syntaxHighlight();
+        }
+    }
+
+    /**
+     * Cleanup the values and show the toast
+     */
+    async createToast() {
+        let me     = this,
+            form   = me.getReference('form'),
+            values = await form.getValues(),
+            clear  = ['position', 'slideDirection', 'ui', 'minHeight', 'maxWidth', 'closable', 'timeout'];
+
+        // use the defaults from toast if not set
+        clear.forEach(item => {
+            if (values[item] === null) {
+                delete values[item];
+            }
+        })
+
+        values.appName = me.component.appName;
+        Neo.toast(values);
+    }
+
+    /**
+     * 3rd party tool to highlight the code
+     */
+    syntaxHighlight() {
+        let me     = this,
+            output = me.getReference('output'),
+            oVdom  = output.vdom;
+
+        Neo.main.addon.HighlightJS.syntaxHighlight({
+            appName: me.component.appName,
+            vnodeId: oVdom.cn[0].id
+        });
+    }
+}
+
+Neo.applyClassConfig(MainContainerController);
+
+export default MainContainerController;
