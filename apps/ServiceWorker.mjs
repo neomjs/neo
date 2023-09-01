8533de4ad4f96cda296b28858db3a54cbe0b7e96
tobiu <tobiasuhlig78@gmail.com>
2023-09-01 12:57:47 +0200

    v6.2.1

--- apps/ServiceWorker.mjs
+++ apps/ServiceWorker.mjs
@@ -20,9 +20,9 @@ class ServiceWorker extends ServiceBase {
          */
         singleton: true,
         /**
-         * @member {String} version='6.2.0'
+         * @member {String} version='6.2.1'
          */
-        version: '6.2.0'
+        version: '6.2.1'
     }
 
     /**
