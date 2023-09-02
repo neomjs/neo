9ccd13692b481e770bd773b7afda41bce948d624
Torsten <td@m-gd.com>
2023-09-02 22:15:30 +0200

    Adding Popover. Does not support Firefox yet.

---
+++ examples/popover/MainContainer.mjs
@@ -0,0 +1,58 @@
+import ConfigurationViewport from '../ConfigurationViewport.mjs';
+
+import Component     from '../../src/component/Base.mjs';
+import PopoverPlugin from '../../src/plugin/Popover.mjs';
+import Button        from '../../src/button/Base.mjs';
+
+/**
+ * @class Neo.examples.popover.MainContainer
+ * @extends Neo.examples.ConfigurationViewport
+ */
+class MainContainer extends ConfigurationViewport {
+    static config = {
+        className           : 'Neo.examples.popover.MainContainer',
+        autoMount           : true,
+        configItemLabelWidth: 100,
+        configItemWidth     : 230,
+        layout              : {ntype: 'hbox', align: 'stretch'},
+        cls                 : ['examples-container-accordion']
+    }
+
+
+    /**
+     * @returns {*}
+     */
+    createExampleComponent() {
+        return Neo.ntype({
+            ntype : 'container',
+            height: '30%',
+            layout: {ntype: 'vbox', align: 'center', pack: 'center'},
+            items : [{
+                module: Component,
+                height: 10
+            }, {
+                module : Button,
+                width  : 200,
+                text   : 'Click Me',
+                plugins: [{
+                    module: PopoverPlugin,
+                    align : 'bc-tc',
+                    items : [{
+                        ntype  : 'panel',
+                        headers: [{
+                            dock: 'top',
+                            html: 'HEADER'
+                        }],
+                        items  : [{
+                            html: 'This is a comment about the button'
+                        }]
+                    }]
+                }]
+            }]
+        });
+    }
+}
+
+Neo.applyClassConfig(MainContainer);
+
+export default MainContainer;
