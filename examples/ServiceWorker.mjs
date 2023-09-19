e071a74d18fc8fec21ed86b16c74114a26e12db6
Tobias Uhlig <tobiasuhlig78@gmail.com>
2023-09-19 10:51:28 +0200

    v6.5.9 (#4924)
    * button.Base: afterSetMenu() => pass the parent model, if available #4923
    
    * menu.List: minor cleanup
    
    * dependencies update
    
    * v6.5.9

--- examples/ServiceWorker.mjs
+++ examples/ServiceWorker.mjs
@@ -20,9 +20,9 @@ class ServiceWorker extends ServiceBase {
          */
         singleton: true,
         /**
-         * @member {String} version='6.5.8'
+         * @member {String} version='6.5.9'
          */
-        version: '6.5.8'
+        version: '6.5.9'
     }
 
     /**
