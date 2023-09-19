7f97878dc3faf5e1a01ba2e68a27700fc11dc859
tobiu <tobiasuhlig78@gmail.com>
2023-09-19 13:41:57 +0200

    v6.5.10

--- apps/ServiceWorker.mjs
+++ apps/ServiceWorker.mjs
@@ -20,9 +20,9 @@ class ServiceWorker extends ServiceBase {
          */
         singleton: true,
         /**
-         * @member {String} version='6.5.9'
+         * @member {String} version='6.5.10'
          */
-        version: '6.5.9'
+        version: '6.5.10'
     }
 
     /**
--- src/DefaultConfig.mjs
+++ src/DefaultConfig.mjs
@@ -236,12 +236,12 @@ const DefaultConfig = {
     useVdomWorker: true,
     /**
      * buildScripts/injectPackageVersion.mjs will update this value
-     * @default '6.5.9'
+     * @default '6.5.10'
      * @memberOf! module:Neo
      * @name config.version
      * @type String
      */
-    version: '6.5.9'
+    version: '6.5.10'
 };
 
 Object.assign(DefaultConfig, {
