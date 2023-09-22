1c2e6c7887fdddabe23e38a03136a8ce8632eaf7
Tobias Uhlig <tobiasuhlig78@gmail.com>
2023-09-22 17:55:27 +0200

    v6.7.0 (#4942)
    * util.VDom: forceSyncVdomIds() #4941
    
    * component.Base: syncVnodeTree() => doc comment update
    
    * component.DateSelector: cleanup
    
    * v6.7.0

--- src/DefaultConfig.mjs
+++ src/DefaultConfig.mjs
@@ -236,12 +236,12 @@ const DefaultConfig = {
     useVdomWorker: true,
     /**
      * buildScripts/injectPackageVersion.mjs will update this value
-     * @default '6.6.5'
+     * @default '6.7.0'
      * @memberOf! module:Neo
      * @name config.version
      * @type String
      */
-    version: '6.6.5'
+    version: '6.7.0'
 };
 
 Object.assign(DefaultConfig, {
--- src/component/Base.mjs
+++ src/component/Base.mjs
@@ -2050,13 +2050,17 @@ class Base extends CoreBase {
      * Placeholder method for util.VDom.syncVdomIds to allow overriding (disabling) it
      * @param {Neo.vdom.VNode} [vnode=this.vnode]
      * @param {Object} [vdom=this.vdom]
+     * @param {Boolean} force=false
      */
-    syncVdomIds(vnode=this.vnode, vdom=this.vdom) {
-        VDomUtil.syncVdomIds(vnode, vdom)
+    syncVdomIds(vnode=this.vnode, vdom=this.vdom, force=false) {
+        VDomUtil.syncVdomIds(vnode, vdom, force)
     }
 
     /**
-     * Placeholder method for util.VDom.syncVdomIds to allow overriding (disabling) it
+     * In case a component receives a new vnode, we want to do:
+     * - sync the vdom ids
+     * - setting rendered to true for child components
+     * - updating the parent component to ensure that the vnode tree stays persistent
      * @param {Neo.vdom.VNode} [vnode=this.vnode]
      */
     syncVnodeTree(vnode=this.vnode) {
