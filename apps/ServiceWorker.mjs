2452ed2193167692a6548793bfd5a85fe9f53428
ThorstenRaab <thorsten.raab@accenture.com>
2023-10-06 16:22:49 +0200

    v6.7.8 (#4988)
    * fixed date picker problem
    
    * v6.7.8

--- apps/ServiceWorker.mjs
+++ apps/ServiceWorker.mjs
@@ -20,9 +20,9 @@ class ServiceWorker extends ServiceBase {
          */
         singleton: true,
         /**
-         * @member {String} version='6.7.7'
+         * @member {String} version='6.7.8'
          */
-        version: '6.7.7'
+        version: '6.7.8'
     }
 
     /**
--- src/DefaultConfig.mjs
+++ src/DefaultConfig.mjs
@@ -236,12 +236,12 @@ const DefaultConfig = {
     useVdomWorker: true,
     /**
      * buildScripts/injectPackageVersion.mjs will update this value
-     * @default '6.7.7'
+     * @default '6.7.8'
      * @memberOf! module:Neo
      * @name config.version
      * @type String
      */
-    version: '6.7.7'
+    version: '6.7.8'
 };
 
 Object.assign(DefaultConfig, {
