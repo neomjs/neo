4189569c41d596369ae8c48e2fa7f0e7bfdacbb7
tobiu <tobiasuhlig78@gmail.com>
2023-10-06 10:05:03 +0200

    remove main.addon.ScrollSync #4982

--- src/DefaultConfig.mjs
+++ src/DefaultConfig.mjs
@@ -126,12 +126,12 @@ const DefaultConfig = {
      * You can also create your own addons within your workspace scope. Make sure to put them inside 'src/main/addon/'
      * and prefix them with 'WS/' inside your neo-config.json file.
      * Example: ['DragDrop', 'Stylesheet', 'WS/MyAddon']
-     * @default ['DragDrop','ScrollSync','Stylesheet']
+     * @default ['DragDrop', 'Stylesheet']
      * @memberOf! module:Neo
      * @name config.mainThreadAddons
      * @type String[]
      */
-    mainThreadAddons: ['DragDrop', 'ScrollSync', 'Stylesheet'],
+    mainThreadAddons: ['DragDrop', 'Stylesheet'],
     /**
      * Pass the URL of a JSON-file, which contains the services and methods from your backend,
      * which you want to expose to the client.
