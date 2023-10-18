9661fdec1c841dcbf86d3e311699850ed3b11554
tobiu <tobiasuhlig78@gmail.com>
2023-10-18 13:44:04 +0200

    #5030 app.mjs => imports comment

--- test/components/app.mjs
+++ test/components/app.mjs
@@ -1,5 +1,5 @@
+// Important: You need to import all classes which you want to use inside tests here
+
 import Button from '../../src/button/Base.mjs';
 
-export const onStart = () => Neo.app({
-    name: 'AppEmpty'
-})
+export const onStart = () => Neo.app({name: 'AppEmpty'})
