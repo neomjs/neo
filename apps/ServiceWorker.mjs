c20418354f41caf287eb0a13fe9d16c720f2ad61
ThorstenRaab <thorsten.raab@accenture.com>
2023-11-03 16:04:45 +0100

    v6.9.8 (#5079)

--- apps/ServiceWorker.mjs
+++ apps/ServiceWorker.mjs
@@ -20,9 +20,9 @@ class ServiceWorker extends ServiceBase {
          */
         singleton: true,
         /**
-         * @member {String} version='6.9.7'
+         * @member {String} version='6.9.8'
          */
-        version: '6.9.7'
+        version: '6.9.8'
     }
 
     /**
--- src/DefaultConfig.mjs
+++ src/DefaultConfig.mjs
@@ -236,12 +236,12 @@ const DefaultConfig = {
     useVdomWorker: true,
     /**
      * buildScripts/injectPackageVersion.mjs will update this value
-     * @default '6.9.7'
+     * @default '6.9.8'
      * @memberOf! module:Neo
      * @name config.version
      * @type String
      */
-    version: '6.9.7'
+    version: '6.9.8'
 };
 
 Object.assign(DefaultConfig, {
