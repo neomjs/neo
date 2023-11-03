54a0b09d9e0f0820eae0d8b71bab8694bab769ab
linlin <l.f.chen@sinnerschrader.com>
2023-11-03 12:05:39 +0100

    fix(dev-7228): value of input field should be trimmed before submission to backend

--- src/form/field/Text.mjs
+++ src/form/field/Text.mjs
@@ -1283,7 +1283,7 @@ class Text extends Base {
     onInputValueChange(data) {
         let me       = this,
             oldValue = me.value,
-            value    = data.value,
+            value    = data.value ? data.value.toString().trim() : me.emptyValue,
             vnode    = VNodeUtil.findChildVnode(me.vnode, {nodeName: 'input'});
 
         if (vnode) {
@@ -1482,7 +1482,7 @@ class Text extends Base {
             minLength    = me.minLength,
             required     = me.required,
             returnValue  = true,
-            value        = me.value,
+            value        = me.value ? me.value.toString().trim() : me.emptyValue,
             valueLength  = value?.toString().length,
             inputPattern = me.inputPattern,
             isEmpty      = value !== 0 && (!value || valueLength < 1),
