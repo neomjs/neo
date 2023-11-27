15f19f993fd30a1568e7c984f4586b72cdef9613
tobiu <tobiasuhlig78@gmail.com>
2023-11-27 18:50:21 +0100

    main.addon.Mwc: commented out the imports to fix the build-all. added a documentation on how to use it better now.

--- src/main/addon/Mwc.mjs
+++ src/main/addon/Mwc.mjs
@@ -8,6 +8,10 @@ import Base from '../../core/Base.mjs';
  * @material/mwc-button
  * @material/mwc-textfield
  *
+ * You also need to un-comment the related imports.
+ * Recommendation: Copy this file into your workspace (src/main/addon), adjust it there and add the custom addon
+ * into the neo-config.json of your app. E.g.: [..., "WS/Mwc"].
+ *
  * @class Neo.main.addon.Mwc
  * @extends Neo.core.Base
  * @singleton
@@ -55,10 +59,10 @@ class Mwc extends Base {
             import(
                 /* webpackIgnore: true */
                 'https://unpkg.com/@material/mwc-button@0.26.1/mwc-button.js?module'
-            );
+                );
         } else {
             // dist/development & dist/production
-            import('@material/mwc-button');
+            // import('@material/mwc-button'); // <= comment me in
         }
     }
 
@@ -70,10 +74,10 @@ class Mwc extends Base {
             import(
                 /* webpackIgnore: true */
                 'https://unpkg.com/@material/mwc-textfield@0.26.1/mwc-textfield.js?module'
-            );
+                );
         } else {
             // dist/development & dist/production
-            import('@material/mwc-textfield');
+            // import('@material/mwc-textfield'); // <= comment me in
         }
     }
 
