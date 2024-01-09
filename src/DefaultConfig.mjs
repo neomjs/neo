7805f3663db02d02df4eb066b02dfadc338c416a
ThorstenRaab <thorsten.raab@accenture.com>
2024-01-09 18:04:15 +0100

    v6.10.11

--- src/DefaultConfig.mjs
+++ src/DefaultConfig.mjs
@@ -236,12 +236,12 @@ const DefaultConfig = {
     useVdomWorker: true,
     /**
      * buildScripts/injectPackageVersion.mjs will update this value
-     * @default '6.10.10'
+     * @default '6.10.11'
      * @memberOf! module:Neo
      * @name config.version
      * @type String
      */
-    version: '6.10.10'
+    version: '6.10.11'
 };
 
 Object.assign(DefaultConfig, {
