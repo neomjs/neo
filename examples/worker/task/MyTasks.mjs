b8331dae4e5ec3e5b15daf9690be36c748808acd
tobiu <tobiasuhlig78@gmail.com>
2024-02-23 15:52:56 +0100

    Neo.examples.worker.Task #5264

---
+++ examples/worker/task/MyTasks.mjs
@@ -0,0 +1,48 @@
+import Base from '../../../src/core/Base.mjs';
+
+/**
+ * @class Neo.examples.worker.task.MyTasks
+ * @extends Neo.core.Base
+ * @singleton
+ */
+class MyTasks extends Base {
+    static config = {
+        /**
+         * @member {String} className='Neo.examples.worker.task.MyTasks'
+         * @protected
+         */
+        className: 'Neo.examples.worker.task.MyTasks',
+        /**
+         * Remote method access for other workers
+         * @member {Object} remote
+         * @protected
+         */
+        remote: {
+            app: [
+                'compute'
+            ]
+        },
+        /**
+         * @member {Boolean} singleton=true
+         * @protected
+         */
+        singleton: true
+    }
+
+    /**
+     * @param {Object} data
+     * @returns {Object}
+     */
+    compute(data) {
+        console.log('compute', {
+            data,
+            workerId: Neo.workerId
+        });
+
+        return {success: true}
+    }
+}
+
+let instance = Neo.setupClass(MyTasks);
+
+export default instance;
