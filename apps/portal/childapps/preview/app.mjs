ea1751831d5a2432ac309ae5a23362240ab9d30d
tobiu <tobiasuhlig78@gmail.com>
2024-03-01 13:00:29 +0100

    Portal app: add a child app shell for source code live previews #5277

---
+++ apps/portal/childapps/preview/app.mjs
@@ -0,0 +1,7 @@
+import MainContainer from './MainContainer.mjs';
+
+export const onStart = () => Neo.app({
+    appThemeFolder: 'covid',
+    mainView      : MainContainer,
+    name          : 'PortalPreview'
+});
