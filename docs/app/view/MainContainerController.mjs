0c3d5aeaf95e04956af78f09a8ea1f4b1b8f8864
tobiu <tobiasuhlig78@gmail.com>
2024-03-10 22:46:49 +0100

    Docs App: switch to "/" based routing #5310

--- docs/app/view/MainContainerController.mjs
+++ docs/app/view/MainContainerController.mjs
@@ -16,7 +16,15 @@ class MainContainerController extends Component {
          * @member {String} ntype='docs-maincontainer-controller'
          * @protected
          */
-        ntype: 'docs-maincontainer-controller'
+        ntype: 'docs-maincontainer-controller',
+        /**
+         * @member {Object} routes
+         * @protected
+         */
+        routes: {
+            '/viewSource/{className}'                  : 'onViewSourceRoute',
+            '/viewSource/{className}/line/{lineNumber}': 'onViewSourceRoute'
+        }
     }
 
     /**
@@ -117,39 +125,6 @@ class MainContainerController extends Component {
     }
 
     /**
-     * @param {Object} value
-     * @param {Object} oldValue
-     */
-    onHashChange(value, oldValue) {return; // todo
-        let me                  = this,
-            hash                = value?.hash,
-            contentTabContainer = me.getReference('content-tabcontainer'),
-            structureStore      = me.getReference('api-treelist').store,
-            record, tab;
-
-        if (hash?.hasOwnProperty('viewSource')) {
-            record = structureStore.find('className', hash.viewSource)[0];
-
-            if (record) {
-                tab = contentTabContainer.add({
-                    ntype        : 'classdetails-sourceviewcomponent',
-                    id           : hash.viewSource + '__source',
-                    line         : hash.line,
-                    structureData: record,
-
-                    tabButtonConfig: {
-                        iconCls: 'fa fa-code',
-                        text   : record.name
-                    }
-                });
-
-                // adjust the highlighted line for already added source view tabs
-                tab.line = hash.line;
-            }
-        }
-    }
-
-    /**
      * @param {Object} data
      */
     onNavigationSearchFieldChange(data) {
@@ -165,7 +140,7 @@ class MainContainerController extends Component {
      * @param {Object} data
      */
     onNavTabContainerResize(data) {
-        console.log('onNavTabContainerResize', data)
+        // console.log('onNavTabContainerResize', data)
     }
 
     /**
@@ -242,6 +217,39 @@ class MainContainerController extends Component {
                 text   : record.name
             }
         })
+    }
+
+    /**
+     * @param {Object} data
+     * @param {String} data.className
+     * @param {String} data.lineNumber
+     */
+    onViewSourceRoute(data) {
+        let me                  = this,
+            className           = data.className,
+            lineNumber          = data.lineNumber && parseInt(data.lineNumber) || null,
+            contentTabContainer = me.getReference('content-tabcontainer'),
+            structureStore      = me.getReference('api-treelist').store,
+            record              = structureStore.find('className', className)[0],
+            tab;
+
+        if (record) {
+            tab = contentTabContainer.add({
+                ntype        : 'classdetails-sourceviewcomponent',
+                id           : className + '__source',
+                line         : lineNumber,
+                structureData: record,
+
+                tabButtonConfig: {
+                    iconCls: 'fa fa-code',
+                    text   : record.name
+                }
+            });
+
+
+            // adjust the highlighted line for already added source view tabs
+            tab.line = lineNumber
+        }
     }
 }
 
