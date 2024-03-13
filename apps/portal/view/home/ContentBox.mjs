d9b26f8a4e8c3ad19c8bdd8fb30a7c6059650c1e
tobiu <tobiasuhlig78@gmail.com>
2024-03-13 17:34:50 +0100

    #5328 Portal.view.home.ContentBox: used inside the MainContainer

--- apps/portal/view/home/ContentBox.mjs
+++ apps/portal/view/home/ContentBox.mjs
@@ -17,10 +17,46 @@ class ContentBox extends Base {
          */
         baseCls: ['portal-content-box'],
         /**
+         * @member {String[]|null} content_=null
+         */
+        content_: null,
+        /**
+         * @member {String|null} header_=null
+         */
+        header_: null,
+        /**
          * @member {Object} _vdom
          */
         _vdom:
-        {}
+        {cn: [
+            {tag: 'h3', cls: ['portal-content-box-headline']},
+            {tag: 'ul', cls: ['portal-content-box-content'], cn: []}
+        ]}
+    }
+
+    /**
+     * Triggered after the content config got changed
+     * @param {String[]|null} value
+     * @param {String[]|null} oldValue
+     * @protected
+     */
+    afterSetContent(value, oldValue) {
+        value?.forEach(item => {
+            this.vdom.cn[1].cn.push({tag: 'li', html: item})
+        })
+
+        this.update()
+    }
+
+    /**
+     * Triggered after the header config got changed
+     * @param {String|null} value
+     * @param {String|null} oldValue
+     * @protected
+     */
+    afterSetHeader(value, oldValue) {
+        this.vdom.cn[0].html = value;
+        this.update()
     }
 }
 
