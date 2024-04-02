2f35fae0c235150b420d8e3f71f096432422f20d
tobiu <tobiasuhlig78@gmail.com>
2024-04-02 14:38:12 +0200

    #5377 child first and parent first tests

--- test/components/files/component/Base.mjs
+++ test/components/files/component/Base.mjs
@@ -1,14 +1,44 @@
 StartTest(t => {
     t.it('Checking colliding vdom updates', async t => {
-        Neo.worker.App.createNeoInstance({
+        const containerId = await Neo.worker.App.createNeoInstance({
             ntype : 'container',
             height: 250,
             style : {backgroundColor: 'red'},
             width : 300
         });
 
+        t.is(containerId, 'neo-container-1');
 
         await t.waitForSelector('.neo-container');
-        t.diag('Container got rendered.')
+        t.diag('Container got rendered.');
+
+        const componentId = await Neo.worker.App.createNeoInstance({
+            ntype   : 'component',
+            height  : 150,
+            parentId: containerId,
+            style   : {backgroundColor: 'blue'},
+            width   : 150
+        });
+
+        t.is(componentId, 'neo-component-1');
+        t.diag('Component got rendered.');
+
+        t.diag('Child update before parent update');
+        Neo.worker.App.setConfigs({id: componentId, style: {backgroundColor: 'green'}});
+        Neo.worker.App.setConfigs({id: containerId, style: {backgroundColor: 'orange'}});
+
+        await t.waitFor(100);
+
+        t.is(document.getElementById(componentId).style.backgroundColor, 'green');
+        t.is(document.getElementById(containerId).style.backgroundColor, 'orange');
+
+        t.diag('Parent update before child update');
+        Neo.worker.App.setConfigs({id: containerId, style: {backgroundColor: 'pink'}});
+        Neo.worker.App.setConfigs({id: componentId, style: {backgroundColor: 'purple'}});
+
+        await t.waitFor(100);
+
+        t.is(document.getElementById(containerId).style.backgroundColor, 'pink');
+        t.is(document.getElementById(componentId).style.backgroundColor, 'purple');
     });
 });
