b0c46f816aa1c789fa59e39a5b3928a1fa744bae
tobiu <tobiasuhlig78@gmail.com>
2024-06-11 15:56:06 +0200

    remove the colors demo app from this repo #5418

--- apps/colors/childapps/widget/app.mjs
+++
@@ -1,7 +0,0 @@
-import Viewport from './view/Viewport.mjs';
-
-export const onStart = () => Neo.app({
-    appThemeFolder: 'colors',
-    mainView      : Viewport,
-    name          : 'Widget'
-});
