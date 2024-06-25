bd82421fd0f2814fb50f546e907feed33b1b95b0
tobiu <tobiasuhlig78@gmail.com>
2024-06-25 20:24:00 +0200

    Portal.view.home.parts.BaseContainer #5487

---
+++ apps/portal/view/home/parts/BaseContainer.mjs
@@ -0,0 +1,31 @@
+import Container from '../../../../../src/container/Base.mjs';
+
+/**
+ * Abstract base class for all views inside the landing page
+ * @class Portal.view.home.parts.BaseContainer
+ * @extends Neo.container.Base
+ * @abstract
+ */
+class BaseContainer extends Container {
+    static config = {
+        /**
+         * @member {String} className='Portal.view.home.parts.BaseContainer'
+         * @protected
+         */
+        className: 'Portal.view.home.parts.BaseContainer',
+        /**
+         * @member {String[]} cls=['portal-home-content-view','neo-container']
+         */
+        baseCls: ['portal-home-content-view', 'neo-container']
+    }
+
+    /**
+     * Hook which will trigger whenever the container fully gets scrolled into the visible area.
+     * Override it as needed.
+     */
+    activate() {}
+}
+
+Neo.setupClass(BaseContainer);
+
+export default BaseContainer;
