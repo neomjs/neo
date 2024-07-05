0ea601220af370d4948838dfffee3ba2c7ee382a
tobiu <tobiasuhlig78@gmail.com>
2024-07-05 15:52:40 +0200

    tests/vdom/layout/Cube: unwrapping container items & changing item attributes #5534

--- test/siesta/tests/vdom/layout/Cube.mjs
+++ test/siesta/tests/vdom/layout/Cube.mjs
@@ -144,4 +144,48 @@ StartTest(t => {
             {action: 'moveNode', id: 'neo-component-6', index: 5, parentId: 'neo-vnode-2'}
         ], 'Deltas got created successfully');
     });
+
+    t.it('Unwrap Container Items & Change Item Attributes', t => {
+        oldVdom =
+        {id: 'neo-container-1', cn: [
+            {id: 'neo-vnode-1', cn: [
+                {id: 'neo-vnode-2', cn: [
+                    {id: 'neo-component-1'},
+                    {id: 'neo-component-2', style: {backgroundColor: 'red'}},
+                    {id: 'neo-component-3'},
+                    {id: 'neo-component-4', cls: ['custom-cls']},
+                    {id: 'neo-component-5'},
+                    {id: 'neo-component-6'}
+                ]}
+            ]}
+        ]};
+
+        oldVnode = VdomHelper.create(oldVdom);
+
+        vdom =
+        {id: 'neo-container-1', cn: [
+            {id: 'neo-component-1'},
+            {id: 'neo-component-2'},
+            {id: 'neo-component-3'},
+            {id: 'neo-component-4'},
+            {id: 'neo-component-5'},
+            {id: 'neo-component-6'}
+        ]};
+
+        let {deltas, vnode} = VdomHelper.update({vdom, vnode: oldVnode});
+
+        console.log(deltas);
+
+        t.is(deltas.length, 7, 'Count deltas equals 7');
+
+        t.isDeeplyStrict(deltas, [
+            {action: 'moveNode', id: 'neo-component-1', index: 0, parentId: 'neo-container-1'},
+            {action: 'moveNode', id: 'neo-component-2', index: 1, parentId: 'neo-container-1'},
+            {action: 'moveNode', id: 'neo-component-3', index: 2, parentId: 'neo-container-1'},
+            {action: 'moveNode', id: 'neo-component-4', index: 3, parentId: 'neo-container-1'},
+            {action: 'moveNode', id: 'neo-component-5', index: 4, parentId: 'neo-container-1'},
+            {action: 'moveNode', id: 'neo-component-6', index: 5, parentId: 'neo-container-1'},
+            {action: 'removeNode', id: 'neo-vnode-1', parentId: 'neo-container-1'}
+        ], 'Deltas got created successfully');
+    });
 });
