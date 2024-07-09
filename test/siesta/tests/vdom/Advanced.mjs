da614946698a080324953e231204172483236597
tobiu <tobiasuhlig78@gmail.com>
2024-07-09 22:07:01 +0200

    #5550 Testcase 2: making the test more challenging

--- test/siesta/tests/vdom/Advanced.mjs
+++ test/siesta/tests/vdom/Advanced.mjs
@@ -80,19 +80,19 @@ StartTest(t => {
             {id: 'neo-column-1', cn: [
                 {id: 'neo-event-1', cls: ['foo1'], cn: [
                     {id: 'neo-event-1__time',  html: '08:00'},
-                    {id: 'neo-event-1__title', html: 'Event 3'}
+                    {id: 'neo-event-1__title', html: 'Event 1'}
                 ]},
                 {id: 'neo-event-2', cls: ['foo2'], cn: [
                     {id: 'neo-event-2__time',  html: '10:00'},
-                    {id: 'neo-event-2__title', html: 'Event 4'}
+                    {id: 'neo-event-2__title', html: 'Event 2'}
                 ]},
                 {id: 'neo-event-3', cls: ['foo3'], cn: [
                     {id: 'neo-event-3__time',  html: '06:00'},
-                    {id: 'neo-event-3__title', html: 'Event 2'}
+                    {id: 'neo-event-3__title', html: 'Event 3'}
                 ]},
                 {id: 'neo-event-4', cn: [
                     {id: 'neo-event-4__time',  html: '08:00'},
-                    {id: 'neo-event-4__title', html: 'Event 1'}
+                    {id: 'neo-event-4__title', html: 'Event 4'}
                 ]}
             ]},
             {id: 'neo-column-2', cls: ['foo4'], cn: []}
@@ -105,43 +105,49 @@ StartTest(t => {
             {id: 'neo-column-1', cn: [
                 {id: 'neo-event-5', cn: [ // new node
                     {id: 'neo-event-5__time',  html: '08:00'},
-                    {id: 'neo-event-5__title', html: 'Event 3'}
+                    {id: 'neo-event-5__title', html: 'Event 5'}
                 ]},
                 {id: 'neo-event-6', cn: [ // new node
                     {id: 'neo-event-6__time',  html: '08:00'},
-                    {id: 'neo-event-6__title', html: 'Event 3'}
+                    {id: 'neo-event-6__title', html: 'Event 6'}
                 ]},
                 {id: 'neo-event-1', cls: ['foo1'], cn: [
-                    {id: 'neo-event-1__time',  html: '08:00'},
-                    {id: 'neo-event-1__title', html: 'Event 3'}
+                    {id: 'neo-event-4__time',  html: '08:00'},  // switched the content with event 4
+                    {id: 'neo-event-4__title', html: 'Event 4'}
                 ]},
                 {id: 'neo-event-4', cn: [
-                    {id: 'neo-event-4__time',  html: '08:00'},
-                    {id: 'neo-event-4__title', html: 'Event 1'}
+                    {id: 'neo-event-1__time',  html: '08:00'},  // switched the content with event 1
+                    {id: 'neo-event-1__title', html: 'Event 1'}
                 ]}
             ]},
             {id: 'neo-column-2', cn: [
                 {id: 'neo-event-7', cn: [ // new node
                     {id: 'neo-event-7__time',  html: '08:00'},
-                    {id: 'neo-event-7__title', html: 'Event 3'}
+                    {id: 'neo-event-7__title', html: 'Event 7'}
                 ]},
-                {id: 'neo-event-2', cls: ['foo2'], cn: [ // moved from column-1
+                {id: 'neo-event-2', cn: [ // moved from column-1
                     {id: 'neo-event-2__time',  html: '10:00'},
-                    {id: 'neo-event-2__title', html: 'Event 4'}
+                    {id: 'neo-event-2__title', html: 'Event 2'}
                 ]},
             ]}
         ]};
 
         output = VdomHelper.update({vdom, vnode}); deltas = output.deltas; vnode = output.vnode;
 
-        t.is(deltas.length, 6, 'Count deltas equals 6');
+        t.is(deltas.length, 11, 'Count deltas equals 11');
 
         t.isDeeplyStrict(deltas, [
-            {action: 'insertNode', id: 'neo-event-5',  index: 0, parentId: 'neo-column-1', outerHTML: t.any(String)},
-            {action: 'insertNode', id: 'neo-event-6',  index: 1, parentId: 'neo-column-1', outerHTML: t.any(String)},
-            {action: 'moveNode',   id: 'neo-event-2',  index: 1, parentId: 'neo-column-2'}, // index: 0 would be correct, but a bigger index does not matter
-            {                      id: 'neo-column-2', cls: {remove: ['foo4']}},
-            {action: 'insertNode', id: 'neo-event-7',  index: 0, parentId: 'neo-column-2', outerHTML: t.any(String)},
+            {action: 'insertNode', id: 'neo-event-5',        index: 0, parentId: 'neo-column-1', outerHTML: t.any(String)},
+            {action: 'insertNode', id: 'neo-event-6',        index: 1, parentId: 'neo-column-1', outerHTML: t.any(String)},
+            {action: 'moveNode',   id: 'neo-event-1__time',  index: 0, parentId: 'neo-event-4'},
+            {action: 'moveNode',   id: 'neo-event-1__title', index: 1, parentId: 'neo-event-4'},
+            {action: 'moveNode',   id: 'neo-event-4__time',  index: 0, parentId: 'neo-event-1'},
+            {action: 'moveNode',   id: 'neo-event-4__title', index: 1, parentId: 'neo-event-1'},
+            // index: 0 would be correct, but a bigger index does not matter
+            {action: 'moveNode',   id: 'neo-event-2',        index: 1, parentId: 'neo-column-2'},
+            {                      id: 'neo-column-2',       cls: {remove: ['foo4']}},
+            {action: 'insertNode', id: 'neo-event-7',        index: 0, parentId: 'neo-column-2', outerHTML: t.any(String)},
+            {                      id: 'neo-event-2',        cls: {remove: ['foo2']}},
             {action: 'removeNode', id: 'neo-event-3'}
         ], 'deltas got created successfully');
     });
