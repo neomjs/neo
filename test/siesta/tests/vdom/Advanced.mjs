27ccd103acfa430cd83c0b8113bee4c8d47eaf2c
tobiu <tobiasuhlig78@gmail.com>
2024-07-09 22:14:00 +0200

    #5550 Testcase 2: making the test more challenging

--- test/siesta/tests/vdom/Advanced.mjs
+++ test/siesta/tests/vdom/Advanced.mjs
@@ -112,11 +112,11 @@ StartTest(t => {
                     {id: 'neo-event-6__title', html: 'Event 6'}
                 ]},
                 {id: 'neo-event-1', cls: ['foo1'], cn: [
-                    {id: 'neo-event-4__time',  html: '08:00'},  // switched the content with event 4
-                    {id: 'neo-event-4__title', html: 'Event 4'}
+                    {id: 'neo-event-4__time',  html: '08:00'},      // switched the content with event 4
+                    {id: 'neo-event-4__title', html: 'Event 4_new'} // and changed the html value
                 ]},
                 {id: 'neo-event-4', cn: [
-                    {id: 'neo-event-1__time',  html: '08:00'},  // switched the content with event 1
+                    {id: 'neo-event-1__time',  html: '08:00'},      // switched the content with event 1
                     {id: 'neo-event-1__title', html: 'Event 1'}
                 ]}
             ]},
@@ -134,7 +134,7 @@ StartTest(t => {
 
         output = VdomHelper.update({vdom, vnode}); deltas = output.deltas; vnode = output.vnode;
 
-        t.is(deltas.length, 11, 'Count deltas equals 11');
+        t.is(deltas.length, 12, 'Count deltas equals 12');
 
         t.isDeeplyStrict(deltas, [
             {action: 'insertNode', id: 'neo-event-5',        index: 0, parentId: 'neo-column-1', outerHTML: t.any(String)},
@@ -143,6 +143,7 @@ StartTest(t => {
             {action: 'moveNode',   id: 'neo-event-1__title', index: 1, parentId: 'neo-event-4'},
             {action: 'moveNode',   id: 'neo-event-4__time',  index: 0, parentId: 'neo-event-1'},
             {action: 'moveNode',   id: 'neo-event-4__title', index: 1, parentId: 'neo-event-1'},
+            {                      id: 'neo-event-4__title', innerHTML: 'Event 4_new'},
             // index: 0 would be correct, but a bigger index does not matter
             {action: 'moveNode',   id: 'neo-event-2',        index: 1, parentId: 'neo-column-2'},
             {                      id: 'neo-column-2',       cls: {remove: ['foo4']}},
