844718f9896a80a259db800d654aaa773e805c2c
tobiu <tobiasuhlig78@gmail.com>
2024-07-10 10:07:48 +0200

    #5550 imports cleanup

--- test/siesta/tests/vdom/Advanced.mjs
+++ test/siesta/tests/vdom/Advanced.mjs
@@ -1,9 +1,6 @@
 import Neo        from '../../../../src/Neo.mjs';
 import * as core  from '../../../../src/core/_export.mjs';
-import NeoArray   from '../../../../src/util/Array.mjs';
-import Style      from '../../../../src/util/Style.mjs';
 import VdomHelper from '../../../../src/vdom/Helper.mjs';
-import VDomUtil   from '../../../../src/util/VDom.mjs';
 
 let deltas, output, vdom, vnode;
 
