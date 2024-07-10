4501c08df0425f3b69dc804a08fedb0fce5a9925
tobiu <tobiasuhlig78@gmail.com>
2024-07-10 11:24:58 +0200

    #5542 TestCase 1 - Revert OP

--- test/siesta/tests/vdom/table/Container.mjs
+++ test/siesta/tests/vdom/table/Container.mjs
@@ -2,11 +2,11 @@ import Neo        from '../../../../../src/Neo.mjs';
 import * as core  from '../../../../../src/core/_export.mjs';
 import VdomHelper from '../../../../../src/vdom/Helper.mjs';
 
-let oldVdom, oldVnode, vdom;
+let deltas, output, vdom, vnode;
 
 StartTest(t => {
     t.it('Remove all Table Rows', t => {
-        oldVdom =
+        vdom =
         {id: 'neo-table-container-1', cn: [
             {tag: 'tbody', id: 'neo-table-view-1', cn: [
                 {tag: 'tr', id: 'neo-table-row-1'},
@@ -17,24 +17,49 @@ StartTest(t => {
             ]}
         ]};
 
-        let oldVnode = VdomHelper.create(oldVdom);
+        vnode = VdomHelper.create(vdom);
 
         vdom =
         {id: 'neo-table-container-1', cn: [
             {tag: 'tbody', id: 'neo-table-view-1', cn: []}
         ]};
 
-        let {deltas, vnode} = VdomHelper.update({vdom, vnode: oldVnode});
+        output = VdomHelper.update({vdom, vnode}); deltas = output.deltas; vnode = output.vnode;
 
         t.is(deltas.length, 1, 'Count deltas equals 1');
 
         t.isDeeplyStrict(deltas, [
             {action: 'removeAll', parentId: 'neo-table-view-1'}
         ], 'Deltas got created successfully');
+
+        t.diag('Revert operation');
+
+        vdom =
+        {id: 'neo-table-container-1', cn: [
+            {tag: 'tbody', id: 'neo-table-view-1', cn: [
+                {tag: 'tr', id: 'neo-table-row-1'},
+                {tag: 'tr', id: 'neo-table-row-2'},
+                {tag: 'tr', id: 'neo-table-row-3'},
+                {tag: 'tr', id: 'neo-table-row-4'},
+                {tag: 'tr', id: 'neo-table-row-5'}
+            ]}
+        ]};
+
+        output = VdomHelper.update({vdom, vnode}); deltas = output.deltas; vnode = output.vnode;
+
+        t.is(deltas.length, 5, 'Count deltas equals 5');
+
+        t.isDeeplyStrict(deltas, [
+            {action: 'insertNode', id: 'neo-table-row-1', index: 0, parentId: 'neo-table-view-1', outerHTML: '<tr id="neo-table-row-1"></tr>'},
+            {action: 'insertNode', id: 'neo-table-row-2', index: 1, parentId: 'neo-table-view-1', outerHTML: '<tr id="neo-table-row-2"></tr>'},
+            {action: 'insertNode', id: 'neo-table-row-3', index: 2, parentId: 'neo-table-view-1', outerHTML: '<tr id="neo-table-row-3"></tr>'},
+            {action: 'insertNode', id: 'neo-table-row-4', index: 3, parentId: 'neo-table-view-1', outerHTML: '<tr id="neo-table-row-4"></tr>'},
+            {action: 'insertNode', id: 'neo-table-row-5', index: 4, parentId: 'neo-table-view-1', outerHTML: '<tr id="neo-table-row-5"></tr>'}
+        ], 'Deltas got created successfully');
     });
 
     t.it('Remove all Table Rows & move 2 into a different Table', t => {
-        oldVdom =
+        vdom =
         {id: 'neo-wrapper-1', cn: [
             {id: 'neo-table-container-1', cn: [
                 {tag: 'tbody', id: 'neo-table-view-1', cn: [
@@ -50,7 +75,7 @@ StartTest(t => {
             ]}
         ]};
 
-        let oldVnode = VdomHelper.create(oldVdom);
+        vnode = VdomHelper.create(vdom);
 
         vdom =
         {id: 'neo-wrapper-1', cn: [
@@ -65,7 +90,7 @@ StartTest(t => {
             ]}
         ]};
 
-        let {deltas, vnode} = VdomHelper.update({vdom, vnode: oldVnode});
+        output = VdomHelper.update({vdom, vnode}); deltas = output.deltas; vnode = output.vnode;
 
         t.is(deltas.length, 3, 'Count deltas equals 3');
 
