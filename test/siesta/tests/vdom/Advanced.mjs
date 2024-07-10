c1f8a8c299b2d8a419cebc7cf6fa7d1488ccb154
tobiu <tobiasuhlig78@gmail.com>
2024-07-10 09:29:46 +0200

    #5550 Testcase 1 - revert OP

--- test/siesta/tests/vdom/Advanced.mjs
+++ test/siesta/tests/vdom/Advanced.mjs
@@ -72,6 +72,47 @@ StartTest(t => {
             {                    id: 'neo-event-2__time', innerHTML: '06:00'},
             {                    id: 'neo-column-2', cls: {add: ['foo4']}}
         ], 'Deltas got created successfully');
+
+        t.diag('Revert operation');
+
+        vdom =
+        {id: 'neo-calendar-week', cn: [
+            {id: 'neo-column-1', cn: [
+                {id: 'neo-event-1', cn: [
+                    {id: 'neo-event-1__time',  html: '08:00'},
+                    {id: 'neo-event-1__title', html: 'Event 1'}
+                ]},
+                {id: 'neo-event-2', cn: [
+                    {id: 'neo-event-2__time',  html: '10:00'},
+                    {id: 'neo-event-2__title', html: 'Event 2'}
+                ]}
+            ]},
+            {id: 'neo-column-2', cn: [
+                {id: 'neo-event-3', cn: [
+                    {id: 'neo-event-3__time',  html: '08:00'},
+                    {id: 'neo-event-3__title', html: 'Event 3'}
+                ]},
+                {id: 'neo-event-4', cn: [
+                    {id: 'neo-event-4__time',  html: '10:00'},
+                    {id: 'neo-event-4__title', html: 'Event 4'}
+                ]}
+            ]}
+        ]};
+
+        output = VdomHelper.update({vdom, vnode}); deltas = output.deltas; vnode = output.vnode;
+
+        t.is(deltas.length, 8, 'Count deltas equals 8');
+
+        t.isDeeplyStrict(deltas, [
+            {action: 'moveNode', id: 'neo-event-3', index: 0, parentId: 'neo-column-2'},
+            {action: 'moveNode', id: 'neo-event-4', index: 1, parentId: 'neo-column-2'},
+            {action: 'moveNode', id: 'neo-event-1', index: 0, parentId: 'neo-column-1'},
+            {                    id: 'neo-event-2', cls: {remove: ['foo3']}},
+            {                    id: 'neo-event-2__time', innerHTML: '10:00'},
+            {                    id: 'neo-column-2', cls: {remove: ['foo4']}},
+            {                    id: 'neo-event-3',  cls: {remove: ['foo1']}},
+            {                    id: 'neo-event-4',  cls: {remove: ['foo2']}}
+        ], 'Deltas got created successfully');
     });
 
     t.it('Add, Move & Edit multiple Events', t => {
