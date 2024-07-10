ab6075d9d411103c80c30885724df4a7aa5a709b
tobiu <tobiasuhlig78@gmail.com>
2024-07-10 10:58:05 +0200

    #5550 Testcase 3 - Wrapping nodes multiple times

--- test/siesta/tests/vdom/Advanced.mjs
+++ test/siesta/tests/vdom/Advanced.mjs
@@ -234,4 +234,64 @@ StartTest(t => {
             {action: 'removeNode', id: 'neo-event-7'}
         ], 'Deltas got created successfully');
     });
+
+    t.it('Wrapping nodes multiple times', t => {
+        vdom =
+        {id: 'neo-container-1', cn: [
+            {id: 'neo-component-1'},
+            {id: 'neo-component-2'},
+            {id: 'neo-component-3'},
+            {id: 'neo-component-4'},
+            {id: 'neo-component-5'},
+            {id: 'neo-component-6'}
+        ]};
+
+        vnode = VdomHelper.create(vdom);
+
+
+        vdom =
+        {id: 'neo-container-1', cn: [
+            {id: 'neo-wrapper-1', cn: [ // single wrapper
+                {id: 'neo-component-1', cls: ['foo1']}
+            ]},
+            {id: 'neo-wrapper-2', cn: [ // double wrapper
+                {id: 'neo-wrapper-3', cn: [
+                    {id: 'neo-component-2', cls: ['foo2']}
+                ]}
+            ]},
+            {id: 'neo-wrapper-4', cn: [ // wrapping multiple nodes
+                {id: 'neo-component-3', cls: ['foo3']},
+                {id: 'neo-component-4', cls: ['foo4']}
+            ]},
+            {id: 'neo-wrapper-5', cn: [ // nested wrapping
+                {id: 'neo-component-5', cls: ['foo5']},
+                {id: 'neo-wrapper-6', cn: [
+                    {id: 'neo-component-6', cls: ['foo6']}
+                ]}
+            ]}
+        ]};
+
+        output = VdomHelper.update({vdom, vnode}); deltas = output.deltas; vnode = output.vnode;
+
+        t.is(deltas.length, 16, 'Count deltas equals 16');
+
+        t.isDeeplyStrict(deltas, [
+            {action: 'insertNode', id: 'neo-wrapper-1',   index: 0, parentId: 'neo-container-1', outerHTML: '<div id="neo-wrapper-1"></div>'},
+            {action: 'moveNode',   id: 'neo-component-1', index: 0, parentId: 'neo-wrapper-1'},
+            {                      id: 'neo-component-1', cls: {add: ['foo1']}},
+            {action: 'insertNode', id: 'neo-wrapper-2',   index: 1, parentId: 'neo-container-1', outerHTML: '<div id="neo-wrapper-2"><div id="neo-wrapper-3"></div></div>'},
+            {action: 'moveNode',   id: 'neo-component-2', index: 0, parentId: 'neo-wrapper-3'},
+            {                      id: 'neo-component-2', cls: {add: ['foo2']}},
+            {action: 'insertNode', id: 'neo-wrapper-4',   index: 2, parentId: 'neo-container-1', outerHTML: '<div id="neo-wrapper-4"></div>'},
+            {action: 'moveNode',   id: 'neo-component-3', index: 0, parentId: 'neo-wrapper-4'},
+            {                      id: 'neo-component-3', cls: {add: ['foo3']}},
+            {action: 'moveNode',   id: 'neo-component-4', index: 1, parentId: 'neo-wrapper-4'},
+            {                      id: 'neo-component-4', cls: {add: ['foo4']}},
+            {action: 'insertNode', id: 'neo-wrapper-5',   index: 3, parentId: 'neo-container-1', outerHTML: '<div id="neo-wrapper-5"><div id="neo-wrapper-6"></div></div>'},
+            {action: 'moveNode',   id: 'neo-component-5', index: 0, parentId: 'neo-wrapper-5'},
+            {                      id: 'neo-component-5', cls: {add: ['foo5']}},
+            {action: 'moveNode',   id: 'neo-component-6', index: 0, parentId: 'neo-wrapper-6'},
+            {                      id: 'neo-component-6', cls: {add: ['foo6']}}
+        ], 'Deltas got created successfully');
+    });
 });
