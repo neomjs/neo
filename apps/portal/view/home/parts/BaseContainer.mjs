6b104ff25dbb558777cc10788b17bb8c3d122654
tobiu <tobiasuhlig78@gmail.com>
2024-07-18 16:47:37 +0200

    reverted the expander portal app change for now => put the code into a feature branch

--- apps/portal/view/home/parts/BaseContainer.mjs
+++ apps/portal/view/home/parts/BaseContainer.mjs
@@ -17,14 +17,20 @@ class BaseContainer extends Container {
          * @member {String[]} cls=['portal-home-content-view','neo-container']
          */
         baseCls: ['portal-home-content-view', 'neo-container'],
+        /**
+         * @member {Object} responsiveConfig
+         */
+        responsiveConfig: {
+            medium: {maxWidth: 840},
+            large : {minWidth: 841}
+        }
     }
 
     /**
      * Hook which will trigger whenever the container fully gets scrolled into the visible area.
      * Override it as needed.
      */
-    activate() {
-    }
+    activate() {}
 }
 
 Neo.setupClass(BaseContainer);
--- apps/portal/view/home/parts/References.mjs
+++ apps/portal/view/home/parts/References.mjs
@@ -38,7 +38,6 @@ class References extends BaseContainer {
             vdom: {tag: 'h2'}
         }, {
             module: Carousel,
-            style : {marginTop: '3em'},
             // will automatically change to the next extry every 5500 ms
             // if not set or 0, this will show arrows to navigate
             autoRun: 5500,
@@ -80,9 +79,6 @@ class References extends BaseContainer {
                 cls : 'neo-details',
                 html: `${data.publisher} - ${data.date}`
             }]
-        }, {
-            ntype: 'container',
-            flex : '1 1 100%'
         }]
     }
 }
