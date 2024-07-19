839734275cba8abb6eb03068f7ed0fc6bdd821fb
tobiu <tobiasuhlig78@gmail.com>
2024-07-19 14:01:49 +0200

    #5571 Portal.view.home.FooterContainer: Social Media Section

--- apps/portal/view/home/FooterContainer.mjs
+++ apps/portal/view/home/FooterContainer.mjs
@@ -1,10 +1,12 @@
-import Base from '../../../../src/container/Base.mjs';
+import Button    from '../../../../src/button/Base.mjs';
+import Component from '../../../../src/component/Base.mjs';
+import Container from '../../../../src/container/Base.mjs';
 
 /**
  * @class Portal.view.home.FooterContainer
  * @extends Neo.container.Base
  */
-class FooterContainer extends Base {
+class FooterContainer extends Container {
     static config = {
         /**
          * @member {String} className='Portal.view.home.FooterContainer'
@@ -16,11 +18,49 @@ class FooterContainer extends Base {
          */
         cls: ['portal-home-footer-container'],
         /**
+         * @member {Object} itemDefaults
+         */
+        itemDefaults: {
+            module: Container,
+            cls   : ['portal-home-footer-section'],
+            layout: {ntype: 'vbox', align: 'start'},
+
+            itemDefaults: {
+                module: Button,
+                ui    : 'ghost'
+            }
+        },
+        /**
          * @member {Object[]} items
          */
         items: [{
-            html: 'Footer'
+            html: 'Section 1'
+        }, {
+            html: 'Section 2'
+        }, {
+            items: [{
+                module: Component,
+                cls   : ['neo-h2'],
+                html  : 'Social Media',
+                tag   : 'h2'
+            }, {
+                iconCls: 'fa-brands fa-linkedin',
+                text   : 'LinkedIn',
+                url    : 'https://www.linkedin.com/company/neo-mjs/'
+            }, {
+                iconCls: 'fa-brands fa-facebook',
+                text   : 'Facebook',
+                url    : 'https://www.facebook.com/neo.mjs'
+            }, {
+                iconCls: 'fa-brands fa-x-twitter',
+                text   : 'X',
+                url    : 'https://x.com/neomjs1'
+            }]
         }],
+        /**
+         * @member {Object} layout={ntype:'hbox',align:'stretch'}
+         */
+        layout: {ntype: 'hbox', align: 'stretch'},
         /**
          * @member {String} tag='footer'
          */
