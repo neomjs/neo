794666505c943bfb032ab8155872b99e184231c9
tobiu <tobiasuhlig78@gmail.com>
2024-07-20 18:00:31 +0200

    calendar.view.MainContainer: enforcing a min-width for the SideBar

--- src/calendar/view/MainContainer.mjs
+++ src/calendar/view/MainContainer.mjs
@@ -468,11 +468,12 @@ class MainContainer extends Container {
             flex  : 1,
             layout: {ntype: 'hbox', align: 'stretch'},
             items : [{
-                module: Container,
-                cls   : ['neo-calendar-sidebar', 'neo-container'],
-                layout: {ntype: 'vbox', align: 'stretch'},
-                width : me.sideBarWidth,
-                items : [me.dateSelector, me.calendarsContainer]
+                module  : Container,
+                cls     : ['neo-calendar-sidebar', 'neo-container'],
+                layout  : {ntype: 'vbox', align: 'stretch'},
+                minWidth: me.sideBarWidth,
+                width   : me.sideBarWidth,
+                items   : [me.dateSelector, me.calendarsContainer]
             }, {
                 module   : Container,
                 flex     : 1,
