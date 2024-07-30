777ba14fbb0506f73dcf5669c65db6d62dc4cfad
tobiu <tobiasuhlig78@gmail.com>
2024-07-30 13:05:58 +0200

    v6.23.0

--- examples/component/video/MainContainer.mjs
+++ examples/component/video/MainContainer.mjs
@@ -1,16 +1,15 @@
-import Viewport  from '../../../src/container/Viewport.mjs';
-import Video     from '../../../src/component/Video.mjs';
-import Panel     from '../../../src/container/Panel.mjs';
 import MainContainerController from "./MainContainerController.mjs";
+import Panel                   from '../../../src/container/Panel.mjs';
+import Video                   from '../../../src/component/Video.mjs';
+import Viewport                from '../../../src/container/Viewport.mjs';
 
 /**
- * @class Neo.examples.component.timer.MainContainer
+ * @class Neo.examples.component.video.MainContainer
  * @extends Neo.examples.Viewport
  */
 class MainContainer extends Viewport {
     static config = {
-        className: 'Neo.examples.component.timer.MainContainer',
-
+        className : 'Neo.examples.component.timer.MainContainer',
         controller: MainContainerController,
 
         items : [{
@@ -35,7 +34,7 @@ class MainContainer extends Viewport {
                 module   : Video,
                 url      : 'https://video-ssl.itunes.apple.com/itunes-assets/Video125/v4/a0/57/54/a0575426-dd8e-2d25-bdf3-139702870b50/mzvf_786190431362224858.640x464.h264lc.U.p.m4v',
                 flag     : 'video-component',
-                minHeight: 400,
+                minHeight: 400
             }]
         }]
     }
--- src/DefaultConfig.mjs
+++ src/DefaultConfig.mjs
@@ -260,12 +260,12 @@ const DefaultConfig = {
     useVdomWorker: true,
     /**
      * buildScripts/injectPackageVersion.mjs will update this value
-     * @default '6.22.1'
+     * @default '6.23.0'
      * @memberOf! module:Neo
      * @name config.version
      * @type String
      */
-    version: '6.22.1'
+    version: '6.23.0'
 };
 
 Object.assign(DefaultConfig, {
