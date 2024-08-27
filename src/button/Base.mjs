fbc03d43895ea898840e4ca6009f1d3ccdd542d7
tobiu <tobiasuhlig78@gmail.com>
2024-08-27 21:38:28 +0200

    button.Base: switch to an A tag for buttons with a route #5838

--- src/button/Base.mjs
+++ src/button/Base.mjs
@@ -92,7 +92,9 @@ class Base extends Component {
          */
         pressed_: false,
         /**
-         * Change the browser hash value on click
+         * Change the browser hash value on click.
+         * Use route for internal navigation and url for external links. Do not use both on the same instance.
+         * Transforms the button tag into an a tag [optional]
          * @member {String|null} route_=null
          */
         route_: null,
@@ -298,6 +300,16 @@ class Base extends Component {
     }
 
     /**
+     * Triggered after the route config got changed
+     * @param {String|null} value
+     * @param {String|null} oldValue
+     * @protected
+     */
+    afterSetRoute(value, oldValue) {
+        this.updateTag()
+    }
+
+    /**
      * Triggered after the theme config got changed
      * @param {String|null} value
      * @param {String|null} oldValue
@@ -338,22 +350,12 @@ class Base extends Component {
 
     /**
      * Triggered after the url config got changed
-     * @param {String} value
-     * @param {String} oldValue
+     * @param {String|null} value
+     * @param {String|null} oldValue
      * @protected
      */
     afterSetUrl(value, oldValue) {
-        let vdomRoot = this.getVdomRoot();
-
-        if (value) {
-            vdomRoot.href = value;
-            vdomRoot.tag  = 'a'
-        } else {
-            delete vdomRoot.href;
-            vdomRoot.tag = 'button'
-        }
-
-        this.update()
+        this.updateTag()
     }
 
     /**
@@ -566,6 +568,26 @@ class Base extends Component {
         if (!hidden) {
             await this.timeout(50)
         }
+    }
+
+    /**
+     *
+     */
+    updateTag() {
+        let me           = this,
+            {route, url} = me,
+            link         = route || url,
+            vdomRoot     = me.getVdomRoot();
+
+        if (link) {
+            vdomRoot.href = link;
+            vdomRoot.tag  = 'a'
+        } else {
+            delete vdomRoot.href;
+            vdomRoot.tag = 'button'
+        }
+
+        me.update()
     }
 }
 
