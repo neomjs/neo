6810743bd8e166a4dd6eedca944770901aa098bd
tobiu <tobiasuhlig78@gmail.com>
2024-10-12 18:31:29 +0200

    v7.15.0

--- apps/portal/view/home/FooterContainer.mjs
+++ apps/portal/view/home/FooterContainer.mjs
@@ -111,7 +111,7 @@ class FooterContainer extends Container {
             }, {
                 module: Component,
                 cls   : ['neo-version'],
-                html  : 'v7.14.0'
+                html  : 'v7.15.0'
             }]
         }],
         /**
