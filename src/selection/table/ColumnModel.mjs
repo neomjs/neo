59f6d3653e9880d54599f93c7337d572419e28a8
tobiu <tobiasuhlig78@gmail.com>
2024-11-07 13:17:51 +0100

    selection.table.*: set the updateDepth to 2 for selection based updates #6077

--- src/selection/table/ColumnModel.mjs
+++ src/selection/table/ColumnModel.mjs
@@ -34,6 +34,35 @@ class ColumnModel extends Model {
     }
 
     /**
+     * @param {Object} item
+     * @param {Boolean} [silent] true to prevent a vdom update
+     * @param {Object[]|String[]} itemCollection=this.items
+     * @param {String} [selectedCls]
+     */
+    deselect(item, silent, itemCollection=this.items, selectedCls) {
+        let {view} = this;
+
+        if (!silent) {
+            view.updateDepth = 2
+        }
+
+        super.deselect(item, silent, itemCollection, selectedCls)
+    }
+
+    /**
+     * @param {Boolean} [silent] true to prevent a vdom update
+     */
+    deselectAll(silent) {
+        let {view} = this;
+
+        if (!silent) {
+            view.updateDepth = 2
+        }
+
+        super.deselectAll(silent)
+    }
+
+    /**
      * @param args
      */
     destroy(...args) {
@@ -149,6 +178,18 @@ class ColumnModel extends Model {
         )
     }
 
+    /**
+     * @param {Object} args
+     */
+    select(...args) {
+        let {view} = this;
+
+        if (!view.silentSelect) {
+            view.updateDepth = 2
+        }
+
+        super.select(...args)
+    }
 
     /**
      *
