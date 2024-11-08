9e135349bbcd72e6643f13877bced1c1c428bf07
tobiu <tobiasuhlig78@gmail.com>
2024-11-08 14:13:48 +0100

    v8.0.0-alpha.1

--- apps/portal/view/home/FooterContainer.mjs
+++ apps/portal/view/home/FooterContainer.mjs
@@ -111,7 +111,7 @@ class FooterContainer extends Container {
             }, {
                 module: Component,
                 cls   : ['neo-version'],
-                html  : 'v7.16.0'
+                html  : 'v8.0.0-alpha.1'
             }]
         }],
         /**
