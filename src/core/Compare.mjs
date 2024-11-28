f4a475071c3ba891ca2c75936bb579971b5be10f
tobiu <tobiasuhlig78@gmail.com>
2024-11-28 08:52:39 +0100

    core.Compare: isEqual() => use a map and move the testing logic into separate methods #6130

--- src/core/Compare.mjs
+++ src/core/Compare.mjs
@@ -14,6 +14,141 @@ class Compare extends Base {
     }
 
     /**
+     * Storing the comparison method names by data type
+     * @member {Object} map
+     */
+    static map = {
+        Array      : 'compareArrays',
+        Date       : 'compareDates',
+        Function   : 'compareFunctions',
+        Map        : 'compareMaps',
+        NeoInstance: 'compareNeoInstances',
+        Object     : 'compareObjects',
+        RegExp     : 'compareRegExps',
+        Set        : 'compareSets'
+    }
+
+    /**
+     * @param {Array} item1
+     * @param {Array} item2
+     * @returns {Boolean}
+     */
+    static compareArrays(item1, item2) {
+        if (item1.length !== item2.length) {
+            return false
+        }
+
+        for (const [i, v] of item1.entries()) {
+            if (!Compare.isEqual(v, item2[i])) {
+                return false
+            }
+        }
+
+        return true
+    }
+
+    /**
+     * @param {Date} item1
+     * @param {Date} item2
+     * @returns {Boolean}
+     */
+    static compareDates(item1, item2) {
+        return item1.valueOf() === item2.valueOf()
+    }
+
+    /**
+     * @param {Function} item1
+     * @param {Function} item2
+     * @returns {Boolean}
+     */
+    static compareFunctions(item1, item2) {
+        if (item1.name !== item2.name) {
+            return false
+        }
+
+        return item1.toString() === item2.toString()
+    }
+
+    /**
+     * @param {Map} item1
+     * @param {Map} item2
+     * @returns {Boolean}
+     */
+    static compareMaps(item1, item2) {
+        if (item1.size !== item2.size) {
+            return false
+        }
+
+        let val2;
+
+        for (const [key, val] of item1) {
+            val2 = item2.get(key);
+
+            if (val2 !== val || val2 === undefined && !item2.has(key)) {
+                return false
+            }
+        }
+
+        return true
+    }
+
+    /**
+     * @param {Neo.core.Base} item1
+     * @param {Neo.core.Base} item2
+     * @returns {Boolean}
+     */
+    static compareNeoInstances(item1, item2) {
+        return item1.id === item2.id
+    }
+
+    /**
+     * @param {Object} item1
+     * @param {Object} item2
+     * @returns {Boolean}
+     */
+    static compareObjects(item1, item2) {
+        if (Object.keys(item1).length !== Object.keys(item2).length) {
+            return false
+        }
+
+        for (let key in item1) {
+            if (!Compare.isEqual(item1[key], item2[key])) {
+                return false
+            }
+        }
+
+        return true
+    }
+
+    /**
+     * @param {RegExp} item1
+     * @param {RegExp} item2
+     * @returns {Boolean}
+     */
+    static compareRegExps(item1, item2) {
+        return item1.toString() === item2.toString()
+    }
+
+    /**
+     * @param {Set} item1
+     * @param {Set} item2
+     * @returns {Boolean}
+     */
+    static compareSets(item1, item2) {
+        if (item1.size !== item2.size) {
+            return false
+        }
+
+        for (let key of item1) {
+            if (!item2.has(key)) {
+                return false
+            }
+        }
+
+        return true
+    }
+
+    /**
      * @param {*} item1
      * @param {*} item2
      * @returns {Boolean}
@@ -24,108 +159,18 @@ class Compare extends Base {
         }
 
         let type1 = Neo.typeOf(item1),
-            type2 = Neo.typeOf(item2),
-            key;
+            type2 = Neo.typeOf(item2);
 
         if (type1 !== type2) {
             return false
         }
 
-        switch (type1) {
-            case 'Array': {
-                if (item1.length !== item2.length) {
-                    return false
-                }
-
-                for (const [i, v] of item1.entries()) {
-                    if (!Compare.isEqual(v, item2[i])) {
-                        return false
-                    }
-                }
-
-                break
-            }
-
-            case 'Date': {
-                return item1.valueOf() === item2.valueOf()
-            }
-
-            case 'Function': {
-                if (item1.name !== item2.name) {
-                    return false
-                }
-
-                return item1.toString() === item2.toString()
-            }
-
-            case 'Map': {
-                if (item1.size !== item2.size) {
-                    return false
-                }
-
-                let val2;
-
-                for (const [key, val] of item1) {
-                    val2 = item2.get(key);
-
-                    if (val2 !== val || val2 === undefined && !item2.has(key)) {
-                        return false
-                    }
-                }
-
-                break
-            }
-
-            case 'NeoInstance': {
-                if (item1.id !== item2.id) {
-                    return false
-                }
-
-                break
-            }
-
-            case 'Object': {
-                if (Object.keys(item1).length !== Object.keys(item2).length) {
-                    return false
-                }
-
-                for (key in item1) {
-                    if (!Compare.isEqual(item1[key], item2[key])) {
-                        return false
-                    }
-                }
-
-                break
-            }
-
-            case 'RegExp': {
-                if (item1.toString() !== item2.toString()) {
-                    return false
-                }
-
-                break
-            }
-
-            case 'Set': {
-                if (item1.size !== item2.size) {
-                    return false
-                }
-
-                for (key of item1) {
-                    if (!item2.has(key)) {
-                        return false
-                    }
-                }
-
-                break
-            }
-
-            default: {
-                return item1 === item2
-            }
+        if (Compare.map[type1]) {
+            return Compare[Compare.map[type1]](item1, item2)
         }
 
-        return true
+        // all other types
+        return item1 === item2
     }
 }
 
