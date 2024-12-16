7329c1d6568ca30f78f00ade6c4ba4f91d75dce6
tobiu <tobiasuhlig78@gmail.com>
2024-12-16 15:41:00 +0100

    #6139 examples.table.cellEditing.MainContainer: simplified the example app

--- examples/table/cellEditing/MainContainer.mjs
+++ examples/table/cellEditing/MainContainer.mjs
@@ -1,15 +1,12 @@
-import Button                from '../../../src/button/Base.mjs';
 import CellColumnModel       from '../../../src/selection/table/CellColumnModel.mjs';
 import CellColumnRowModel    from '../../../src/selection/table/CellColumnRowModel.mjs';
 import CellModel             from '../../../src/selection/table/CellModel.mjs';
 import CellRowModel          from '../../../src/selection/table/CellRowModel.mjs';
 import Checkbox              from '../../../src/form/field/CheckBox.mjs';
 import ConfigurationViewport from '../../ConfigurationViewport.mjs';
-import ColumnModel           from '../../../src/selection/table/ColumnModel.mjs';
 import MainStore             from './MainStore.mjs';
 import NumberField           from '../../../src/form/field/Number.mjs';
 import Radio                 from '../../../src/form/field/Radio.mjs';
-import RowModel              from '../../../src/selection/table/RowModel.mjs';
 import TableContainer        from '../../../src/table/Container.mjs';
 
 /**
@@ -52,16 +49,6 @@ class MainContainer extends ConfigurationViewport {
             listeners     : {change: me.onRadioChange.bind(me, 'selectionModel', CellModel)},
             style         : {marginTop: '10px'},
             valueLabelText: 'Cell'
-        }, {
-            ...selectionModelRadioDefaults,
-            checked       : me.exampleComponent.selectionModel.ntype === 'selection-table-columnmodel',
-            listeners     : {change: me.onRadioChange.bind(me, 'selectionModel', ColumnModel)},
-            valueLabelText: 'Column'
-        }, {
-            ...selectionModelRadioDefaults,
-            checked       : me.exampleComponent.selectionModel.ntype === 'selection-table-rowmodel',
-            listeners     : {change: me.onRadioChange.bind(me, 'selectionModel', RowModel)},
-            valueLabelText: 'Row'
         }, {
             ...selectionModelRadioDefaults,
             checked       : me.exampleComponent.selectionModel.ntype === 'selection-table-cellcolumnmodel',
@@ -120,65 +107,9 @@ class MainContainer extends ConfigurationViewport {
                 {dataField: 'firstname', text: 'Firstname'},
                 {dataField: 'lastname',  text: 'Lastname'},
                 {dataField: 'githubId',  text: 'Github Id'},
-                {dataField: 'country',   text: 'Country'},
-                {
-                    dataField: 'edit',
-                    text     : 'Edit Action',
-                    renderer({column, index}) {
-                        const
-                            me       = this,
-                            widgetId = `${column.id}-widget-${index}`,
-                            button   = (column.widgetMap || (column.widgetMap = {}))[widgetId] || (column.widgetMap[widgetId] = Neo.create({
-                                module  : Button,
-                                appName : me.appName,
-                                handler : 'up.editButtonHandler',
-                                parentId: 'myTableStoreContainer',
-                                text    : 'Edit'
-                            }));
-
-                        me.view.updateDepth = -1;
-
-                        return button.createVdomReference()
-                    }
-                }, {
-                    dataField: 'menu',
-                    text     : 'Menu',
-                    renderer({column, record, index}) {
-                        const
-                            me       = this,
-                            widgetId = `${column.id}-widget-${index}`,
-                            button   = (column.widgetMap || (column.widgetMap = {}))[widgetId] || (column.widgetMap[widgetId] = Neo.create('Neo.button.Base', {
-                                ntype   : 'button',
-                                appName : me.appName,
-                                text    : '\u22ee',
-                                windowId: me.windowId,
-                                menu    : {
-                                    items : [{
-                                        text : 'Menu option 1'
-                                    }, {
-                                        text : 'Menu option 2'
-                                    }, {
-                                        text : 'Menu option 3'
-                                    }, {
-                                        text : 'Menu option 4'
-                                    }]
-                                }
-                            }));
-
-                        me.view.updateDepth = -1;
-
-                        return button.createVdomReference()
-                    }
-                }
+                {dataField: 'country',   text: 'Country'}
             ]
         })
-    }
-
-    /**
-     * @param {Object} data
-     */
-    editButtonHandler(data) {
-        console.log(data)
     }
 }
 
