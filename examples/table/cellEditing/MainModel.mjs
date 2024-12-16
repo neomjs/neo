4ba116bb3d92e9b6312dfec1d2da00e662a1281d
tobiu <tobiasuhlig78@gmail.com>
2024-12-16 15:38:11 +0100

    Create a PoC for a table cell editing feature #6139 => example shell

---
+++ examples/table/cellEditing/MainModel.mjs
@@ -0,0 +1,30 @@
+import Model from '../../../src/data/Model.mjs';
+
+/**
+ * @class Neo.examples.table.cellEditing.MainModel
+ * @extends Neo.data.Model
+ */
+class MainModel extends Model {
+    static config = {
+        className: 'Neo.examples.table.cellEditing.MainModel',
+
+        fields: [{
+            name: 'colspan',
+            type: 'Object'
+        }, {
+            name: 'country',
+            type: 'String'
+        }, {
+            name: 'firstname',
+            type: 'String'
+        }, {
+            name: 'githubId',
+            type: 'String'
+        }, {
+            name: 'lastname',
+            type: 'String'
+        }]
+    }
+}
+
+export default Neo.setupClass(MainModel);
---
+++ examples/table/cellEditing/MainStore.mjs
@@ -0,0 +1,48 @@
+import Model from './MainModel.mjs';
+import Store from '../../../src/data/Store.mjs';
+
+/**
+ * @class Neo.examples.table.cellEditing.MainStore
+ * @extends Neo.data.Store
+ */
+class MainStore extends Store {
+    static config = {
+        className  : 'Neo.examples.table.cellEditing.MainStore',
+        keyProperty: 'githubId',
+        model      : Model,
+
+        data: [{
+            country  : 'Germany',
+            firstname: 'Tobias',
+            githubId : 'tobiu',
+            lastname : 'Uhlig'
+        }, {
+            country  : 'USA',
+            firstname: 'Rich',
+            githubId : 'rwaters',
+            lastname : 'Waters'
+        }, {
+            country  : 'Germany',
+            firstname: 'Nils',
+            githubId : 'mrsunshine',
+            lastname : 'Dehl'
+        }, {
+            country  : 'USA',
+            firstname: 'Gerard',
+            githubId : 'camtnbikerrwc',
+            lastname : 'Horan'
+        }, {
+            country  : 'Slovakia',
+            firstname: 'Jozef',
+            githubId : 'jsakalos',
+            lastname : 'Sakalos'
+        }, {
+            country  : 'Germany',
+            firstname: 'Bastian',
+            githubId : 'bhaustein',
+            lastname : 'Haustein'
+        }]
+    }
+}
+
+export default Neo.setupClass(MainStore);
