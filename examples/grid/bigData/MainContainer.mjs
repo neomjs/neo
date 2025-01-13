d92e4daf2bfea54f8f4785ee5b79f9f37f2df8a3
tobiu <tobiasuhlig78@gmail.com>
2025-01-13 19:14:08 +0100

    #6212 itemDefaults

--- examples/grid/bigData/MainContainer.mjs
+++ examples/grid/bigData/MainContainer.mjs
@@ -21,41 +21,37 @@ class MainContainer extends Viewport {
             module: Toolbar,
             flex  : 'none',
             style : {marginBottom: '1em'},
-            items : [{
+
+            itemDefaults: {
                 module      : ComboBox,
                 clearable   : false,
                 displayField: 'id',
-                editable    : false,
-                labelText   : 'Amount Rows',
-                labelWidth  : 110,
-                listeners   : {change: 'up.onAmountRowsChange'},
-                store       : ['1000', '5000', '10000', '20000'],
-                value       : '10000',
-                width       : 200
+                editable    : false
+            },
+
+            items: [{
+                labelText : 'Amount Rows',
+                labelWidth: 110,
+                listeners : {change: 'up.onAmountRowsChange'},
+                store     : ['1000', '5000', '10000', '20000'],
+                value     : '10000',
+                width     : 200
             }, {
-                module      : ComboBox,
-                clearable   : false,
-                displayField: 'id',
-                editable    : false,
-                labelText   : 'Amount Columns',
-                labelWidth  : 135,
-                listeners   : {change: 'up.onAmountColumnsChange'},
-                store       : ['25', '50', '75', '100'],
-                style       : {marginLeft: '2em'},
-                value       : '50',
-                width       : 200
+                labelText : 'Amount Columns',
+                labelWidth: 135,
+                listeners : {change: 'up.onAmountColumnsChange'},
+                store     : ['25', '50', '75', '100'],
+                style     : {marginLeft: '2em'},
+                value     : '50',
+                width     : 200
             }, {
-                module      : ComboBox,
-                clearable   : false,
-                displayField: 'id',
-                editable    : false,
-                labelText   : 'Buffer Rows',
-                labelWidth  : 100,
-                listeners   : {change: 'up.onAmountBufferRowsChange'},
-                store       : ['0', '3', '5', '10', '25', '50'],
-                style       : {marginLeft: '2em'},
-                value       : '5',
-                width       : 160
+                labelText : 'Buffer Rows',
+                labelWidth: 100,
+                listeners : {change: 'up.onAmountBufferRowsChange'},
+                store     : ['0', '3', '5', '10', '25', '50'],
+                style     : {marginLeft: '2em'},
+                value     : '5',
+                width     : 160
             }]
         }, {
             module: GridContainer
