873ada0135002249f1f7a6a476fa7ac801b0bd3a
tobiu <tobiasuhlig78@gmail.com>
2025-01-14 14:40:10 +0100

    v8.4.0

--- src/DefaultConfig.mjs
+++ src/DefaultConfig.mjs
@@ -262,12 +262,12 @@ const DefaultConfig = {
     useVdomWorker: true,
     /**
      * buildScripts/injectPackageVersion.mjs will update this value
-     * @default '8.3.0'
+     * @default '8.4.0'
      * @memberOf! module:Neo
      * @name config.version
      * @type String
      */
-    version: '8.3.0'
+    version: '8.4.0'
 };
 
 Object.assign(DefaultConfig, {
