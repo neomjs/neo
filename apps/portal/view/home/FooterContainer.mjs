fed3f6128469b99151d0a7a6617e8f90a20fbc3e
tobiu <tobiasuhlig78@gmail.com>
2025-01-21 19:32:00 +0100

    v8.6.2

--- apps/portal/view/home/FooterContainer.mjs
+++ apps/portal/view/home/FooterContainer.mjs
@@ -107,7 +107,7 @@ class FooterContainer extends Container {
             }, {
                 module: Component,
                 cls   : ['neo-version'],
-                html  : 'v8.6.1'
+                html  : 'v8.6.2'
             }]
         }],
         /**
