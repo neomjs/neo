8495cccf802e3e384ceb45efb17e5faebcfc2f28
tobiu <tobiasuhlig78@gmail.com>
2025-02-09 21:40:45 +0100

    examples.grid.bigData.ControlsContainer: switch to a slide & fade show / hide OP #6425

--- examples/grid/bigData/ControlsContainer.mjs
+++ examples/grid/bigData/ControlsContainer.mjs
@@ -27,6 +27,8 @@ class ControlsContainer extends Container {
             iconCls: 'fas fa-bars'
         }, {
             module: Container,
+            cls   : ['neo-examples-bigdata-controls-container-content'],
+            layout: 'vbox',
 
             itemDefaults: {
                 module      : ComboBox,
@@ -87,7 +89,7 @@ class ControlsContainer extends Container {
         /**
          * @member {Object} layout={ntype:'vbox'}
          */
-        layout: {ntype: 'vbox'},
+        layout: {ntype: 'fit'},
         /**
          * @member {String} tag='aside'
          */
