0f1acf723c4f378741b17ef3372a6ceb12bf18b5
tobiu <tobiasuhlig78@gmail.com>
2025-03-24 18:03:12 +0100

    #6582 Finance.view.GridContainer

---
+++ apps/finance/view/GridContainer.mjs
@@ -0,0 +1,38 @@
+import BaseGridContainer from '../../../src/grid/Container.mjs';
+
+/**
+ * @class Finance.view.GridContainer
+ * @extends Neo.table.Container
+ */
+class GridContainer extends BaseGridContainer {
+    static config = {
+        /**
+         * @member {String} className='Finance.view.GridContainer'
+         * @protected
+         */
+        className: 'Finance.view.GridContainer',
+        /**
+         * @member {Object} bind
+         */
+        bind: {
+            store: 'stores.companies'
+        },
+        /**
+         * @member {Object[]} columns
+         */
+        columns: [{
+            dataField: 'symbol',
+            text     : 'Symbol'
+        }, {
+            dataField: 'name',
+            text     : 'Name',
+            width    : 200
+        }, {
+            dataField: 'sector',
+            text     : 'Sector',
+            width    : 200
+        }]
+    }
+}
+
+export default Neo.setupClass(GridContainer);
--- apps/finance/view/Viewport.mjs
+++ apps/finance/view/Viewport.mjs
@@ -1,6 +1,5 @@
 import BaseViewport          from '../../../src/container/Viewport.mjs';
-import Component             from '../../../src/component/Base.mjs';
-import TabContainer          from '../../../src/tab/Container.mjs';
+import GridContainer         from './GridContainer.mjs';
 import ViewportStateProvider from './ViewportStateProvider.mjs';
 
 /**
@@ -18,30 +17,7 @@ class Viewport extends BaseViewport {
          * @member {Object[]} items
          */
         items: [{
-            module: TabContainer,
-            height: 300,
-            width : 500,
-            style : {flex: 'none', margin: '20px'},
-
-            itemDefaults: {
-                module: Component,
-                cls   : ['neo-examples-tab-component'],
-                style : {padding: '20px'},
-            },
-
-            items: [{
-                header: {
-                    iconCls: 'fa fa-home',
-                    text   : 'Tab 1'
-                },
-                vdom: {innerHTML: 'Welcome to your new Neo App.'}
-            }, {
-                header: {
-                    iconCls: 'fa fa-play-circle',
-                    text   : 'Tab 2'
-                },
-                vdom: {innerHTML: 'Have fun creating something awesome!'}
-            }]
+            module: GridContainer
         }],
         /*
          * @member {Object} layout={ntype:'fit'}
@@ -50,7 +26,8 @@ class Viewport extends BaseViewport {
         /**
          * @member {Neo.state.Provider} stateProvider=ViewportStateProvider
          */
-        stateProvider: ViewportStateProvider
+        stateProvider: ViewportStateProvider,
+        style: {padding: '2em'}
     }
 }
 
