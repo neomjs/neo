01b7f68828f84238e8419f9d982e18261f4b468d
tobiu <tobiasuhlig78@gmail.com>
2025-04-15 10:52:13 +0200

    #6650 removed testing log

--- src/table/Container.mjs
+++ src/table/Container.mjs
@@ -472,7 +472,7 @@ class Container extends BaseContainer {
      * @param {Object[]} data
      * @protected
      */
-    onStoreLoad(data) {console.log(data);
+    onStoreLoad(data) {
         let me = this;
 
         if (me.rendered) {
