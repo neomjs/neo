6c8ad45101f53188969fc81ef6407a608e240621
tobiu <tobiasuhlig78@gmail.com>
2025-05-23 20:18:50 +0200

    v9.3.0

--- examples/ServiceWorker.mjs
+++ examples/ServiceWorker.mjs
@@ -20,9 +20,9 @@ class ServiceWorker extends ServiceBase {
          */
         singleton: true,
         /**
-         * @member {String} version='9.2.0'
+         * @member {String} version='9.3.0'
          */
-        version: '9.2.0'
+        version: '9.3.0'
     }
 
     /**
--- src/DefaultConfig.mjs
+++ src/DefaultConfig.mjs
@@ -263,12 +263,12 @@ const DefaultConfig = {
     useVdomWorker: true,
     /**
      * buildScripts/injectPackageVersion.mjs will update this value
-     * @default '9.2.0'
+     * @default '9.3.0'
      * @memberOf! module:Neo
      * @name config.version
      * @type String
      */
-    version: '9.2.0'
+    version: '9.3.0'
 };
 
 Object.assign(DefaultConfig, {
