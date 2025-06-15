10e582cdd10344f45109369c405e6e9cca07a5e9
tobiu <tobiasuhlig78@gmail.com>
2025-06-15 12:57:08 +0200

    Neo.config.useStringBasedMounting #6795

--- src/DefaultConfig.mjs
+++ src/DefaultConfig.mjs
@@ -247,6 +247,18 @@ const DefaultConfig = {
      */
     useSharedWorkers: false,
     /**
+     * `true` will let the `vdom.Helper` create a String-based representation of the vnode tree.
+     * Main will then use e.g.`parentNode.insertAdjacentHTML('beforeend', delta.outerHTML);`
+     * This affects the initial painting of your apps, but also the creation of new component trees at run-time.
+     * `false` will skip the creation of the String, and instead use DOM APIs to generate a fragment inside Main,
+     * into which the vnode tree will get applied.
+     * @default false
+     * @memberOf! module:Neo
+     * @name config.useStringBasedMounting
+     * @type Boolean
+     */
+    useStringBasedMounting: false,
+    /**
      * True will generate a new task worker, which can get filled with own expensive remote methods
      * @default false
      * @memberOf! module:Neo
