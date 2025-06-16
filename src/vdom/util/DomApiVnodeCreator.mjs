5af1f7f56d65d9c8d78a52f5906576d53061d493
tobiu <tobiasuhlig78@gmail.com>
2025-06-16 19:22:55 +0200

    Neo.vdom.util.DomApiVnodeCreator: create() => opt out for componentId reference nodes #6821

--- src/vdom/util/DomApiVnodeCreator.mjs
+++ src/vdom/util/DomApiVnodeCreator.mjs
@@ -8,10 +8,14 @@ const DomApiVnodeCreator = {
      * @param {Map}            [movedNodes] A map of VNodes that are being moved.
      * @returns {Object|null} A new VNode tree (or subtree) with moved nodes pruned, or null if the root is a moved node.
      */
-    create(vnode, movedNodes) { // Renamed from _createDomApiReadyVnode to 'create'
-        // If this specific vnode is in the movedNodes map, it means its DOM element
-        // will be moved by a separate delta. So, we should not include it in this fragment.
-        if (vnode.id && movedNodes?.get(vnode.id)) {
+    create(vnode, movedNodes) {
+        /*
+         * If the node has a componentId, there is nothing to do (scoped vdom updates), opt out.
+         *
+         * If this specific vnode is in the movedNodes map, it means its DOM element
+         * will be moved by a separate delta. So, we should not include it in this fragment.
+         */
+        if (vnode.componentId || (vnode.id && movedNodes?.get(vnode.id))) {
             return null // Prune this branch
         }
 
