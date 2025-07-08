3bf40d40b79176d51e2a77168da05abe4840d3f2
tobiu <tobiasuhlig78@gmail.com>
2025-07-08 21:05:06 +0200

    #6905 Added test testing class into the current feature branch, to not lose it.

---
+++ test/siesta/tests/neo/MixinStaticConfig.mjs
@@ -0,0 +1,29 @@
+import Neo       from '../../../../src/Neo.mjs';
+import * as core from '../../../../src/core/_export.mjs';
+
+StartTest(t => {
+    t.it('should merge static configs from mixins', t => {
+        class TestMixin extends core.Base {
+            static config = {
+                className           : 'Neo.test.mixin.TestMixin',
+                mixinConfig         : 'mixinValue',
+                reactiveMixinConfig_: 'reactiveValue'
+            }
+        }
+
+        Neo.setupClass(TestMixin);
+
+        class TestClass extends core.Base {
+            static config = {
+                className: 'Neo.test.mixin.TestClass',
+                mixins   : [TestMixin]
+            }
+        }
+
+        Neo.setupClass(TestClass);
+
+        //  const instance = Neo.create(TestClass);
+        //  t.is(instance.mixinConfig, 'mixinValue', 'mixinConfig should be merged from the mixin');
+        //  t.is(instance.reactiveMixinConfig, 'reactiveValue', 'reactiveMixinConfig should be merged from the mixin and the underscore removed');
+    });
+});
