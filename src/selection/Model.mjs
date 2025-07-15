57c844a9b647378f4f07be6eb3807dd00a71ba61
tobiu <tobiasuhlig78@gmail.com>
2025-07-16 00:08:31 +0200

    selection.Model: items_ => use a config descriptor #7067

--- src/selection/Model.mjs
+++ src/selection/Model.mjs
@@ -1,10 +1,12 @@
-import Base       from '../core/Base.mjs';
-import NeoArray   from '../util/Array.mjs';
-import Observable from '../core/Observable.mjs';
+import Base           from '../core/Base.mjs';
+import NeoArray       from '../util/Array.mjs';
+import Observable     from '../core/Observable.mjs';
+import {isDescriptor} from '../core/ConfigSymbols.mjs';
 
 /**
  * @class Neo.selection.Model
  * @extends Neo.core.Base
+ * @mixes Neo.core.Observable
  */
 class Model extends Base {
     /**
@@ -32,11 +34,17 @@ class Model extends Base {
          */
         cls: null,
         /**
-         * @member {Array} items_=null
+         * @member {Array} items_
          * @protected
          * @reactive
          */
-        items_: null,
+        items_: {
+            [isDescriptor]: true,
+            clone         : 'shallow',
+            cloneOnGet    : 'none',
+            isEqual       : () => false,
+            value         : []
+        },
         /**
          * @member {String} selectedCls='selected'
          */
@@ -52,19 +60,6 @@ class Model extends Base {
          * @reactive
          */
         view_: null
-    }
-
-    /**
-     * Gets triggered before getting the value of the items config
-     * @param {Array|null} value
-     * @returns {Array}
-     */
-    beforeGetItems(value) {
-        if (!value) {
-            this._items = value = []
-        }
-
-        return value
     }
 
     /**
