d15386a0fb95982db6d95031a0e2f4667cad3484
tobiu <tobiasuhlig78@gmail.com>
2025-07-27 02:26:05 +0200

    Neo.selection.Model: select() => pass potential record items to the selectionChange event #7116

--- src/selection/Model.mjs
+++ src/selection/Model.mjs
@@ -234,8 +234,9 @@ class Model extends Base {
             items = [items]
         }
 
-        let me     = this,
-            {view} = me;
+        let me      = this,
+            {view}  = me,
+            records = [...items]; // Potential records
 
         // We hold vdom ids for now, so all incoming selections must be converted.
         items = items.map(item => item.isRecord ? view.getItemId(item) : Neo.isObject(item) ? item.id : item);
@@ -264,6 +265,7 @@ class Model extends Base {
             view.onSelect?.(items);
 
             me.fire('selectionChange', {
+                records,
                 selection: itemCollection
             })
         }
