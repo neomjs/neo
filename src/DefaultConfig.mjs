2a4b352b45b61d48ff396bd1e50fa993cf9b70db
tobiu <tobiasuhlig78@gmail.com>
2025-08-02 16:32:46 +0200

    v10.3.0

--- src/DefaultConfig.mjs
+++ src/DefaultConfig.mjs
@@ -299,12 +299,12 @@ const DefaultConfig = {
     useVdomWorker: true,
     /**
      * buildScripts/injectPackageVersion.mjs will update this value
-     * @default '10.2.1'
+     * @default '10.3.0'
      * @memberOf! module:Neo
      * @name config.version
      * @type String
      */
-    version: '10.2.1'
+    version: '10.3.0'
 };
 
 Object.assign(DefaultConfig, {
