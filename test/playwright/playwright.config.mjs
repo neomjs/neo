ff35fc545d464bf5aa8aac7b2026bcfa4ac5a81a
tobiu <tobiasuhlig78@gmail.com>
2025-09-26 16:09:57 +0200

    Correct Playwright Output Directory #7260

--- test/playwright/playwright.config.mjs
+++ test/playwright/playwright.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from '@playwright/test';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './unit',
-  outputDir: 'test-results',
+  testDir: path.join(__dirname, 'unit'),
+  outputDir: path.join(__dirname, 'test-results'),
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -15,7 +20,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['json', { outputFile: 'test-results/test-results.json' }]],
+  reporter: [['json', { outputFile: path.join(__dirname, 'test-results/test-results.json') }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
