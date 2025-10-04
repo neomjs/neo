4eb6e3fed2e2209486d71a708ea45d44ae35469b
kart-u <pandeykartik715@gmail.com>
2025-10-04 11:28:47 +0000

    Convert functional/Parse5Processor.mjs Test from Siesta to Playwright #7284

---
+++ test/playwright/unit/functional/Parse5Processor.spec.mjs
@@ -0,0 +1,82 @@
+import { test, expect } from '@playwright/test';
+import Neo from '../../../../src/Neo.mjs';
+import Button from '../../../../src/button/Base.mjs';
+import HtmlTemplateProcessor from '../../../../src/functional/util/HtmlTemplateProcessor.mjs';
+import { html } from '../../../../src/functional/util/html.mjs';
+
+const processor = HtmlTemplateProcessor;
+
+test.describe('functional/Parse5Processor', () => {
+    let parsedVdomResult;
+    const mockComponent = {
+        continueUpdateWithVdom: vdom => {
+            parsedVdomResult = vdom;
+        }
+    };
+
+    test('should parse a simple template with a single root node', async () => {
+        const template = html`<div><p>Hello</p></div>`;
+        await processor.process(template, mockComponent);
+
+        expect(parsedVdomResult).toEqual({
+            tag: 'div',
+            cn: [{
+                tag: 'p',
+                text: 'Hello'
+            }]
+        });
+    });
+
+    test('should handle interpolated values in text nodes', async () => {
+        const name = 'Neo';
+        const template = html`<p>Hello ${name}</p>`;
+        await processor.process(template, mockComponent);
+
+        expect(parsedVdomResult).toEqual({
+            tag: 'p',
+            text: 'Hello Neo'
+        });
+    });
+
+    test('should handle interpolated values in attributes', async () => {
+        const id = 'my-div';
+        const template = html`<div id="${id}"></div>`;
+        await processor.process(template, mockComponent);
+
+        expect(parsedVdomResult).toEqual({
+            tag: 'div',
+            id: 'my-div'
+        });
+    });
+
+    test('should handle non-string (object) values in attributes', async () => {
+        const styleObj = { color: 'blue', fontWeight: 'bold' };
+        const template = html`<div style="${styleObj}"></div>`;
+        await processor.process(template, mockComponent);
+
+        expect(parsedVdomResult).toEqual({
+            tag: 'div',
+            style: styleObj
+        });
+    });
+
+    test('should handle component tags via interpolation (lexical scope)', async () => {
+        const template = html`<${Button} text="Click Me"/>`;
+        await processor.process(template, mockComponent);
+
+        expect(parsedVdomResult).toEqual({
+            module: Button,
+            text: 'Click Me'
+        });
+    });
+
+    test('should handle component tags via global namespace string', async () => {
+        const template = html`<Neo.button.Base text="Global Click"/>`;
+        await processor.process(template, mockComponent);
+
+        expect(parsedVdomResult).toEqual({
+            module: Button,
+            text: 'Global Click'
+        });
+    });
+});
