502790ad418f25687ce36bd1d99d7aa7970f8066
tobiu <tobiasuhlig78@gmail.com>
2025-11-11 14:08:15 +0100

    #7446 generate-seo-files.mjs: switched sitemap url to https

--- buildScripts/generate-seo-files.mjs
+++ buildScripts/generate-seo-files.mjs
@@ -257,7 +257,7 @@ export async function getSitemapXml(options={}) {
     }).join('\n');
 
     return `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
 ${xmlEntries}
 </urlset>
 `;
