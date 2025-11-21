2488a1d7f5bec51b9a15645aac55a50d14e3f58a
tobiu <tobiasuhlig78@gmail.com>
2025-11-21 16:08:19 +0100

    v11.8.0

--- apps/portal/view/home/FooterContainer.mjs
+++ apps/portal/view/home/FooterContainer.mjs
@@ -108,7 +108,7 @@ class FooterContainer extends Container {
             }, {
                 module: Component,
                 cls   : ['neo-version'],
-                text  : 'v11.7.0'
+                text  : 'v11.8.0'
             }]
         }],
         /**
--- src/DefaultConfig.mjs
+++ src/DefaultConfig.mjs
@@ -299,12 +299,12 @@ const DefaultConfig = {
     useVdomWorker: true,
     /**
      * buildScripts/injectPackageVersion.mjs will update this value
-     * @default '11.7.0'
+     * @default '11.8.0'
      * @memberOf! module:Neo
      * @name config.version
      * @type String
      */
-    version: '11.7.0'
+    version: '11.8.0'
 };
 
 Object.assign(DefaultConfig, {
