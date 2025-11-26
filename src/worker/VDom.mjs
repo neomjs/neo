c1956c143895e17f81cb6fa94db3b8085129da06
tobiu <tobiasuhlig78@gmail.com>
2025-11-26 13:06:26 +0100

    Fix race condition in VDom worker initialization #7907

--- src/worker/VDom.mjs
+++ src/worker/VDom.mjs
@@ -1,6 +1,5 @@
-import Neo    from '../Neo.mjs';
-import Base   from './Base.mjs';
-import Helper from '../vdom/Helper.mjs';
+import Neo  from '../Neo.mjs';
+import Base from './Base.mjs';
 
 /**
  * The Vdom worker converts vdom templates into vnodes, as well as creating delta-updates.
@@ -40,6 +39,21 @@ class VDom extends Base {
         channel.port1.onmessage = me.onMessage.bind(me);
 
         me.sendMessage('app', {action: 'registerPort', transfer: port2}, [port2])
+    }
+
+    /**
+     * We need to ensure that the global Neo.config is set before importing the vdom Helper.
+     * Otherwise, the Helper.initAsync() method might use the wrong default values.
+     * This is critical for Neo.config.useDomApiRenderer === false, as it would otherwise load the
+     * wrong renderer utility (DomApiVnodeCreator instead of StringFromVnode).
+     * See: https://github.com/neomjs/neo/issues/7907
+     * @param {Object} msg The incoming message object.
+     * @param {Object} msg.data The initial global Neo.config data object.
+     * @param {String} msg.data.windowId The unique ID of the window/tab.
+     */
+    onRegisterNeoConfig(msg) {
+        super.onRegisterNeoConfig(msg);
+        import('../vdom/Helper.mjs')
     }
 }
 
