b5fa7c064a548f6aef852e907937ebc945d5cf04
tobiu <tobiasuhlig78@gmail.com>
2025-12-08 12:29:53 +0100

    legit app: live preview

--- apps/legit/view/Viewport.mjs
+++ apps/legit/view/Viewport.mjs
@@ -1,7 +1,9 @@
 import BaseViewport       from '../../../src/container/Viewport.mjs';
-import Container          from '../../../src/tree/List.mjs';
+import Container          from '../../../src/container/Base.mjs';
+import LivePreview        from '../../../src/code/LivePreview.mjs';
 import TreeList           from '../../../src/tree/List.mjs';
 import FileStore          from '../store/Files.mjs';
+import Splitter           from '../../../src/component/Splitter.mjs';
 import ViewportController from './ViewportController.mjs';
 
 /**
@@ -24,18 +26,42 @@ class Viewport extends BaseViewport {
          */
         legitApiKey: null,
         /*
-         * @member {Object} layout={ntype:'hbox', align:'stretch'}
+         * @member {Object} layout={ntype:'hbox',align:'stretch'}
          */
         layout: {ntype: 'hbox', align: 'stretch'},
-
         /**
          * @member {Object[]} items
          */
         items: [{
-            module: TreeList,
-            store : FileStore
+            module                    : TreeList,
+            store                     : FileStore,
+            showCollapseExpandAllIcons: false,
+            width                     : 300
         }, {
-
+            module      : Splitter,
+            resizeTarget: 'previous',
+            size        : 3
+        }, {
+            module: Container,
+            flex  : 1,
+            layout: {ntype: 'vbox', align: 'center', pack: 'center'},
+            items: [{
+                module: LivePreview,
+                style : {minHeight: '85%', minWidth: '85%'},
+                value: [
+                    "import Viewport from '../../examples/component/multiWindowHelix/Viewport.mjs';",
+                    "",
+                    "class MainView extends Viewport {",
+                    "    static config = {",
+                    "        className           : 'Portal.view.MultiWindowHelix',",
+                    "        showGitHubStarButton: false,",
+                    "        theme               : 'neo-theme-dark'",
+                    "    }",
+                    "}",
+                    "",
+                    "MainView = Neo.setupClass(MainView);"
+                ].join('\n'),
+            }]
         }]
     }
 }
