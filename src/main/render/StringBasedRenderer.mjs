7713cbbf595e5ef7151ea18c3fcb243eff1dbb85
tobiu <tobiasuhlig78@gmail.com>
2026-01-14 00:09:02 +0100

    refactor(StringBasedRenderer): Consolidate logic into createNode and remove unused methods (#8617)

--- src/main/render/StringBasedRenderer.mjs
+++ src/main/render/StringBasedRenderer.mjs
@@ -1,68 +1,13 @@
-import DomAccess from '../DomAccess.mjs';
-
 const StringBasedRenderer = {
     /**
-     * @param {String} html representing a single element
+     * @param {Object}      data
+     * @param {String}      data.outerHTML The HTML string of the node to create.
      * @returns {DocumentFragment}
      */
-    htmlStringToElement(html) {
+    createNode({outerHTML}) {
         const template = document.createElement('template');
-        template.innerHTML = html;
+        template.innerHTML = outerHTML;
         return template.content
-    },
-
-    /**
-     * Handles string-based insertion of a new node into the DOM.
-     * This method is called by `insertNode()` when `NeoConfig.useDomApiRenderer` is false.
-     *
-     * @param {Object}      data
-     * @param {Boolean}     data.hasLeadingTextChildren Flag to honor leading comments.
-     * @param {Number}      data.index                  The index at which to insert the new node.
-     * @param {String}      data.outerHTML              The HTML string of the node to insert.
-     * @param {HTMLElement} data.parentNode             The parent DOM node to insert into.
-     * @param {Object[]}    [data.postMountUpdates]     Array of post-mount updates (e.g. scroll state).
-     * @private
-     */
-    insertNodeAsString({hasLeadingTextChildren, index, outerHTML, parentNode, postMountUpdates}) {
-        let me = this;
-
-        // If comments detected, parse HTML string to a node and use insertBefore/appendChild on childNodes.
-        if (hasLeadingTextChildren) {
-            let node = me.htmlStringToElement(outerHTML);
-
-            if (index < parentNode.childNodes.length) {
-                parentNode.insertBefore(node, parentNode.childNodes[index])
-            } else {
-                parentNode.appendChild(node)
-            }
-        }
-        // If no comments detected, use insertAdjacentHTML for element nodes.
-        else {
-            let countChildren = parentNode.children.length; // Use `children` for `insertAdjacentHTML` context
-
-            if (index > 0 && index >= countChildren) {
-                parentNode.insertAdjacentHTML('beforeend', outerHTML);
-            } else if (countChildren > 0 && countChildren > index) {
-                parentNode.children[index].insertAdjacentHTML('beforebegin', outerHTML)
-            } else if (countChildren > 0) {
-                parentNode.children[countChildren - 1].insertAdjacentHTML('afterend', outerHTML)
-            } else {
-                parentNode.insertAdjacentHTML('beforeend', outerHTML)
-            }
-        }
-
-        if (postMountUpdates?.length > 0) {
-            let node;
-
-            postMountUpdates.forEach(update => {
-                node = DomAccess.getElement(update.id);
-
-                if (node) {
-                    if (update.scrollLeft) {node.scrollLeft = update.scrollLeft}
-                    if (update.scrollTop)  {node.scrollTop  = update.scrollTop}
-                }
-            })
-        }
     }
 };
 
