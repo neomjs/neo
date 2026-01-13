21fe331ea48cc52775b14f8139475551264b1894
tobiu <tobiasuhlig78@gmail.com>
2026-01-13 19:37:56 +0100

    fix: Use optional chaining for layout access in Container.insert (#8610)

--- src/container/Base.mjs
+++ src/container/Base.mjs
@@ -646,7 +646,7 @@ class Container extends Component {
 
             // added the true param => for card layouts, we do not want a dynamically inserted cmp to get removed right away
             // since it will most likely get activated right away
-            me.layout.applyChildAttributes(item, index, true);
+            me.layout?.applyChildAttributes(item, index, true);
 
             items.splice(index, 0, item);
 
