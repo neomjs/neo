25a51584fdc1cae61e7ed9de87b3ea11c53450ee
tobiu <tobiasuhlig78@gmail.com>
2026-02-05 10:37:47 +0100

    feat: Add Selection Model tab to DevRank controls (#8997)

--- apps/devrank/view/ControlsContainer.mjs
+++ apps/devrank/view/ControlsContainer.mjs
@@ -1,4 +1,6 @@
+import * as selection from '../../../src/selection/grid/_export.mjs';
 import Container from '../../../src/container/Base.mjs';
+import Radio     from '../../../src/form/field/Radio.mjs';
 import TabContainer from '../../../src/tab/Container.mjs';
 
 /**
@@ -55,6 +57,47 @@ class ControlsContainer extends Container {
                     ntype    : 'label',
                     reference: 'count-rows-label',
                     style    : {marginTop: '1em'}
+                }]
+            }, {
+                module: Container,
+                header: {text: 'Selection'},
+                layout: 'vbox',
+
+                itemDefaults: {
+                    module        : Radio,
+                    hideLabel     : true,
+                    hideValueLabel: false,
+                    labelText     : '',
+                    listeners     : {change: 'up.onSelectionModelChange'},
+                    name          : 'selectionModel',
+                    style         : {marginTop: '.3em'},
+                    width         : 200
+                },
+
+                items: [{
+                    ntype: 'label',
+                    style: {marginTop: 0},
+                    text : 'Pick the Selection Model'
+                }, {
+                    style         : {marginTop: '1em'},
+                    selectionModel: selection.CellModel,
+                    valueLabel    : 'Cell'
+                }, {
+                    selectionModel: selection.ColumnModel,
+                    valueLabel    : 'Column'
+                }, {
+                    checked       : true,
+                    selectionModel: selection.RowModel,
+                    valueLabel    : 'Row'
+                }, {
+                    selectionModel: selection.CellColumnModel,
+                    valueLabel    : 'Cell & Column'
+                }, {
+                    selectionModel: selection.CellRowModel,
+                    valueLabel    : 'Cell & Row'
+                }, {
+                    selectionModel: selection.CellColumnRowModel,
+                    valueLabel    : 'Cell & Column & Row'
                 }]
             }]
         }],
@@ -117,6 +160,13 @@ class ControlsContainer extends Container {
         // Simple single-field filter for now
         // We can expand this to search multiple fields if needed
         me.grid.store.getFilter('login').value = data.value;
+    }
+
+    /**
+     * @param {Object} data
+     */
+    onSelectionModelChange(data) {
+        this.grid.body.selectionModel = data.component.selectionModel
     }
 
     /**
