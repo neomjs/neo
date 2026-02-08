4e458ea705383169bf5b02621b98814846d97aee
tobiu <tobiasuhlig78@gmail.com>
2026-02-08 21:59:38 +0100

    feat: Refactor DevRank Controls: Split Search & Reorder Fields (#9057)

--- apps/devrank/store/Contributors.mjs
+++ apps/devrank/store/Contributors.mjs
@@ -1,16 +1,47 @@
-import Contributor from '../model/Contributor.mjs';
-import Store       from '../../../src/data/Store.mjs';
+import ContributorModel from '../model/Contributor.mjs';
+import Store            from '../../../src/data/Store.mjs';
 
+/**
+ * @class DevRank.store.Contributors
+ * @extends Neo.data.Store
+ */
 class Contributors extends Store {
     static config = {
+        /**
+         * @member {String} className='DevRank.store.Contributors'
+         * @protected
+         */
         className: 'DevRank.store.Contributors',
-        model    : Contributor,
+        /**
+         * @member {Neo.data.Model} model=ContributorModel
+         */
+        model: ContributorModel,
+        /**
+         * @member {String} keyProperty='login'
+         */
         keyProperty: 'login',
-        autoLoad : true,
-        url      : '../../apps/devrank/resources/users.json',
-        sorters  : [
+        /**
+         * @member {Boolean} autoLoad=true
+         */
+        autoLoad: true,
+        /**
+         * @member {Object[]} filters
+         */
+        filters: [
+            {property: 'location', operator: 'like', value: null},
+            {property: 'login',    operator: 'like', value: null},
+            {property: 'name',     operator: 'like', value: null}
+        ],
+        /**
+         * @member {Object[]} sorters
+         */
+        sorters: [
             {property: 'total_contributions', direction: 'DESC'}
-        ]
+        ],
+        /**
+         * @member {String} url='../../apps/devrank/resources/users.json'
+         */
+        url: '../../apps/devrank/resources/users.json'
     }
 }
 
