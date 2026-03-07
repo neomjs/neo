1f287ef09523983ebfeb7ca523d59084b6e67fa2
tobiu <tobiasuhlig78@gmail.com>
2026-03-07 17:59:47 +0100

    refactor(Grid): migrate GridDragScroll to use the new ScrollSync coordinator API (#9372)

--- src/main/addon/GridDragScroll.mjs
+++ src/main/addon/GridDragScroll.mjs
@@ -93,12 +93,14 @@ class GridDragScroll extends Base {
             return
         }
 
-        if (registration.containerElement) {
-            registration.containerElement.scrollLeft -= velocity.x
-        }
+        let newScrollLeft = registration.containerElement ? registration.containerElement.scrollLeft - velocity.x : undefined,
+            newScrollTop  = registration.bodyElement      ? registration.bodyElement.scrollTop       - velocity.y : undefined;
 
-        if (registration.bodyElement) {
-            registration.bodyElement.scrollTop -= velocity.y
+        if (Neo.main.addon.ScrollSync) {
+            Neo.main.addon.ScrollSync.syncTo(registration.id, newScrollLeft, newScrollTop)
+        } else {
+            if (newScrollLeft !== undefined) registration.containerElement.scrollLeft = newScrollLeft;
+            if (newScrollTop  !== undefined) registration.bodyElement.scrollTop       = newScrollTop;
         }
 
         velocity.x *= friction;
@@ -189,12 +191,14 @@ class GridDragScroll extends Base {
             deltaX         = drag.lastX - x,
             deltaY         = drag.lastY - y;
 
-        if (deltaX !== 0 && registration.containerElement) {
-            registration.containerElement.scrollLeft += deltaX
-        }
+        let newScrollLeft = deltaX !== 0 && registration.containerElement ? registration.containerElement.scrollLeft + deltaX : undefined,
+            newScrollTop  = deltaY !== 0 && registration.bodyElement      ? registration.bodyElement.scrollTop       + deltaY : undefined;
 
-        if (deltaY !== 0 && registration.bodyElement) {
-            registration.bodyElement.scrollTop += deltaY
+        if (Neo.main.addon.ScrollSync) {
+            Neo.main.addon.ScrollSync.syncTo(registration.id, newScrollLeft, newScrollTop)
+        } else {
+            if (newScrollLeft !== undefined) registration.containerElement.scrollLeft = newScrollLeft;
+            if (newScrollTop  !== undefined) registration.bodyElement.scrollTop       = newScrollTop;
         }
 
         drag.lastX = x;
