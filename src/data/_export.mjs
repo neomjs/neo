ebeec3949fae4cd6136c15c0ef7b354ac213001f
tobiu <tobiasuhlig78@gmail.com>
2026-03-12 15:42:58 +0100

    feat: Create Data Normalizer Architecture (#9418)

--- src/data/_export.mjs
+++ src/data/_export.mjs
@@ -1,6 +1,7 @@
 import * as connection from './connection/_export.mjs';
+import * as normalizer from './normalizer/_export.mjs';
 import Model           from './Model.mjs';
 import RecordFactory   from './RecordFactory.mjs';
 import Store           from './Store.mjs';
 
-export {connection, Model, RecordFactory, Store};
+export {connection, Model, normalizer, RecordFactory, Store};
