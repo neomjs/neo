ebc66d958de74049c6aa94040606365e2608536b
neo-gpt <neo-gpt@neomjs.com>
2026-07-01 01:12:28 +0200

    fix(build): support Commander 15 imports (#14387)

--- buildScripts/create/appMinimal.mjs
+++ buildScripts/create/appMinimal.mjs
@@ -1,6 +1,6 @@
 import chalk           from 'chalk';
 import {spawnSync}     from 'child_process';
-import {Command}       from 'commander/esm.mjs';
+import {Command}       from 'commander';
 import envinfo         from 'envinfo';
 import fs              from 'fs-extra';
 import inquirer        from 'inquirer';
--- buildScripts/util/copyFolder.mjs
+++ buildScripts/util/copyFolder.mjs
@@ -1,4 +1,4 @@
-import {Command}       from 'commander/esm.mjs';
+import {Command}       from 'commander';
 import fs              from 'fs-extra';
 import {sanitizeInput} from './sanitizer.mjs';
 
